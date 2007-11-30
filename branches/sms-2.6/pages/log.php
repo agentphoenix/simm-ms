9509923cba893a90f17deb6b9e5b6d0c13595bc5
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2007-11-30 20:46:34 +0000

    adding the pages directory

---
+++ branches/sms-2.6/pages/log.php
@@ -0,0 +1,154 @@
+<?php
+
+/**
+This is a necessary system file. Do not modify this page unless you are highly
+knowledgeable as to the structure of the system. Modification of this file may
+cause SMS to no longer function.
+
+Author: Nathan Wharry [ mail@herschwolf.net ]
+File: pages/log.php
+Purpose: To display the individual personal logs
+
+System Version: 2.6.0
+Last Modified: 2007-10-10 1004 EST
+**/
+
+/* define the page class */
+$pageClass = "simm";
+
+/* pull in the menu */
+if( isset( $sessionCrewid ) ) {
+	include_once( 'skins/' . $sessionDisplaySkin . '/menu.php' );
+} else {
+	include_once( 'skins/' . $skin . '/menu.php' );
+}
+
+/* do some advanced checking to make sure someone's not trying to do a SQL injection */
+if( !empty( $_GET['position'] ) && preg_match( "/^\d+$/", $_GET['position'], $matches ) == 0 ) {
+	errorMessageIllegal( "activation page" );
+	exit();
+} else {
+	/* set the GET variable */
+	$pl_id = $_GET['id'];
+}
+
+/* get post id for individual message display */
+if( isset( $pl_id ) ) {
+
+	/* pull all the information relating to the post */
+	$getlog = "SELECT * FROM sms_personallogs ";
+	$getlog.= "WHERE logid = '$pl_id' LIMIT 1";
+	$getlogResult = mysql_query ( $getlog );
+	
+	/* pull all posts to create the next and prev post links */
+	$getlogs = "SELECT logid ";
+	$getlogs.= "FROM sms_personallogs ";
+	$getlogs.= "WHERE logStatus = 'activated' ";
+	$getlogs.= "ORDER BY logPosted ASC";
+	$getlogsResult = mysql_query ( $getlogs );
+	
+	/* extract the post data into the MySQL field name variables */
+	$loginfo = mysql_fetch_array( $getlogResult );
+		extract( $loginfo, EXTR_OVERWRITE ); 
+	
+	/* if log is untitled give it a title */
+	if ( $logTitle == "" ) {
+		$logTitle = "Untitled";
+	}
+	
+?>
+
+	<div class="body">
+	
+		<span class="fontTitle">
+			<? printCrewName( $loginfo['logAuthor'], "rank", "noLink" ); ?>'s Personal Log - 
+			<? printText( $logTitle ); ?>
+		</span><br /><br />
+		
+		<span class="fontNormal postDetails">
+		<div align="center">
+		
+		<?
+		
+			/* point the previous and next post buttons to the correct posts */
+		
+			$idNumbers = array();
+			
+			while ( $myrow = mysql_fetch_array( $getlogsResult ) ) {
+				$idNumbers[] = $myrow['logid'];
+			}	
+			
+			foreach( $idNumbers as $key => $value ) {
+				if( $pl_id == $value ) {
+					
+					$nextKey = $key+1;
+					$prevKey = $key-1;
+			
+				/* display the previous and next links in the post details box */
+				if( $idNumbers[$prevKey] != '' ) {
+						printText ( "<a href='$webLocation/index.php?page=log&id=$idNumbers[$prevKey]'><img src='$webLocation/images/previous.png' alt='Previous Entry' border='0' class='image' /></a>" );
+					} if( ($idNumbers[$prevKey] != '') && ($idNumbers[$nextKey] != '') ) {
+						echo "&nbsp;";
+					} if( $idNumbers[$nextKey] != '' ) {
+						printText ( "<a href='$webLocation/index.php?page=log&id=$idNumbers[$nextKey]'><img src='$webLocation/images/next.png' alt='Next Entry' border='0' class='image' /></a>" );
+					}
+				}
+			}
+		
+		?>
+				
+				<br />
+				<b>Log Details</b><br />
+				<?
+			
+				if( in_array( "m_logs", $sessionAccess ) ) {
+					echo "<a href='" . $webLocation . "admin.php?page=manage&sub=logs&id=" . $pl_id . "' class='edit'><b>Edit</b></a>";
+					echo "&nbsp; &middot; &nbsp;";
+	
+				?>	
+	
+					<script type="text/javascript">
+						document.write( "<a href=\"<?=$webLocation;?>admin.php?page=manage&sub=logs&remove=<?=$pl_id;?>\" class=\"delete\" onClick=\"javascript:return confirm('This action is permanent and cannot be undone. Are you sure you want to delete this personal log?')\">Delete</a>" );
+					</script>
+					<noscript>
+						<a href="<?=$webLocation;?>admin.php?page=manage&sub=logs&remove=<?=$pl_id;?>" class="delete"><b>Delete</b></a>
+					</noscript>
+					
+				<?
+					
+					if( $loginfo['logStatus'] == "pending" ) {
+					
+						echo "&nbsp; &middot; &nbsp;";
+						echo "<a href='" . $webLocation . "admin.php?page=manage&sub=activate&type=log&id=" . $pl_id . "&action=activate'><b>Activate</b></a>";
+					
+					}
+				}
+				
+				?><p></p>
+			</div> <!-- close the centering div -->
+			
+			<table>
+				<tr>
+					<td class="tableCellLabel">Title</td>
+					<td>&nbsp;</td>
+					<td><? printText( $logTitle ); ?></td>
+				</tr>
+				<tr>
+					<td class="tableCellLabel">Author</td>
+					<td>&nbsp;</td>
+					<td><? printCrewName( $logAuthor, "rank", "link" ); ?></td>
+				</tr>
+				<tr>
+					<td class="tableCellLabel">Posted</td>
+					<td>&nbsp;</td>
+					<td><?=dateFormat( "medium", $logPosted );?></td>
+				</tr>
+			</table>
+		
+		</span>
+	
+		<? printText( $logContent ); ?>
+	
+	</div> <!--Close Div content class tag-->
+	
+<? } ?>
---
+++ branches/sms-2.6/pages/manifest.php
@@ -0,0 +1,227 @@
+<?php
+
+/**
+This is a necessary system file. Do not modify this page unless you are highly
+knowledgeable as to the structure of the system. Modification of this file may
+cause SMS to no longer function.
+
+Author: David VanScott [ davidv@anodyne-productions.com ]
+File: pages/manifest.php
+Purpose: Provides a listing from the database of either the active crew, the 
+	inactive crew (departed players), available positions, or non-playing characters 
+	on the simm.
+
+System Version: 2.6.0
+Last Modified: 2007-10-10 1008 EST
+**/
+
+if( $manifestDisplay == "full" && $_GET['disp'] == "crew" ) {
+	include_once( 'pages/manifestFull.php' );
+} else {
+
+/* define the page class and set vars */
+$pageClass = "personnel";
+$display = $_GET['disp'];
+
+/* pull in the main navigation */
+if( isset( $sessionCrewid ) ) {
+	include_once( 'skins/' . $sessionDisplaySkin . '/menu.php' );
+} else {
+	include_once( 'skins/' . $skin . '/menu.php' );
+}
+
+/* set the rank variable */
+if( isset( $sessionCrewid ) ) {
+	$rankSet = $sessionDisplayRank;
+} else {
+	$rankSet = $rankSet;
+}
+
+/* if there is no GET variable, set it to the players manifest */
+if( !$display ) {
+	$display = "crew";
+}
+
+if( $display == "crew" || $display == "open" ) {
+	$departments = "SELECT * FROM sms_departments WHERE deptDisplay = 'y' ";
+	$departments.= "AND deptType = 'playing' ORDER BY deptOrder ASC";
+} elseif( $display == "npcs" || $display == "past" ) {
+	$departments = "SELECT * FROM sms_departments WHERE deptDisplay = 'y' ORDER BY deptOrder ASC";
+}
+
+$deptResults = mysql_query( $departments );
+
+?>
+
+<div class="body">
+	<span class="fontTitle">
+	<?
+		if( $display == "crew" ) {
+			echo "Crew Manifest";
+		} elseif( $display == "open" ) {
+			echo "Open Positions";
+		} elseif( $display == "past" ) {
+			echo "Departed Crew Manifest";
+		} elseif( $display == "npcs" ) {
+			echo "NPC Manifest";
+		}
+	?>
+	</span><br /><br />
+	
+	<!-- manifest navigation table -->
+	<div align="center">
+	<span class="fontSmall">
+		<? if( $manifestDisplay == "full" ) { ?>
+		<a href="<?=$webLocation;?>index.php?page=manifestFull&disp=crew">Crew Manifest</a>
+		&nbsp; &middot; &nbsp;
+		<? } else { ?>
+		<a href="<?=$webLocation;?>index.php?page=manifest&disp=crew">Crew Manifest</a>
+		&nbsp; &middot; &nbsp;
+		<? } if( $manifestDisplay == "split" ) { ?>
+		<a href="<?=$webLocation;?>index.php?page=manifest&disp=npcs">NPC Manifest</a>
+		&nbsp; &middot; &nbsp;
+		<? } ?>
+		<a href="<?=$webLocation;?>index.php?page=manifest&disp=open">Open Positions</a>
+		&nbsp; &middot; &nbsp;
+		<a href="<?=$webLocation;?>index.php?page=manifest&disp=past">Departed Crew</a>
+	</span>
+	</div>
+
+	<table>
+	<?
+
+	/* pull the data out of the department query */
+	while ( $dept = mysql_fetch_array( $deptResults ) ) {
+		extract( $dept, EXTR_OVERWRITE );
+		
+	?>
+		<tr>
+			<td colspan="4" height="15"></td>
+		</tr>
+		<tr>
+			<td colspan="4">
+				<font class="fontMedium" color="#<?=$deptColor;?>">
+					<b><? printText( $deptName ); ?></b>
+				</font>
+			</td>
+		</tr>
+		<?
+		
+		if( $display == "crew" ) {
+		
+			$manifest = "SELECT position.positionName, position.positionDept, crew.crewid, ";
+			$manifest.= "crew.firstName, crew.lastName, crew.rankid, crew.species, crew.gender, ";
+			$manifest.= "crew.loa, rank.rankImage, rank.rankName FROM sms_positions AS position, ";
+			$manifest.= "sms_crew AS crew, sms_ranks AS rank WHERE ";
+			$manifest.= "position.positionDept = '$dept[deptid]' AND ( position.positionid = crew.positionid ";
+			$manifest.= "OR position.positionid = crew.positionid2 ) AND position.positionDisplay = 'y' AND ";
+			$manifest.= "crew.rankid = rank.rankid AND crew.crewType = 'active' ORDER BY position.positionOrder, rank.rankid ASC";
+			$manifestResults = mysql_query( $manifest );
+		
+		} elseif( $display == "open" ) {
+		
+			$manifest = "SELECT position.positionid, position.positionName, position.positionOpen, ";
+			$manifest.= "position.positionDept FROM sms_positions AS position, sms_departments AS dept ";
+			$manifest.= "WHERE position.positionDept = '$dept[deptid]' AND position.positionOpen > '0' ";
+			$manifest.= "AND position.positionDisplay = 'y' AND dept.deptid = '$dept[deptid]' AND ";
+			$manifest.= "dept.deptType = 'playing' ORDER BY position.positionOrder ASC";
+			$manifestResults = mysql_query( $manifest );
+		
+		} elseif( $display == "npcs" ) {
+		
+			$manifest = "SELECT position.positionName, position.positionid, position.positionDept, ";
+			$manifest.= "position.positionOpen, crew.gender, crew.species, ";
+			$manifest.= "crew.crewid, crew.firstName, crew.lastName, crew.rankid, crew.positionid, ";
+			$manifest.= "rank.rankid, rank.rankImage, rank.rankName, dept.deptType FROM ";
+			$manifest.= "sms_positions AS position, sms_crew AS crew, sms_ranks AS rank, ";
+			$manifest.= "sms_departments AS dept WHERE position.positionDept = '$dept[deptid]' ";
+			$manifest.= "AND ( position.positionid = crew.positionid OR position.positionid = crew.positionid2 ) ";
+			$manifest.= "AND position.positionDisplay = 'y' AND crew.rankid = rank.rankid AND ";
+			$manifest.= "crew.crewType = 'npc' AND dept.deptid = '$dept[deptid]' ";
+			$manifest.= "ORDER BY position.positionOrder, rank.rankid ASC";
+			$manifestResults = mysql_query( $manifest );
+		
+		} elseif( $display == "past" ) {
+		
+			$manifest = "SELECT position.positionName, crew.crewid, crew.firstName, crew.lastName, crew.species, ";
+			$manifest.= "crew.gender, rank.rankImage, rank.rankName FROM sms_positions AS position, ";
+			$manifest.= "sms_crew AS crew, sms_ranks AS rank WHERE position.positionDept = '$dept[deptid]' AND ";
+			$manifest.= "( position.positionid = crew.positionid OR position.positionid = crew.positionid2 ) ";
+			$manifest.= "AND position.positionDisplay = 'y' AND crew.rankid = rank.rankid ";
+			$manifest.= "AND crewType = 'inactive' ORDER BY position.positionOrder, rank.rankid ASC";
+			$manifestResults = mysql_query( $manifest );
+		
+		}
+		
+		while ( $manifestList = mysql_fetch_assoc($manifestResults) ) {
+			extract( $manifestList, EXTR_OVERWRITE );
+		
+		?>
+		<tr>
+			<td width="35%" valign="middle" style="padding-left: 1em;"><? printText( $positionName ); ?></td>
+			<td width="15%" valign="middle" align="right">
+				<?
+					if( $display == "open" ) {
+						echo "<img src='" . $webLocation . "images/ranks/" . $rankSet . "/blank.png' />";
+					} else {
+						if( !empty( $rankImage ) ) {
+							echo "<img src='" . $webLocation . "images/ranks/" . $rankSet . "/" . $rankImage . "' />";
+						} else {
+							echo "<img src='" . $webLocation . "images/ranks/" . $rankSet . "/blank.png' />";
+						}
+					}
+				?>
+			</td>
+			<td width="40%" valign="middle">
+				<?
+					if( $display == "npcs" ) {
+						echo "<font class='fontSmall'><b>";
+						printText( $rankName . " " . $firstName . " " . $lastName );
+						echo "</b></font>";
+						echo "<br />";
+						echo "<font class='fontSmall'>";
+						printText( $species . " " . $gender );
+						echo "</font>";
+						
+						if( $deptType == "playing" && $positionOpen > "0" ) {
+							echo "<br />";
+							echo "<font class='fontSmall'><a href='" . $webLocation . "index.php?page=join&position=" . $positionid . "'>Position Available - Apply Now</a></font>";
+						}
+						
+					} elseif( $display == "open" ) {
+						echo "<font class='fontSmall'><a href='" . $webLocation . "index.php?page=join&position=" . $positionid . "'>Position Available - Apply Now</a></font>";
+					} elseif( $display =="crew" || "past" ) {
+						echo "<font class='fontSmall'><b>";
+						printText( $rankName . " " . $firstName . " " . $lastName );
+						echo "</b></font>";
+						echo "<br />";
+						echo "<font class='fontSmall'>";
+						printText( $species . " " . $gender );
+						echo "</font>";
+					}
+				?>
+			</td>
+			<td width="10%" valign="middle">
+				<?
+					if( $display == "npcs" ) {
+						echo "<a href='" . $webLocation . "index.php?page=bio&crew=" . $crewid . "'><img src='" . $webLocation . "images/combadge-npc.jpg' border='0' class='image' /></a>";
+					} elseif( $display == "open" ) {
+						echo "";
+					} elseif( $display == "crew" ) {
+						if( $loa == "1" ) {
+							echo "<a href='" . $webLocation . "index.php?page=bio&crew=" . $crewid . "'><img src='" . $webLocation . "images/combadge-loa.jpg' border='0' class='image' /></a>";
+						} elseif( $loa == "2" ) {
+							echo "<a href='" . $webLocation . "index.php?page=bio&crew=" . $crewid . "'><img src='" . $webLocation . "images/combadge-eloa.jpg' border='0' class='image' /></a>";
+						} elseif( $loa == "0" ) {
+							echo "<a href='" . $webLocation . "index.php?page=bio&crew=" . $crewid . "'><img src='" . $webLocation . "images/combadge.jpg' border='0' class='image' /></a>";
+						}
+					} elseif( $display == "past" ) {
+						echo "<a href='" . $webLocation . "index.php?page=bio&crew=" . $crewid . "'><img src='" . $webLocation . "images/combadge.jpg' border='0' class='image' /></a>";
+					}
+				?>
+			</td>
+		</tr>
+	<? } } ?>
+	</table>
+</div>
+<? } ?>
