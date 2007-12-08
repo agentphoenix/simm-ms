692e760dfbda6f148a9ed4d682cfd769fa82a311
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2007-12-08 23:37:32 +0000

    removing jefferson trunk

--- trunk/system/application/views/_footer.php
+++
@@ -1,8 +0,0 @@
-			</div> <!-- close #container .body .content -->
-		</div> <!-- close #container .body -->
-		<div class="footer">
-			&copy; <?=date('Y');?> Anodyne Productions. All rights reserved.
-		</div>
-		</div> <!-- close #container -->
-	</body>
-</html>
--- trunk/system/application/views/main_index.php
+++
@@ -1,6 +0,0 @@
-<? $this->load->view('_header.php');?>
-
-	<div class="fontTitle bold"><?=$title;?></div>
-	<?=$content;?>
-
-<? $this->load->view('_footer.php');?>
