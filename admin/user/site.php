2b1bb7f1dbb9e2bf2f5283c0b8c46a3d13e531ba
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2007-12-26 18:36:56 +0000

    we're gonna start using tabs on the site options page as well ... putting the first of those changes into place

--- admin/user/site.php
+++ admin/user/site.php
@@ -10,49 +10,42 @@ File: admin/user/site.php
 Purpose: Page that allows a user to various site options
 
 System Version: 2.6.0
-Last Modified: 2007-12-22 2224 EST
+Last Modified: 2007-12-26 0923 EST
 **/
 
 /* access check */
 if( in_array( "u_options", $sessionAccess ) ) {
+	
+	$sec = $_GET['sec'];
+	
+	if( !isset( $sec ) ) {
+		$sec = 1;
+	}
+	
+	if( !is_numeric( $sec ) ) {
+		$sec = 1;
+	}
 
-	/* set the page class */
+?>
+	
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('#container-1 > ul').tabs(<?php echo $sec; ?>);
+		});
+	</script>
+	
+<?php
+
+	/* set the page variables */
 	$pageClass = "admin";
 	$subMenuClass = "user";
-	$action = $_GET['action'];
 	$options = $_POST['options_x'];
+	$actionSkin = $_POST['action_skin_x'];
+	$actionRank = $_POST['action_rank_x'];
 	
-	/* explode the strings */
-	$allowedRanksArray = explode( ",", $allowedRanks );
-	$allowedSkinsArray = explode( ",", $allowedSkins );
-	
-	/* create a temporary array for the SQL injection check */
-	foreach( $allowedSkinsArray as $key => $value ) {
-		$skinsArray[] = trim( $value );
-	}
-	
-	/* create a temporary array for the SQL injection check */
-	foreach( $allowedRanksArray as $key => $value ) {
-		$ranksArray[] = trim( $value );
-	}
-	
-	if( !empty( $_GET['changerank'] ) && !in_array( $_GET['changerank'], $ranksArray ) ) {
-		errorMessage( "site options" );
-		exit();
-	} else {
-		/* set the GET variable */
-		$changerank = $_GET['changerank'];
-	}
-	
-	if( !empty( $_GET['changeskin'] ) && !in_array( $_GET['changeskin'], $skinsArray ) ) {
-		errorMessage( "site options" );
-		exit();
-	} else {
-		/* set the GET variable */
-		$changeskin = $_GET['changeskin'];
-	}
-	
-	if( $action == "skin" ) {
+	if( isset( $actionSkin ) ) {
+		/* define the POST variables */
+		$changeskin = $_POST['changeskin'];
 		
 		/* do the update query */
 		$query = "UPDATE sms_crew SET displaySkin = '$changeskin' WHERE crewid = '$sessionCrewid' LIMIT 1";
@@ -64,22 +57,26 @@ if( in_array( "u_options", $sessionAccess ) ) {
 		/* set a new session variable */
 		$_SESSION['sessionDisplaySkin'] = $changeskin;
 		$sessionDisplaySkin = $_SESSION['sessionDisplaySkin'];
-	
+		
+		/* set the type for the query check */
 		$type = "skin";
 		
-	} if( $action == "rank" ) {
+	} if( isset( $actionRank ) ) {
+		/* define the POST variables */
+		$rankSet = $_POST['rankSet'];
 		
 		/* do the update query */
-		$query = "UPDATE sms_crew SET displayRank = '$changerank' WHERE crewid = '$sessionCrewid' LIMIT 1";
+		$query = "UPDATE sms_crew SET displayRank = '$rankSet' WHERE crewid = '$sessionCrewid' LIMIT 1";
 		$result = mysql_query( $query );
 		
 		/* optimize the table */
 		optimizeSQLTable( "sms_crew" );
 		
 		/* set a new session variable */
-		$_SESSION['sessionDisplayRank'] = $changerank;
+		$_SESSION['sessionDisplayRank'] = $rankSet;
 		$sessionDisplayRank = $_SESSION['sessionDisplayRank'];
-	
+		
+		/* set the type for the query check */
 		$type = "rank set";
 		
 	} if( isset( $options ) ) {
@@ -166,16 +163,15 @@ if( in_array( "u_options", $sessionAccess ) ) {
 		
 		<span class="fontTitle">Site Options</span><br /><br />
 		
-		SMS gives users more control of what they see when they're logged in. From this page, you
-		can set the skin you use when you're logged in and the rank set you see. In the future,
-		we'll be letting you choose whether you see some of the items in the control panel, and 
-		even how many of those items you want to see.<br /><br />
+		SMS gives users more control of what they see when they're logged in now. From this page,
+		you can set the skin you use when you're logged in, the rank set you see as well as control
+		panel options and personalized menu items.<br />
 	
 		<?
 	
 		$getUserInfo = "SELECT cpShowPosts, cpShowLogs, cpShowNews, cpShowPostsNum, ";
 		$getUserInfo.= "cpShowLogsNum, cpShowNewsNum, menu1, menu2, menu3, menu4, menu5, ";
-		$getUserInfo.= "menu6 FROM sms_crew WHERE crewid = '$sessionCrewid' LIMIT 1";
+		$getUserInfo.= "menu6, menu7, menu8, menu9, menu10 FROM sms_crew WHERE crewid = '$sessionCrewid' LIMIT 1";
 		$getUserInfoResult = mysql_query( $getUserInfo );
 	
 		while( $userPrefs = mysql_fetch_assoc( $getUserInfoResult ) ) {
@@ -183,188 +179,248 @@ if( in_array( "u_options", $sessionAccess ) ) {
 		}
 	
 		?>
-		<form method="post" action="<?=$webLocation;?>admin.php?page=user&sub=site">
-		<table>
-			<tr>
-				<td colspan="3" class="fontMedium"><b>Show the following in the Control Panel?</b></td>
-			</tr>
-			<tr>
-				<td colspan="3" height="1"></td>
-			</tr>
-			<tr>
-				<td class="tableCellLabel">Latest Mission Entries</td>
-				<td>&nbsp;</td>
-				<td>
-					<input type="radio" id="showPostsY" name="cpShowPosts" value="y" <? if( $cpShowPosts == "y" ) { echo "checked"; } ?>/> <label for="showPostsY">Yes</label>
-					<input type="radio" id="showPostsN" name="cpShowPosts" value="n" <? if( $cpShowPosts == "n" ) { echo "checked"; } ?>/> <label for="showPostsN">No</label>
-				</td>
-			</tr>
-			<tr>
-				<td class="tableCellLabel">Latest Personal Logs</td>
-				<td>&nbsp;</td>
-				<td>
-					<input type="radio" id="showLogsY" name="cpShowLogs" value="y" <? if( $cpShowLogs == "y" ) { echo "checked"; } ?>/> <label for="showLogsY">Yes</label>
-					<input type="radio" id="showLogsN" name="cpShowLogs" value="n" <? if( $cpShowLogs == "n" ) { echo "checked"; } ?>/> <label for="showLogsN">No</label>
-				</td>
-			</tr>
-			<tr>
-				<td class="tableCellLabel">Latest News Items</td>
-				<td>&nbsp;</td>
-				<td>
-					<input type="radio" id="showNewsY" name="cpShowNews" value="y" <? if( $cpShowNews == "y" ) { echo "checked"; } ?>/> <label for="showNewsY">Yes</label>
-					<input type="radio" id="showNewsN" name="cpShowNews" value="n" <? if( $cpShowNews == "n" ) { echo "checked"; } ?>/> <label for="showNewsN">No</label>
-				</td>
-			</tr>
-			<tr>
-				<td colspan="3" height="5"></td>
-			</tr>
-			<tr>
-				<td colspan="3" class="fontMedium">
-					<b>How many of the following should be shown in the Control Panel?</b>
-				</td>
-			</tr>
-			<tr>
-				<td colspan="3" height="1"></td>
-			</tr>
-			<tr>
-				<td class="tableCellLabel">Latest Mission Entries</td>
-				<td>&nbsp;</td>
-				<td>
-					<input type="text" class="order" name="cpShowPostsNum" value="<?=$cpShowPostsNum;?>" maxlength="3" />
-				</td>
-			</tr>
-			<tr>
-				<td class="tableCellLabel">Latest Personal Logs</td>
-				<td>&nbsp;</td>
-				<td>
-					<input type="text" class="order" name="cpShowLogsNum" value="<?=$cpShowLogsNum;?>" maxlength="3" />
-				</td>
-			</tr>
-			<tr>
-				<td class="tableCellLabel">Latest News Items</td>
-				<td>&nbsp;</td>
-				<td>
-					<input type="text" class="order" name="cpShowNewsNum" value="<?=$cpShowNewsNum;?>" maxlength="3" />
-				</td>
-			</tr>
-			<tr>
-				<td colspan="3" height="15"></td>
-			</tr>
-			<tr>
-				<td colspan="3" class="fontMedium">
-					<b>Personalized Menu Items</b>
-				</td>
-			</tr>
-			
-			<?php
-					
-			for( $i=1; $i<11; $i++ )
-			{
-				$menu = "menu" . $i;
-				
-				echo "<tr>";
-					echo "<td class='tableCellLabel'>Menu Item #" . $i . "</td>";
-					echo "<td>&nbsp;</td>";
-					echo "<td>";
-						echo "<select name='" . $menu . "'>";
-							echo "<option value='0'>No Selection</option>";
-							
-							echo "<optgroup label='General'>";
-								foreach( $array1 as $key1 => $value1 )
-								{
-									if( $$menu == $value1[0] ) {
-										$selected = " selected";
-									} else {
-										$selected = "";
-									}
-									
-									echo "<option value='" . $value1[0] . "'" . $selected . ">";
-										echo ucwords( $value1[2] ) . " - " . $value1[1];
-									echo "</option>";
-								}
-							echo "</optgroup>";
-							
-							echo "<optgroup label='Admin'>";
-								foreach( $array2 as $key2 => $value2 )
-								{
-									if( $$menu == $value2[0] ) {
-										$selected = " selected";
-									} else {
-										$selected = "";
-									}
-									
-									echo "<option value='" . $value2[0] . "'" . $selected . ">";
-										echo ucwords( $value2[3] ) . " - " . $value2[1];
-									echo "</option>";
-								}
-							echo "</optgroup>";
-						echo "</select>";
-					echo "</td>";
-				echo "</tr>";
-			}
-			
-			?>
-			
-			<tr>
-				<td colspan="3" height="15"></td>
-			</tr>
-			<tr>
-				<td colspan="3">
-					<input type="image" src="<?=path_userskin;?>buttons/update.png" name="options" value="Update" class="button" />
-				</td>
-			</tr>
-		</table>
-		</form>
 		
-		<h1>Ranks</h1>
-		<h3>Current Rank Set</h3>
-		<img src="<?=$webLocation;?>images/ranks/<?=$sessionDisplayRank;?>/preview.png" border="0" alt="<?=$sessionDisplayRank;?>" />
+		<div id="container-1">
+			<ul>
+				<li><a href="#one"><span>Options</span></a></li>
+				<li><a href="#two"><span>Rank Sets</span></a></li>
+				<li><a href="#three"><span>Site Skins</span></a></li>
+			</ul>
 	
-		<h3>Other Available Rank Sets</h3>
-		<?
-			
-			foreach( $allowedRanksArray as $key => $value ) {
-				
-				if( $value != $sessionDisplayRank ) {
-					echo "<p><a href='" . $webLocation . "admin.php?page=user&sub=site&action=rank&changerank=" . trim( $value ) . "'>";
-					echo "<img src='" . $webLocation . "images/ranks/" . trim( $value ) . "/preview.png' alt='" . $value . "' border='0' class='image' />";
-					echo "</a>";
-	
-					/* make a note of which rankSet is the default */
-					if( $value == $rankSet ) {
-						echo "&nbsp;&nbsp;";
-						echo "<span class='yellow fontSmall'>* System Default</span>";
+			<div id="one" class="ui-tabs-container ui-tabs-hide">
+				<form method="post" action="<?=$webLocation;?>admin.php?page=user&sub=site&sec=1">
+				<table>
+					<tr>
+						<td colspan="3" class="fontMedium"><b>Show the following in the Control Panel?</b></td>
+					</tr>
+					<tr>
+						<td colspan="3" height="1"></td>
+					</tr>
+					<tr>
+						<td class="tableCellLabel">Latest Mission Entries</td>
+						<td>&nbsp;</td>
+						<td>
+							<input type="radio" id="showPostsY" name="cpShowPosts" value="y" <? if( $cpShowPosts == "y" ) { echo "checked"; } ?>/> <label for="showPostsY">Yes</label>
+							<input type="radio" id="showPostsN" name="cpShowPosts" value="n" <? if( $cpShowPosts == "n" ) { echo "checked"; } ?>/> <label for="showPostsN">No</label>
+						</td>
+					</tr>
+					<tr>
+						<td class="tableCellLabel">Latest Personal Logs</td>
+						<td>&nbsp;</td>
+						<td>
+							<input type="radio" id="showLogsY" name="cpShowLogs" value="y" <? if( $cpShowLogs == "y" ) { echo "checked"; } ?>/> <label for="showLogsY">Yes</label>
+							<input type="radio" id="showLogsN" name="cpShowLogs" value="n" <? if( $cpShowLogs == "n" ) { echo "checked"; } ?>/> <label for="showLogsN">No</label>
+						</td>
+					</tr>
+					<tr>
+						<td class="tableCellLabel">Latest News Items</td>
+						<td>&nbsp;</td>
+						<td>
+							<input type="radio" id="showNewsY" name="cpShowNews" value="y" <? if( $cpShowNews == "y" ) { echo "checked"; } ?>/> <label for="showNewsY">Yes</label>
+							<input type="radio" id="showNewsN" name="cpShowNews" value="n" <? if( $cpShowNews == "n" ) { echo "checked"; } ?>/> <label for="showNewsN">No</label>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="3" height="5"></td>
+					</tr>
+					<tr>
+						<td colspan="3" class="fontMedium">
+							<b>How many of the following should be shown in the Control Panel?</b>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="3" height="1"></td>
+					</tr>
+					<tr>
+						<td class="tableCellLabel">Latest Mission Entries</td>
+						<td>&nbsp;</td>
+						<td>
+							<input type="text" class="order" name="cpShowPostsNum" value="<?=$cpShowPostsNum;?>" maxlength="3" />
+						</td>
+					</tr>
+					<tr>
+						<td class="tableCellLabel">Latest Personal Logs</td>
+						<td>&nbsp;</td>
+						<td>
+							<input type="text" class="order" name="cpShowLogsNum" value="<?=$cpShowLogsNum;?>" maxlength="3" />
+						</td>
+					</tr>
+					<tr>
+						<td class="tableCellLabel">Latest News Items</td>
+						<td>&nbsp;</td>
+						<td>
+							<input type="text" class="order" name="cpShowNewsNum" value="<?=$cpShowNewsNum;?>" maxlength="3" />
+						</td>
+					</tr>
+					<tr>
+						<td colspan="3" height="15"></td>
+					</tr>
+					<tr>
+						<td colspan="3" class="fontMedium">
+							<b>Personalized Menu Items</b>
+						</td>
+					</tr>
+
+					<?php
+
+					for( $i=1; $i<11; $i++ )
+					{
+						$menu = "menu" . $i;
+
+						echo "<tr>";
+							echo "<td class='tableCellLabel'>Menu Item #" . $i . "</td>";
+							echo "<td>&nbsp;</td>";
+							echo "<td>";
+								echo "<select name='" . $menu . "'>";
+									echo "<option value='0'>No Selection</option>";
+
+									echo "<optgroup label='General'>";
+										foreach( $array1 as $key1 => $value1 )
+										{
+											if( $$menu == $value1[0] ) {
+												$selected = " selected";
+											} else {
+												$selected = "";
+											}
+
+											echo "<option value='" . $value1[0] . "'" . $selected . ">";
+												echo ucwords( $value1[2] ) . " - " . $value1[1];
+											echo "</option>";
+										}
+									echo "</optgroup>";
+
+									echo "<optgroup label='Admin'>";
+										foreach( $array2 as $key2 => $value2 )
+										{
+											if( $$menu == $value2[0] ) {
+												$selected = " selected";
+											} else {
+												$selected = "";
+											}
+
+											echo "<option value='" . $value2[0] . "'" . $selected . ">";
+												echo ucwords( $value2[3] ) . " - " . $value2[1];
+											echo "</option>";
+										}
+									echo "</optgroup>";
+								echo "</select>";
+							echo "</td>";
+						echo "</tr>";
 					}
-					
-					echo "</p>";
-				} else {
-				}
-			}
+
+					?>
+
+					<tr>
+						<td colspan="3" height="15"></td>
+					</tr>
+					<tr>
+						<td colspan="3">
+							<input type="image" src="<?=path_userskin;?>buttons/update.png" name="options" value="Update" class="button" />
+						</td>
+					</tr>
+				</table>
+				</form>
+			</div>
 			
-		?>
+			<div id="two" class="ui-tabs-container ui-tabs-hide">
+				<form method="post" action="<?=$webLocation;?>admin.php?page=user&sub=site&sec=2">
+				<table>
+					<tr>
+						<td>&nbsp;</td>
+						<td>&nbsp;</td>
+						<td>
+							<?php
 		
-		<h1>Skins</h1>
-		<h3>Current Skin</h3>
-		<h4><?=ucwords( $displaySkin );?></h4>
-		<p>
-			<img src="<?=$webLocation;?>skins/<?=$sessionDisplaySkin;?>/preview.jpg" alt="<?=$sessionDisplaySkin;?>" style="border: 2px solid #efefef;" />
-		</p>
+							/* break the string into an array */
+							$rankArray = explode( ",", $allowedRanks );
 		
-		<h3>Other Available Skins</h3>
-		<?
+							/* loop through the array */
+							foreach( $rankArray as $key => $value ) {
+		
+							?>
+							
+								<input type="radio" id="<?=$value;?>" name="rankSet" value="<?=$value;?>"<? if( $sessionDisplayRank == trim( $value ) ) { echo " checked"; } ?> />
+								<label for="<?=$value;?>"><img src="<?=$webLocation;?>images/ranks/<?=trim( $value );?>/preview.png" alt="" border="0" /></label><br />
+		
+							<?php } ?>
+							
+						</td>
+					</tr>
+					<tr>
+						<td colspan="3" height="25"></td>
+					</tr>
+					<tr>
+						<td colspan="3">
+							<input type="image" src="<?=path_userskin;?>buttons/update.png" name="action_rank" value="Update" class="button" />
+						</td>
+					</tr>
+				</table>
+				</form>
+			</div>
 			
-			foreach( $allowedSkinsArray as $key => $value ) {
+			<div id="three" class="ui-tabs-container ui-tabs-hide">
+				<form method="post" action="<?=$webLocation;?>admin.php?page=user&sub=site&sec=3">
+				<table>
+					<tr>
+						<td>&nbsp;</td>
+						<td>&nbsp;</td>
+						<td>
+							<?php
+		
+							/* break the string into an array */
+							$skinArray = explode( ",", $allowedSkins );
+		
+							/* loop through the array */
+							foreach( $skinArray as $key => $value ) {
+		
+							?>
+							
+								<input type="radio" id="<?=$value;?>" name="changeskin" value="<?=$value;?>"<? if( $sessionDisplaySkin == trim( $value ) ) { echo " checked"; } ?> />
+								<label for="<?=$value;?>"><img src="<?=$webLocation;?>skins/<?=trim( $value );?>/preview.jpg" alt="" border="0" style="border: 2px solid #efefef" class="image" /></label><br /><br />
+		
+							<?php } ?>
+							
+						</td>
+					</tr>
+					<tr>
+						<td colspan="3" height="25"></td>
+					</tr>
+					<tr>
+						<td colspan="3">
+							<input type="image" src="<?=path_userskin;?>buttons/update.png" name="action_skin" value="Update" class="button" />
+						</td>
+					</tr>
+				</table>
+				</form>
 				
-				if( $value != $sessionDisplaySkin ) {
-					echo "<h4>" . ucwords( $value ) . "</h4>";
-					echo "<a href='" . $webLocation . "admin.php?page=user&sub=site&action=skin&changeskin=" . trim( $value ) . "'>";
-					echo "<img src='" . $webLocation . "skins/" . trim( $value ) . "/preview.jpg' alt='" . $value . "' style='border: 2px solid #efefef;' class='image' />";
-					echo "</a>";
-				} else {
-				}
-			}
+				
+				
+				<? /* ?>
+				<h3>Current Skin</h3>
+				<h4><?=ucwords( $displaySkin );?></h4>
+				<p>
+					<img src="<?=$webLocation;?>skins/<?=$sessionDisplaySkin;?>/preview.jpg" alt="<?=$sessionDisplaySkin;?>" style="border: 2px solid #efefef;" />
+				</p>
+
+				<h3>Other Available Skins</h3>
+				<?
+
+					foreach( $allowedSkinsArray as $key => $value ) {
+
+						if( $value != $sessionDisplaySkin ) {
+							echo "<h4>" . ucwords( $value ) . "</h4>";
+							echo "<a href='" . $webLocation . "admin.php?page=user&sub=site&action=skin&changeskin=" . trim( $value ) . "'>";
+							echo "<img src='" . $webLocation . "skins/" . trim( $value ) . "/preview.jpg' alt='" . $value . "' style='border: 2px solid #efefef;' class='image' />";
+							echo "</a>";
+						} else {
+						}
+					}
+				*/
+				?>
 			
-		?>
+			</div>
+		</div>
+	
 	</div>
 	
 <? } else { errorMessage( "site options" ); } ?>
