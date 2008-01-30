da712caf04b9e865da1dbdd1403a52f442845457
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-01-30 15:48:14 +0000

    adding UTF-8 character set declaration and english language declaration

--- trunk/skins/default/header.php
+++ trunk/skins/default/header.php
@@ -1,16 +1,14 @@
 <?php
 
 /**
-This skin is the property of its owner and should not be duplicated or
-reproduced with the express written consent of the author. Edits to this skin
-are permissible if the original credits stay intact.
+Edits to this skin are permissible if the original credits stay intact.
 
 Author: David VanScott [ davidv@anodyne-productions.com ]
 File: skins/default/header.php
 Purpose: The header file that the system calls for the template
 
-Skin Version: 2.1
-Last Modified: 2007-12-22 2213 EST
+Skin Version: 2.5
+Last Modified: 2008-01-30 1045 EST
 **/
 
 $path = dirname( __FILE__ ); /* absolute path of the current file (header.php) */
@@ -26,14 +24,17 @@ define( 'SKIN_PATH', $path[$pathElement1] . '/' . $path[$pathElement2] . '/' );
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
 		<title><?=$shipPrefix . " " . $shipName;?></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		
 		<link rel="stylesheet" href="<?=$webLocation . SKIN_PATH;?>style.css" type="text/css" />
+		
 		<script type="text/javascript">
 			<? include_once( "framework/functionsJavascript.js" ); ?>
 			
-			if( document.images )
+			if(document.images)
 			{
 				preload_image_object = new Image();
 				
@@ -42,7 +43,7 @@ define( 'SKIN_PATH', $path[$pathElement1] . '/' . $path[$pathElement2] . '/' );
 				image_url[1] = "<?=SKIN_PATH;?>buttons/button-hover.png";
 				
 				var i = 0;
-				for( i = 0; i < image_url.length; i++ )
+				for(i = 0; i < image_url.length; i++)
 				{
 					preload_image_object.src = image_url[i];
 				}
@@ -51,7 +52,5 @@ define( 'SKIN_PATH', $path[$pathElement1] . '/' . $path[$pathElement2] . '/' );
 	</head>
 	<body>
 		<div id="headerTopSpacer"></div>
-		<div id="header">
-			<!-- <img src="<?=SKIN_PATH;?>images/header.png" alt="SMS 2" style="padding-left:2.5em;" /> -->
-		</div>
+		<div id="header"></div>
 		<div id="container">
