b2605e58fb815cffa10f919c98a67e22304277ec
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-02-05 23:07:43 +0000

    playing around with the new activation page and updating the edit, add and delete styles to not use paddings or font weights

--- trunk/admin/manage/activate_new.php
+++ trunk/admin/manage/activate_new.php
@@ -1,18 +1,31 @@
 <?php
 
+/* get pending users */
+$getPendingUsers = "SELECT crew.crewid, crew.firstName, crew.lastName, position.positionName ";
+$getPendingUsers.= "FROM sms_crew AS crew, sms_positions AS position WHERE ";
+$getPendingUsers.= "crew.positionid = position.positionid AND crewType = 'pending'";
+$getPendingUsersResult = mysql_query( $getPendingUsers );
+$countPendingUsers = mysql_num_rows( $getPendingUsersResult );
+
 ?>
 
 <script type="text/javascript">
 	$(document).ready(function() {
 		$('#container-1 > ul').tabs();
+		$('.zebra tr:odd').addClass('alt');
 		
-		$('.zebra tr').mouseover(function() {
-			$(this).addClass('over');
-		})
-		.mouseout(function() {
-			$(this).removeClass('over');
+		$("a[rel*=facebox]").click(function() {
+			var id = $(this).attr("myID");
+			var type = $(this).attr("myType");
+			var action = $(this).attr("myAction");
+			
+			jQuery.facebox(function() {
+				jQuery.get('admin/ajax/activate_' + type + "_" + action + '.php?id=' + id, function(data) {
+					jQuery.facebox(data);
+				});
+			});
+			return false;
 		});
-		$('.zebra tr:even').addClass('alt');
 	});
 </script>
 
@@ -21,7 +34,7 @@
 
 	<div id="container-1">
 		<ul>
-			<li><a href="#one"><span>Users</span></a></li>
+			<li><a href="#one"><span>Users (<?=$countPendingUsers;?>)</span></a></li>
 			<li><a href="#two"><span>Mission Posts</span></a></li>
 			<li><a href="#three"><span>Personal Logs</span></a></li>
 			<li><a href="#four"><span>News Items</span></a></li>
@@ -34,27 +47,30 @@
 			<table class="zebra" cellpadding="3" cellspacing="0">
 				<thead>
 					<tr class="fontMedium">
-						<th width="30%">Name</th>
-						<th width="30%">Position</th>
-						<th></th>
-						<th></th>
-						<th></th>
+						<th width="35%">Name</th>
+						<th width="35%">Position</th>
+						<th width="10%"></th>
+						<th width="10%"></th>
+						<th width="10%"></th>
 					</tr>
 				</thead>
-				<tr>
-					<td>Jean-Luc Picard</td>
-					<td>Commanding Officer</td>
-					<td>View Bio</td>
-					<td><a href="#">Deny</a></td>
-					<td><a href="#">Approve</a></td>
+				
+				<?php
+				
+				/* loop through the results and fill the form */
+				while( $pendingUser = mysql_fetch_assoc( $getPendingUsersResult ) ) {
+					extract( $pendingUser, EXTR_OVERWRITE );
+				
+				?>
+				<tr class="fontNormal">
+					<td><? printText( $pendingUser['firstName'] . " " . $pendingUser['lastName'] ); ?></td>
+					<td><? printText( $pendingUser['positionName'] ); ?></td>
+					<td align="center"><a href="<?=$webLocation;?>index.php?page=bio&crew=<?=$pendingUser['crewid'];?>"><b>View Bio</b></a></td>
+					<td align="center"><a href="#" class="delete" rel="facebox" myID="<?=$pendingUser['crewid'];?>" myType="user" myAction="reject"><b>Reject</b></a></td>
+					<td align="center"><a href="#" class="add" rel="facebox" myID="<?=$pendingUser['crewid'];?>" myType="user" myAction="accept"><b>Accept</b></a></td>
 				</tr>
-				<tr>
-					<td>William Riker</td>
-					<td>Executive Officer</td>
-					<td>View Bio</td>
-					<td><a href="#">Deny</a></td>
-					<td><a href="#">Approve</a></td>
-				</tr>
+				<?php } ?>
+				
 			</table>
 		</div>
 		
