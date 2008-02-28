c783a683cbe152b780c03ab78989ecd3c105ce4d
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-02-28 21:33:17 +0000

    starting to make some updates to cobalt

--- trunk/skins/cobalt/header.php
+++ trunk/skins/cobalt/header.php
@@ -1,16 +1,14 @@
 <?php
 
 /**
-This skin is the property of its owner and should not be duplicated or
-reproduced with the express written consent of the author. Edits to this skin
-are permissible if the original credits stay intact.
+Edits to this skin are permissible if the original credits stay intact.
 
 Author: David VanScott [ davidv@anodyne-productions.com ]
 File: skins/cobalt/header.php
 Purpose: The header file that the system calls for the template
 
-Skin Version: 2.0
-Last Modified: 2007-12-26 0914 EST
+Skin Version: 2.1
+Last Modified: 2008-02-28 1632 EST
 **/
 
 $path = dirname( __FILE__ ); /* absolute path of the current file (header.php) */
@@ -26,29 +24,25 @@ define( 'SKIN_PATH', $path[$pathElement1] . '/' . $path[$pathElement2] . '/' );
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
 		<title><?=$shipPrefix . " " . $shipName;?></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		
 		<link rel="stylesheet" href="<?=$webLocation . SKIN_PATH;?>style.css" type="text/css" />
+		
 		<script type="text/javascript">
 			<? include_once( "framework/functionsJavascript.js" ); ?>
-			
-			if( document.images )
-			{
-				preload_image_object = new Image();
-				
-				image_url = new Array();
-				image_url[0] = "<?=SKIN_PATH;?>buttons/button-off.png";
-				image_url[1] = "<?=SKIN_PATH;?>buttons/button-hover.png";
-				
-				var i = 0;
-				for( i = 0; i < image_url.length; i++ )
-				{
-					preload_image_object.src = image_url[i];
-				}
-			}
 		</script>
 	</head>
 	<body>
+		<script type="text/javascript">
+			$(document).ready(function() {
+				$.preload( '.button img', {
+				    find:'-off.png',
+				    replace:'-hover.png'
+				});
+			});
+		</script>
 		<div id="container">
 			<div id="headerTopSpacer"></div>
