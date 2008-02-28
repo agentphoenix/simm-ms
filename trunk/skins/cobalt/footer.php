6c210fa624c7ee2b53a2ba5eccd0929f147747f9
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-02-28 22:45:14 +0000

    updating cobalt

--- trunk/skins/cobalt/footer.php
+++ trunk/skins/cobalt/footer.php
@@ -1,16 +1,14 @@
 <?php
 
 /**
-This is a necessary system file. Do not modify this page unless you are highly
-knowledgeable as to the structure of the system. Modification of this file may
-cause the system to no longer function.
+Edits to this skin are permissible if the original credits stay intact.
 
-Author: David VanScott [ david.vanscott@gmail.com ]
-File: skins/default/footer.php
+Author: David VanScott [ davidv@anodyne-productions.com ]
+File: skins/cobalt/footer.php
 Purpose: The footer file that the system calls in for the template
 
-System Version: 26.0
-Last Modified: 2007-08-22 1004 EST
+Skin Version: 2.1
+Last Modified: 2008-02-28 1635 EST
 **/
 
 ?>
@@ -19,8 +17,10 @@ Last Modified: 2007-08-22 1004 EST
 	
 	<!-- Please do not edit this. If you wish to add credits, you can do so through the Site Messages panel -->
 	<div class="footer">
-		Powered by SMS 2 &nbsp; | &nbsp;
-		Copyright &copy; Anodyne Productions 2005 - <?=date('Y');?> &nbsp; | &nbsp;
+		Powered by SMS 2
+		&nbsp; | &nbsp;
+		Copyright &copy; <a href="http://www.anodyne-productions.com/" target="_blank">Anodyne Productions</a> 2005 - <?=date('Y');?>
+		&nbsp; | &nbsp;
 		<a href="<?=$webLocation;?>index.php?page=credits">Site Credits</a>
 	</div>
 	
