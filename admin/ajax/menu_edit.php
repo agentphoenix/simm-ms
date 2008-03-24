9e1a62d7fe35adb9c758cbc5f4202165361aaf1c
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-03-24 02:56:00 +0000

    completing work on the menu item edit AJAX page

--- admin/ajax/menu_edit.php
+++ admin/ajax/menu_edit.php
@@ -27,18 +27,142 @@ if(in_array("x_menu", $sessionAccess))
 	$pendingArray = mysql_fetch_assoc( $getR );
 
 ?>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('a#show_adv').click(function() {
+				$('#advanced').toggle(75);
+				return false;
+			});
+		});
+	</script>
+	
 	<h2>Edit Menu Item</h2>
-	<p>Use the fields below to edit the menu item. If you want more advanced options, you can show the advanced options below. <strong class='orange'>Please use extreme caution when editing menu items. Incorrect modification can cause you to not be able to access the menu items any more!</strong></p>
+	<p>Use the fields below to edit the menu item. If you want more options (menu availability, section, category, authentication, or user access control), you can use the <strong>advanced options</strong> link below to show the remaining menu item options. <strong class='orange'>Use extreme caution when editing menu items. Incorrect modification can cause you to not be able to access the menu items any more!</strong></p>
 	
 	<hr size="1" width="100%" />
 	
 	<form method="post" action="">
-		<h3><? printText( $pendingArray['postTitle'] );?></h3>
-		<h4>By <? displayAuthors( $pendingArray['postid'], 'noLink' );?></h4>
+		<table class="hud_guts">
+			<tr>
+				<td class="hudLabel">Title</td>
+				<td></td>
+				<td><input type="text" class="image" name="menuTitle" value="<?=$pendingArray['menuTitle'];?>" /></td>
+			</tr>
+			
+			<tr>
+				<td colspan="3" height="15"></td>
+			</tr>
+			
+			<tr>
+				<td class="hudLabel">Link URL</td>
+				<td></td>
+				<td><input type="text" class="image" name="menuLink" value="<?=$pendingArray['menuLink'];?>" /></td>
+			</tr>
+			<tr>
+				<td class="hudLabel">Link Type</td>
+				<td></td>
+				<td>
+					<select name="menuLinkType">
+						<option value="onsite"<? if( $pendingArray['menuLinkType'] == "onsite" ) { echo " selected"; } ?>>Onsite</option>
+						<option value="offsite"<? if( $pendingArray['menuLinkType'] == "offsite" ) { echo " selected"; } ?>>Offsite</option>
+					</select>
+				</td>
+			</tr>
+			
+			<tr>
+				<td colspan="3" height="15"></td>
+			</tr>
+			
+			<tr>
+				<td class="hudLabel">Group</td>
+				<td></td>
+				<td><input type="text" class="order" name="menuGroup" size="3" value="<?=$pendingArray['menuGroup'];?>" /></td>
+			</tr>
+			<tr>
+				<td class="hudLabel">Order</td>
+				<td></td>
+				<td><input type="text" class="order" name="menuOrder" size="3" value="<?=$pendingArray['menuOrder'];?>" /></td>
+			</tr>
+		</table>
 		
-		<div class="overflow"><? printText( $pendingArray['postContent'] );?></div>
+		<p><a href="#" id="show_adv" class="fontNormal"><strong>Advanced Options &raquo;</strong></a></p>
 		
-		<p></p>
+		<div id="advanced" style="display:none;">
+			<table class="hud_guts">
+				<tr>
+					<td class="hudLabel">Status</td>
+					<td></td>
+					<td>
+						<input type="radio" name="menuAvailability" id="maOn" value="on"<? if( $pendingArray['menuAvailability'] == "on" ) { echo " checked"; } ?>/><label for="maOn">On</label>
+						<input type="radio" name="menuAvailability" id="maOff" value="off"<? if( $pendingArray['menuAvailability'] == "off" ) { echo " checked"; } ?>/><label for="maOff">Off</label>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="3" height="15"></td>
+				</tr>
+				<tr>
+					<td class="hudLabel">Category</td>
+					<td></td>
+					<td>
+						<select name="menuCat">
+							<option value="main"<? if( $pendingArray['menuCat'] == "main" ) { echo " selected"; } ?>>Main Navigation</option>
+							<option value="general"<? if( $pendingArray['menuCat'] == "general" ) { echo " selected"; } ?>>General Menus</option>
+							<option value="admin"<? if( $pendingArray['menuCat'] == "admin" ) { echo " selected"; } ?>>Admin Menus</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td class="hudLabel">Section</td>
+					<td></td>
+					<td>
+						<select name="menuMainSec">
+							<optgroup label="Main Navigation">
+								<option value=""<? if( $pendingArray['menuMainSec'] == "" ) { echo " selected"; } ?>>Main Navigation</option>
+							</optgroup>
+							<optgroup label="General Menus">
+								<option value="main"<? if( $pendingArray['menuMainSec'] == "main" ) { echo " selected"; } ?>>Main</option>
+								<option value="personnel"<? if( $pendingArray['menuMainSec'] == "personnel" ) { echo " selected"; } ?>>Personnel</option>
+								<option value="ship"<? if( $pendingArray['menuMainSec'] == "ship" ) { echo " selected"; } ?>><?=ucfirst( $simmType );?></option>
+								<option value="simm"<? if( $pendingArray['menuMainSec'] == "simm" ) { echo " selected"; } ?>>Simm</option>
+							</optgroup>
+							<optgroup label="Admin Menus">
+								<option value="post"<? if( $pendingArray['menuMainSec'] == "post" ) { echo " selected"; } ?>>Post</option>
+								<option value="manage"<? if( $pendingArray['menuMainSec'] == "manage" ) { echo " selected"; } ?>>Manage</option>
+								<option value="reports"<? if( $pendingArray['menuMainSec'] == "reports" ) { echo " selected"; } ?>>Reports</option>
+								<option value="user"<? if( $pendingArray['menuMainSec'] == "user" ) { echo " selected"; } ?>>User</option>
+							</optgroup>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="3" height="15"></td>
+				</tr>
+			
+				<tr>
+					<td class="hudLabel">Requires Login?</td>
+					<td></td>
+					<td>
+						<input type="radio" id="menuLoginY" name="menuLogin" value="y" <? if( $pendingArray['menuLogin'] == "y" ) { echo "checked"; } ?>/><label for="menuLoginY">Yes</label>
+						<input type="radio" id="menuLoginN" name="menuLogin" value="n" <? if( $pendingArray['menuLogin'] == "n" ) { echo "checked"; } ?>/><label for="menuLoginN">No</label>
+					</td>
+				</tr>
+			
+				<tr>
+					<td colspan="3" height="15"></td>
+				</tr>
+				
+				<?php if($pendingArray['menuCat'] == 'admin') {} else { ?>
+				<tr>
+					<td colspan="3" class="fontNormal yellow bold">Main navigation and general menus do not use the access control system. Changing this field for either category will not affect anything.</td>
+				</tr>	
+				<?php } ?>
+				<tr>
+					<td class="hudLabel">Access Code</td>
+					<td></td>
+					<td><input type="text" class="title" name="menuAccess" value="<?=$pendingArray['menuAccess'];?>" /></td>
+				</tr>
+			</table><br /><br />
+		</div>
 		
 		<div>
 			<input type="hidden" name="action_id" value="<?=$pendingArray['menuid'];?>" />
