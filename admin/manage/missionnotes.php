9a56ea2a61d4ef12be234cbc62b2d74c0bb7a264
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-03-27 22:47:47 +0000

    mission notes still weren't using the new tabs

--- admin/manage/missionnotes.php
+++ admin/manage/missionnotes.php
@@ -5,12 +5,12 @@ This is a necessary system file. Do not modify this page unless you are highly
 knowledgeable as to the structure of the system. Modification of this file may
 cause SMS to no longer function.
 
-Author: David VanScott [ anodyne.sms@gmail.com ]
+Author: David VanScott [ davidv@anodyne-productions.com ]
 File: admin/manage/missionnotes.php
 Purpose: Page that moderates the notes for each mission
 
-System Version: 2.5.0
-Last Modified: 2007-04-17 1406 EST
+System Version: 2.6.0
+Last Modified: 2008-03-27 1847 EST
 **/
 
 /* access check */
@@ -19,42 +19,83 @@ if( in_array( "m_missionnotes", $sessionAccess ) ) {
 	/* set the page class and vars */
 	$pageClass = "admin";
 	$subMenuClass = "manage";
-	$actionUpdate = $_POST['action_update_x'];
-	$sec = $_GET['sec'];
+	$result = false;
+	$query = false;
 	
-	/* set the default section */
-	if( !$sec ) {
-		$sec = "current";
+	if(isset($_GET['s']) && is_numeric($_GET['s']))
+	{
+		$sec = $_GET['s'];
+	}
+	else
+	{
+		$sec = 1;
 	}
 	
-	/* if the POST action is update */
-	if( $actionUpdate ) {
+	if(isset($_POST['action_update_x']))
+	{
+		if(is_numeric($_POST['missionid']))
+		{
+			$id = $_POST['missionid'];
+		}
 		
-		/* define the POST variables */
-		$missionid = $_POST['missionid'];
-		$missionNotes = addslashes( $_POST['missionNotes'] );
+		$update = "UPDATE sms_missions SET missionNotes = %s WHERE missionid = $id LIMIT 1";
 		
-		/* do the update query */
-		$updateNotes = "UPDATE sms_missions SET ";
-		$updateNotes.= "missionNotes = '$missionNotes' WHERE missionid = '$missionid' LIMIT 1";
-		$result = mysql_query( $updateNotes );
+		$query = sprintf(
+			$update,
+			escape_string($_POST['missionNotes'])
+		);
+		
+		$result = mysql_query( $query );
 		
 		/* optimize the table */
 		optimizeSQLTable( "sms_missions" );
-		
-		/* strip the slashes from the vars */
-		$missionNotes = stripslashes( $missionNotes );
-		
 	}
 
-?>
+$mission_array = array(
+	'current' => array(),
+	'completed' => array(),
+	'upcoming' => array()
+);
 
+$missions = "SELECT * FROM sms_missions ORDER BY missionOrder DESC";
+$missionsResult = mysql_query( $missions );
+
+while( $notes = mysql_fetch_array( $missionsResult ) ) {
+	extract( $notes, EXTR_OVERWRITE );
+	
+	$mission_array[$missionStatus][] = array('id' => $missionid, 'title' => $missionTitle, 'order' => $missionOrder, 'notes' => $missionNotes);
+	
+}
+
+$disable = array();
+
+if(count($mission_array['current']) == 0) {
+	$disable[] = 1;
+}
+
+if(count($mission_array['completed']) == 0) {
+	$disable[] = 2;
+}
+
+if(count($mission_array['upcoming']) == 0) {
+	$disable[] = 3;
+}
+
+$disable_string = implode(",", $disable);
+
+?>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('#container-1 > ul').tabs(<?php echo $sec; ?>, { disabled: [<?php echo $disable_string; ?>] });
+		});
+	</script>
+	
 	<div class="body">
 	
 		<?
 		
 		$check = new QueryCheck;
-		$check->checkQuery( $result, $updateNotes );
+		$check->checkQuery( $result, $query );
 		
 		if( !empty( $check->query ) ) {
 			$check->message( "mission note", "update" );
@@ -69,138 +110,106 @@ if( in_array( "m_missionnotes", $sessionAccess ) ) {
 		mission posting pages (single or joint) as well as the Mission Notes link in the Post section of the menu.
 		<br /><br />
 		
-		<div id="subnav">
+		<div id="container-1">
 			<ul>
-				<li <? if( $sec == "current" ) { echo "id='current'"; } ?>><a href="<?=$webLocation;?>admin.php?page=manage&sub=missionnotes&sec=current">Current Mission</a></li>
-				<li <? if( $sec == "completed" ) { echo "id='current'"; } ?>><a href="<?=$webLocation;?>admin.php?page=manage&sub=missionnotes&sec=completed">Completed Missions</a></li>
-				<li <? if( $sec == "upcoming" ) { echo "id='current'"; } ?>><a href="<?=$webLocation;?>admin.php?page=manage&sub=missionnotes&sec=upcoming">Upcoming Missions</a></li>				
+				<li><a href="#one"><span>Current Mission</span></a></li>
+				<li><a href="#two"><span>Completed Missions</span></a></li>
+				<li><a href="#three"><span>Upcoming Missions</span></a></li>
 			</ul>
-		</div>
 	
-		<div class="tabcontainer">
-		
-		<? if( $sec == "current" ) { ?>
-		<br /><br />
-		<table>
-			<?
-		
-			$missions = "SELECT missionid, missionTitle, missionNotes ";
-			$missions.= "FROM sms_missions WHERE missionStatus = '$sec' ";
-			$missions.= "ORDER BY missionOrder DESC";
-			$missionsResult = mysql_query( $missions );
+			<div id="one" class="ui-tabs-container ui-tabs-hide">
+				<table>
+					<?php foreach($mission_array['current'] as $k1 => $v1) { ?>
+					<form method="post" action="<?=$webLocation;?>admin.php?page=manage&sub=missionnotes&s=1">
+						<tr>
+							<td class="tableCellLabel">
+								<? printText( $v1['title'] );?>
+								<input type="hidden" name="missionid" value="<?=$v1['id'];?>" />
+							</td>
+							<td>&nbsp;</td>
+							<td>
+								<textarea name="missionNotes" rows="10" class="wideTextArea"><?=stripslashes( $v1['notes'] );?></textarea>
+							</td>
+						</tr>
+						<tr>
+							<td colspan="3" height="15"></td>
+						</tr>
+						<tr>
+							<td colspan="2"></td>
+							<td align="right">
+								<input type="image" src="<?=path_userskin;?>buttons/update.png" name="action_update" class="button" value="Update" />
+							</td>
+						</tr>
+						<tr>
+							<td colspan="3" height="10"></td>
+						</tr>
+					</form>
+					<?php } ?>
+				</table>
+			</div>
 			
-			while( $notes = mysql_fetch_array( $missionsResult ) ) {
-				extract( $notes, EXTR_OVERWRITE );
+			<div id="two" class="ui-tabs-container ui-tabs-hide">
+				<table>
+					<?php foreach($mission_array['completed'] as $k2 => $v2) { ?>
+					<form method="post" action="<?=$webLocation;?>admin.php?page=manage&sub=missionnotes&s=2">
+						<tr>
+							<td class="tableCellLabel">
+								<? printText( $v2['title'] );?>
+								<input type="hidden" name="missionid" value="<?=$v2['id'];?>" />
+							</td>
+							<td>&nbsp;</td>
+							<td>
+								<textarea name="missionNotes" rows="10" class="wideTextArea"><?=stripslashes( $v2['notes'] );?></textarea>
+							</td>
+						</tr>
+						<tr>
+							<td colspan="3" height="15"></td>
+						</tr>
+						<tr>
+							<td colspan="2"></td>
+							<td align="right">
+								<input type="image" src="<?=path_userskin;?>buttons/update.png" name="action_update" class="button" value="Update" />
+							</td>
+						</tr>
+						<tr>
+							<td colspan="3" height="10"></td>
+						</tr>
+					</form>
+					<?php } ?>
+				</table>
+			</div>
 			
-			?>
-			<form method="post" action="<?=$webLocation;?>admin.php?page=manage&sub=missionnotes&sec=<?=$sec;?>">
-			<tr>
-				<td class="tableCellLabel">
-					<? printText( $missionTitle );?>
-					<input type="hidden" name="missionid" value="<?=$missionid;?>" />
-				</td>
-				<td>&nbsp;</td>
-				<td>
-					<textarea name="missionNotes" rows="10" class="wideTextArea"><?=stripslashes( $missionNotes );?></textarea>
-				</td>
-			</tr>
-			<tr>
-				<td colspan="3" height="15"></td>
-			</tr>
-			<tr>
-				<td colspan="2"></td>
-				<td align="right">
-					<input type="image" src="<?=path_userskin;?>buttons/update.png" name="action_update" class="button" value="Update" />
-				</td>
-			</tr>
-			</form>
-		<? } ?>
-		</table>
-		
-		<? } if( $sec == "completed" ) { ?>
-		<br /><br />
-		<table>
-			<?
-		
-			$missions = "SELECT missionid, missionTitle, missionNotes ";
-			$missions.= "FROM sms_missions WHERE missionStatus = '$sec' ";
-			$missions.= "ORDER BY missionOrder DESC";
-			$missionsResult = mysql_query( $missions );
-			
-			while( $notes = mysql_fetch_array( $missionsResult ) ) {
-				extract( $notes, EXTR_OVERWRITE );
-			
-			?>
-			<form method="post" action="<?=$webLocation;?>admin.php?page=manage&sub=missionnotes&sec=<?=$sec;?>">
-			<tr>
-				<td class="tableCellLabel">
-					<? printText( $missionTitle );?>
-					<input type="hidden" name="missionid" value="<?=$missionid;?>" />
-				</td>
-				<td>&nbsp;</td>
-				<td>
-					<textarea name="missionNotes" rows="10" class="wideTextArea"><?=stripslashes( $missionNotes );?></textarea>
-				</td>
-			</tr>
-			<tr>
-				<td colspan="3" height="15"></td>
-			</tr>
-			<tr>
-				<td colspan="2"></td>
-				<td align="right">
-					<input type="image" src="<?=path_userskin;?>buttons/update.png" name="action_update" class="button" value="Update" />
-				</td>
-			</tr>
-			<tr>
-				<td colspan="3" height="25"></td>
-			</tr>
-			</form>
-		<? } ?>
-		</table>
-		
-		<? } if( $sec == "upcoming" ) { ?>
-		<br /><br />
-		<table>
-			<?
-		
-			$missions = "SELECT missionid, missionTitle, missionNotes ";
-			$missions.= "FROM sms_missions WHERE missionStatus = '$sec' ";
-			$missions.= "ORDER BY missionOrder DESC";
-			$missionsResult = mysql_query( $missions );
-			
-			while( $notes = mysql_fetch_array( $missionsResult ) ) {
-				extract( $notes, EXTR_OVERWRITE );
-			
-			?>
-			<form method="post" action="<?=$webLocation;?>admin.php?page=manage&sub=missionnotes&sec=<?=$sec;?>">
-			<tr>
-				<td class="tableCellLabel">
-					<? printText( $missionTitle );?>
-					<input type="hidden" name="missionid" value="<?=$missionid;?>" />
-				</td>
-				<td>&nbsp;</td>
-				<td>
-					<textarea name="missionNotes" rows="10" class="wideTextArea"><?=stripslashes( $missionNotes );?></textarea>
-				</td>
-			</tr>
-			<tr>
-				<td colspan="3" height="15"></td>
-			</tr>
-			<tr>
-				<td colspan="2"></td>
-				<td align="right">
-					<input type="image" src="<?=path_userskin;?>buttons/update.png" name="action_update" class="button" value="Update" />
-				</td>
-			</tr>
-			<tr>
-				<td colspan="3" height="25"></td>
-			</tr>
-			</form>
-		<? } ?>
-		</table>
-		<? } ?>
-		
-		</div> <!-- close the tab container -->
+			<div id="three" class="ui-tabs-container ui-tabs-hide">
+				<table>
+					<?php foreach($mission_array['upcoming'] as $k3 => $v3) { ?>
+					<form method="post" action="<?=$webLocation;?>admin.php?page=manage&sub=missionnotes&s=3">
+						<tr>
+							<td class="tableCellLabel">
+								<? printText( $v3['title'] );?>
+								<input type="hidden" name="missionid" value="<?=$v3['id'];?>" />
+							</td>
+							<td>&nbsp;</td>
+							<td>
+								<textarea name="missionNotes" rows="10" class="wideTextArea"><?=stripslashes( $v3['notes'] );?></textarea>
+							</td>
+						</tr>
+						<tr>
+							<td colspan="3" height="15"></td>
+						</tr>
+						<tr>
+							<td colspan="2"></td>
+							<td align="right">
+								<input type="image" src="<?=path_userskin;?>buttons/update.png" name="action_update" class="button" value="Update" />
+							</td>
+						</tr>
+						<tr>
+							<td colspan="3" height="10"></td>
+						</tr>
+					</form>
+					<?php } ?>
+				</table>
+			</div>
+		</div>
 		
 	</div>
 
