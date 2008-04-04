2892d7df60acc3d95637c55d56047f3a97a29abc
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-04-04 18:50:59 +0000

    working on the database edit AJAX page

--- trunk/admin/ajax/database_edit.php
+++ trunk/admin/ajax/database_edit.php
@@ -15,88 +15,143 @@ if(in_array("m_database1", $sessionAccess) || in_array("m_database2",$sessionAcc
 	include_once('../../framework/functionsGlobal.php');
 	include_once('../../framework/functionsUtility.php');
 	
+	if(in_array("m_database2", $sessionAccess))
+	{
+		$depts = "SELECT * FROM sms_departments WHERE deptDisplay = 'y'";
+		$deptsR = mysql_query($depts);
+	}
+	elseif(in_array("m_database1", $sessionAccess))
+	{
+		$depts = "SELECT crew.positionid, position.positionDept, dept.deptName, dept.deptColor FROM ";
+		$depts.= "sms_crew AS crew, sms_positions AS position, sms_departments AS dept WHERE ";
+		$depts.= "crew.crewid = '$_SESSION[sessionCrewid]' AND crew.positionid = position.positionid LIMIT 1";
+		$deptsR = mysql_query($depts);
+	}
+	
 	if(isset($_GET['id']) && is_numeric($_GET['id']))
 	{
 		$id = $_GET['id'];
 	}
 	
 	/* get the data */
-	$get = "SELECT * FROM sms_menu_items WHERE menuid = $id LIMIT 1";
+	$get = "SELECT * FROM sms_database WHERE dbid = $id LIMIT 1";
 	$getR = mysql_query( $get );
 	$pendingArray = mysql_fetch_assoc( $getR );
 
 ?>
 
 	<h2>Edit Database Entry</h2>
-	<p>Use the fields below to provide details for your new mission. <strong class="yellow">Note:</strong> Mission images <strong>must</strong> be located in the <em>images/missionimages</em> directory. All you need to do is specify the image in that directory.</p>
-	<br />
+	<p>Use the fields below to create a new database entry. <strong class="yellow">Note:</strong> For off-site URL forwarding entries, give the full URL (e.g. http://www.something.com/), for on-site URL forwarding entries only give what comes after the location of SMS (e.g. index.php?page=manifest). For reference with on-site entries, your web location is: <strong><?php echo WEBLOC; ?></strong>.</p>
+	<p>
+		<em><sup>&dagger;</sup>Applies only to on-site &amp; off-site entries</em><br />
+		<em><sup>&Dagger;</sup>Applies only to entries</em>
+	</p><br />
 
-<form method="post" action="">
-<table cellpadding="0" cellspacing="3">
-	<tr>
-		<td>
-			<span class="fontNormal"><b>Order</b></span><br />
-			<input type="text" class="order" name="dbOrder" maxlength="4" value="99" />
-		</td>
-		<td>
-			<span class="fontNormal"><b>Display?</b></span><br />
-			<select name="dbDisplay">
-				<option value="y">Yes</option>
-				<option value="n">No</option>
-			</select>
-		</td>
-		<td rowspan="5" valign="top" align="center" width="5">&nbsp;</td>
-		<td rowspan="5" valign="top" align="center" width="70%">
-			<span class="fontNormal"><b>Content</b></span><br />
-			<span class="fontSmall">* used only for database entries, not URL forwarding</span><br />
-			<textarea name="dbContent" rows="12" class="desc"></textarea>
-		</td>
-	</tr>
-	<tr>
-		<td colspan="2">
-			<span class="fontNormal"><b>Title</b></span><br />
-			<input type="text" class="name" name="dbTitle" maxlength="100" />
-		</td>
-	</tr>
-	<tr>
-		<td colspan="2">
-			<span class="fontNormal"><b>Short Description</b></span><br />
-			<input type="text" class="name" name="dbDesc" />
-		</td>
-	</tr>
-	<tr>
-		<td colspan="2">
-			<span class="fontNormal"><b>Entry Type</b></span><br />
-			<select name="dbType">
-				<option value="onsite">URL Forward (On-Site)</option>
-				<option value="offsite">URL Forward (Off-Site)</option>
-				<option value="entry">Database Entry</option>
-			</select>
-		</td>
-	</tr>
-	<tr>
-		<td colspan="2">
-			<span class="fontNormal"><b>URL</b></span><br />
-			<span class="fontSmall">* used only for URL forwarding entries</span><br />
-			<input type="text" class="name" name="dbURL" maxlength="255" />
-		</td>
-	</tr>
-	<tr>
-		<td colspan="2"></td>
-		<td align="center">&nbsp;</td>
-		<td align="center">
-			<input type="image" src="<?=path_userskin;?>buttons/create.png" class="button" name="action_create" value="Create" />
-		</td>
-	</tr>
-</table>
+	<form method="post" action="">
+		<table class="hud_guts" cellpadding="3">
+			<tr>
+				<td class="hudLabel">Title</td>
+				<td></td>
+				<td><input type="text" class="image" name="dbTitle" value="<?=$pendingArray['dbTitle'];?>" /></td>
+			</tr>
+			
+			<tr>
+				<td colspan="3" height="15"></td>
+			</tr>
+			
+			<tr>
+				<td class="hudLabel">Type</td>
+				<td></td>
+				<td>
+					<select name="dbType">
+						<option value="entry"<? if($pendingArray['dbType'] == "entry") { echo " selected"; } ?>>Database Entry</option>
+						<option value="onsite"<? if($pendingArray['dbType'] == "onsite") { echo " selected"; } ?>>URL Forward (On-Site)</option>
+						<option value="offsite"<? if($pendingArray['dbType'] == "offsite") { echo " selected"; } ?>>URL Forward (Off-Site)</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td class="hudLabel">URL<sup>&dagger;</sup></td>
+				<td></td>
+				<td><input type="text" class="image" name="dbURL" value="<?=$pendingArray['dbURL'];?>" /></td>
+			</tr>
+			
+			<tr>
+				<td colspan="3" height="15"></td>
+			</tr>
+			
+			<tr>
+				<td class="hudLabel">Order</td>
+				<td></td>
+				<td><input type="text" class="order" name="dbOrder" value="<?=$pendingArray['dbOrder'];?>" /></td>
+			</tr>
+			<tr>
+				<td class="hudLabel">Display?</td>
+				<td></td>
+				<td>
+					<select name="dbDisplay">
+						<option value="y"<? if($pendingArray['dbDisplay'] == "y") { echo " selected"; } ?>>Yes</option>
+						<option value="n"<? if($pendingArray['dbDisplay'] == "n") { echo " selected"; } ?>>No</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td class="hudLabel">Department</td>
+				<td></td>
+				<td>
+					<?php if(in_array("m_database2", $sessionAccess)) { ?>
+						<select name="dbDept">
+							<option value="0">Global Database</option>
+							
+							<?php
+							
+							while($deptFetch = mysql_fetch_assoc($deptsR)) {
+								extract($deptFetch, EXTR_OVERWRITE);
+							
+							?>
+								
+								<option value="<?=$deptFetch['deptid'];?>"<? if($pendingArray['dbDept'] == $deptFetch['deptid']) { echo " selected"; } ?>><? printText($deptFetch['deptName']);?></option>
+							
+							<?php } ?>
+							
+						</select>
+					<?php
+					
+					} elseif(in_array("m_database1", $sessionAccess)) {
+						$deptFetch = mysql_fetch_row($deptsR);
+						
+						echo "<input type='hidden' name='dbDept' value='" . $deptFetch[1] . "' />";
+						echo "<strong style='color:#" . $deptFetch[3] . ";'>";
+						printText($deptFetch[2]);
+						echo "</strong>";
+					}
+					
+					?>
+				</td>
+			</tr>
+			
+			<tr>
+				<td colspan="3" height="15"></td>
+			</tr>
+			
+			<tr>
+				<td class="hudLabel">Short Description</td>
+				<td></td>
+				<td><textarea name="dbDesc" rows="2" class="desc"><?=$pendingArray['dbDesc'];?></textarea></td>
+			</tr>
+			<tr>
+				<td class="hudLabel">Content<sup>&Dagger;</sup></td>
+				<td></td>
+				<td><textarea name="dbContent" rows="12" class="desc"><?=$pendingArray['dbContent'];?></textarea></td>
+			</tr>
+		</table>
 
-<div>
-	<input type="hidden" name="action_id" value="<?=$pendingArray['menuid'];?>" />
-	<input type="hidden" name="action_type" value="update" />
+		<div>
+			<input type="hidden" name="action_type" value="create" />
 	
-	<input type="image" src="<?=$webLocation;?>images/hud_button_ok.png" name="activate" value="Activate" />
-</div>
+			<input type="image" src="<?=$webLocation;?>images/hud_button_ok.png" name="activate" value="Activate" />
+		</div>
 
-</form>
+	</form>
 
 <?php } /* close the referer check */ ?>
