6863e394c374f80bc0ec102d0e47d2b4031e7b0e
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-04-04 18:50:12 +0000

    working on the database delete AJAX page

--- trunk/admin/ajax/database_delete.php
+++ trunk/admin/ajax/database_delete.php
@@ -10,93 +10,57 @@ if( !is_array( $sessionAccess ) ) {
 	$sessionAccess = explode( ",", $_SESSION['sessionAccess'] );
 }
 
-if(in_array("m_database1", $sessionAccess) || in_array("m_database2",$sessionAccess))
+if(in_array("x_approve_news", $sessionAccess))
 {
 	include_once('../../framework/functionsGlobal.php');
 	include_once('../../framework/functionsUtility.php');
-	
+
 	if(isset($_GET['id']) && is_numeric($_GET['id']))
 	{
 		$id = $_GET['id'];
 	}
 	
 	/* get the data */
-	$get = "SELECT * FROM sms_menu_items WHERE menuid = $id LIMIT 1";
+	$get = "SELECT * FROM sms_database WHERE dbid = $id LIMIT 1";
 	$getR = mysql_query( $get );
 	$pendingArray = mysql_fetch_assoc( $getR );
 
 ?>
-
 	<h2>Delete Database Entry?</h2>
-	<p>Use the fields below to provide details for your new mission. <strong class="yellow">Note:</strong> Mission images <strong>must</strong> be located in the <em>images/missionimages</em> directory. All you need to do is specify the image in that directory.</p>
-	<br />
-
-<form method="post" action="">
-<table cellpadding="0" cellspacing="3">
-	<tr>
-		<td>
-			<span class="fontNormal"><b>Order</b></span><br />
-			<input type="text" class="order" name="dbOrder" maxlength="4" value="99" />
-		</td>
-		<td>
-			<span class="fontNormal"><b>Display?</b></span><br />
-			<select name="dbDisplay">
-				<option value="y">Yes</option>
-				<option value="n">No</option>
-			</select>
-		</td>
-		<td rowspan="5" valign="top" align="center" width="5">&nbsp;</td>
-		<td rowspan="5" valign="top" align="center" width="70%">
-			<span class="fontNormal"><b>Content</b></span><br />
-			<span class="fontSmall">* used only for database entries, not URL forwarding</span><br />
-			<textarea name="dbContent" rows="12" class="desc"></textarea>
-		</td>
-	</tr>
-	<tr>
-		<td colspan="2">
-			<span class="fontNormal"><b>Title</b></span><br />
-			<input type="text" class="name" name="dbTitle" maxlength="100" />
-		</td>
-	</tr>
-	<tr>
-		<td colspan="2">
-			<span class="fontNormal"><b>Short Description</b></span><br />
-			<input type="text" class="name" name="dbDesc" />
-		</td>
-	</tr>
-	<tr>
-		<td colspan="2">
-			<span class="fontNormal"><b>Entry Type</b></span><br />
-			<select name="dbType">
-				<option value="onsite">URL Forward (On-Site)</option>
-				<option value="offsite">URL Forward (Off-Site)</option>
-				<option value="entry">Database Entry</option>
-			</select>
-		</td>
-	</tr>
-	<tr>
-		<td colspan="2">
-			<span class="fontNormal"><b>URL</b></span><br />
-			<span class="fontSmall">* used only for URL forwarding entries</span><br />
-			<input type="text" class="name" name="dbURL" maxlength="255" />
-		</td>
-	</tr>
-	<tr>
-		<td colspan="2"></td>
-		<td align="center">&nbsp;</td>
-		<td align="center">
-			<input type="image" src="<?=path_userskin;?>buttons/create.png" class="button" name="action_create" value="Create" />
-		</td>
-	</tr>
-</table>
-
-<div>
-	<input type="hidden" name="action_id" value="<?=$pendingArray['menuid'];?>" />
-	<input type="hidden" name="action_type" value="delete" />
+	<p>Are you sure you want to delete this database entry?  This action cannot be undone!</p>
 	
-	<input type="image" src="<?=$webLocation;?>images/hud_button_ok.png" name="activate" value="Activate" />
-</div>
-
-</form>
+	<hr size="1" width="100%" />
+	
+	<form method="post" action="">
+		<h3><? printText( $pendingArray['dbTitle'] );?></h3>
+		<p><? printText($pendingArray['dbDesc']);?></p>
+		
+		<?php if($pendingArray['dbType'] == "entry") { ?>
+			<div class="overflow"><? printText( $pendingArray['dbContent'] );?></div>
+		<?php
+		
+		} else {
+			switch($pendingArray['dbType'])
+			{
+				case 'onsite':
+					echo "On-site Entry: <a href='" . $webLocation . $pendingArray['dbURL'] . "'>" . $webLocation . $pendingArray['dbURL'] . "</a>";
+					break;
+				case 'offsite':
+					echo "Off-site Entry: <a href=" . $pendingArray['dbURL'] . "' target='_blank'>" . $pendingArray['dbURL'] . "</a>";
+					break;
+			}
+		}
+		
+		?>
+		
+		<p></p>
+		
+		<div>
+			<input type="hidden" name="action_id" value="<?=$pendingArray['dbid'];?>" />
+			<input type="hidden" name="action_type" value="delete" />
+		
+			<input type="image" src="<?=$webLocation;?>images/hud_button_ok.png" name="activate" value="Delete" />
+		</div>
+	</form>
 
 <?php } /* close the referer check */ ?>
