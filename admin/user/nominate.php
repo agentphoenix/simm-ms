4fad9d869e6dfbf2e6d6b7f3347e7b561c5e94a9
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-04-07 01:17:21 +0000

    tabs to break apart playing characters and NPCs for award nomination

--- admin/user/nominate.php
+++ admin/user/nominate.php
@@ -10,7 +10,7 @@ File: admin/user/nominate.php
 Purpose: Page to nominate another crew member for an award
 
 System Version: 2.6.0
-Last Modified: 2008-03-27 1820 EST
+Last Modified: 2008-04-06 2116 EST
 **/
 
 /* access check */
@@ -21,6 +21,15 @@ if( in_array( "u_nominate", $sessionAccess ) ) {
 	$subMenuClass = "user";
 	$result = false;
 	$query = false;
+	
+	if(isset($_GET['t']) && is_numeric($_GET['t']))
+	{
+		$tab = $_GET['t'];
+	}
+	else
+	{
+		$tab = 1;
+	}
 
 	if(isset($_POST['action_x']))
 	{
@@ -56,127 +65,239 @@ if( in_array( "u_nominate", $sessionAccess ) ) {
 		
 		$from = $rankName . " " . $firstName . " " . $lastName . " < " . $email . " >";
 		
+		/* set the TO email addresses */
+		$emFetch = "SELECT crewid, email FROM sms_crew WHERE (accessManage LIKE 'm_giveaward,%' OR accessManage LIKE '%,m_giveaward' ";
+		$emFetch.= "OR accessManage LIKE '%,m_giveaward,%')";
+		$emFetchR = mysql_query($emFetch);
+		
+		$email_array = array();
+		
+		while($em_raw = mysql_fetch_array($emFetchR)) {
+			extract($em_raw, EXTR_OVERWRITE);
+			
+			$email_array[] = $em_raw[1];
+		}
+		
+		/* if there isn't anything in the email array, put the CO into the string */
+		if(count($email_array) == 0) {
+			$to = printCOEmail();
+		} else {
+			$to = implode(",", $email_array);
+		}
+		
 		/* define the email variables */
-		$to = printCOEmail() . ", " . printXOEmail();
 		$subject = $emailSubject . " Crew Award Nomination";
-		$message = "A member of your crew has nominated a player for an award. The award has been added to the queue and is available for review and activation.
+		$message = "A member of your crew has nominated a character for an award. The award has been added to the queue and is available for review and activation from the control panel.
 
 Login to your control panel at " . $webLocation . "login.php?action=login to approve or deny this award.";
 		
 		/* send the nomination email */
 		mail( $to, $subject, $message, "From: " . $from . "\nX-Mailer: PHP/" . phpversion() );
-	
 	}
 
 ?>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('#container-1 > ul').tabs(<?php echo $tab; ?>);
+	});
+</script>
 
-	<div class="body">
-		
-		<?
-		
-		$check = new QueryCheck;
-		$check->checkQuery( $result, $query );
-				
-		if( isset( $action ) ) {
-			$check->message( "crew award nomination", "submit" );
-			$check->display();
-		}
-		
-		?>
-		
-		<span class="fontTitle">Award Nomination</span><br /><br />
-		
-		You can nominate a member of the crew for an award with this form.  Your 
-		nomination will be sent to the CO and XO for their review.  Please refer to the 
-		<a href="<?=$webLocation;?>index.php?page=crewawards">Crew Awards page</a> 
-		for descriptions and any requirements for the awards.<br /><br />
-		
-		<form method="post" action="<?=$webLocation;?>admin.php?page=user&sub=nominate">
-		<table>
-			<tr>
-				<td class="tableCellLabel">Crew Member</td>
-				<td>&nbsp;</td>
-				<td>
-					<select name="crew">
+<div class="body">
+	
+	<?php
+	
+	$check = new QueryCheck;
+	$check->checkQuery( $result, $query );
 			
-						<?
-						
-						/* query the users database */
-						$getCrew = "SELECT crew.crewid, crew.firstName, crew.lastName, rank.rankName ";
-						$getCrew.= "FROM sms_crew AS crew, sms_ranks AS rank WHERE ";
-						$getCrew.= "crew.crewType = 'active' AND crew.rankid = rank.rankid AND ";
-						$getCrew.= "crew.crewid != '$sessionCrewid' ORDER BY crew.rankid ASC";
-						$getCrewResult = mysql_query( $getCrew );
-						
-						$author = $firstName . " " . $lastName;
-						
-						/* start looping through what the query returns */
-						/* until it runs out of records */
-						while( $crewArray = mysql_fetch_array( $getCrewResult ) ) {
-							extract( $crewArray, EXTR_OVERWRITE );
-						
-						?>
-					
-						<option value="<?=$crewid;?>"><? printText( $rankName . " " . $firstName . " " . $lastName ); ?></option>
-					
-						<?php } ?>
-					
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td class="tableCellLabel">Nominated By</td>
-				<td>&nbsp;</td>
-				<td>
-					<? printCrewName( $sessionCrewid, "rank", "noLink" ); ?>
-					<input type="hidden" name="nominator" value="<?=$sessionCrewid;?>" />
-				</td>
-			</tr>
-			<tr>
-				<td class="tableCellLabel">Award</td>
-				<td>&nbsp;</td>
-				<td>
-					<select name="award">
-			
-						<?
-						
-						/* query the awards database */
-						$pullAwards = "SELECT awardid, awardName FROM sms_awards ORDER BY awardOrder ASC";
-						$pullAwardsResult = mysql_query( $pullAwards );
-						
-						/* start looping through what the query returns until it runs out of records */
-						while( $awardArray = mysql_fetch_array( $pullAwardsResult ) ) {
-							extract( $awardArray, EXTR_OVERWRITE );
-						
-						?>
-			
-						<option value="<?=$awardid;?>"><? printText( $awardName ); ?></option>
-						
-						<? } ?>
+	if( !empty( $check->query ) ) {
+		$check->message( "crew award nomination", "submit" );
+		$check->display();
+	}
+	
+	?>
+	
+	<span class="fontTitle">Award Nominations</span><br /><br />
+	
+	Awards can now be given to both playing characters as well as non-playing characters. If you want to
+	nominate a fellow player for an award, use the playing character tab; if you&rsquo;d like to nominate
+	an NPC for an award for their in character actions, use the non-playing character tab. Once you nominate
+	someone for an award, it will be added to a queue for the CO to review. If the CO agrees with your
+	award, they will be able to approve the award. Please refer to the 
+	<a href="<?=$webLocation;?>index.php?page=crewawards">Crew Awards page</a> for descriptions and any 
+	requirements for the awards.<br /><br />
+	
+	<div id="container-1">
+		<ul>
+			<li><a href="#one"><span>Playing Characters</span></a></li>
+			<li><a href="#two"><span>Non-Playing Characters</span></a></li>
+		</ul>
 		
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td colspan="3" height="15"></td>
-			</tr>
-			<tr>
-				<td class="tableCellLabel">
-					Please give a brief reason why you believe this crew member deserves this award.
-				</td>
-				<td>&nbsp;</td>
-				<td><textarea name="reason" class="desc" rows="6"></textarea></td>
-			</tr>
-			<tr>
-				<td colspan="3" height="15"></td>
-			</tr>
-			<tr>
-				<td colspan="2"></td>
-				<td><input type="image" src="<?=path_userskin;?>buttons/nominate.png" name="action" value="Nominate" class="button" /></td>
-			</tr>
-		</table>
-		</form>
+		<div id="one" class="ui-tabs-container ui-tabs-hide">
+			<form method="post" action="<?=$webLocation;?>admin.php?page=user&sub=nominate&t=1">
+			<table>
+				<tr>
+					<td class="tableCellLabel">Crew Member</td>
+					<td>&nbsp;</td>
+					<td>
+						<select name="crew">
+
+							<?php
+
+							$getCrew = "SELECT crew.crewid, crew.firstName, crew.lastName, rank.rankName ";
+							$getCrew.= "FROM sms_crew AS crew, sms_ranks AS rank WHERE ";
+							$getCrew.= "crew.crewType = 'active' AND crew.rankid = rank.rankid AND ";
+							$getCrew.= "crew.crewid != '$sessionCrewid' ORDER BY crew.rankid ASC";
+							$getCrewResult = mysql_query( $getCrew );
+
+							$author = $firstName . " " . $lastName;
+
+							while( $crewArray = mysql_fetch_array( $getCrewResult ) ) {
+								extract( $crewArray, EXTR_OVERWRITE );
+
+							?>
+
+							<option value="<?=$crewid;?>"><? printText( $rankName . " " . $firstName . " " . $lastName ); ?></option>
+
+							<?php } ?>
+
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td class="tableCellLabel">Nominated By</td>
+					<td>&nbsp;</td>
+					<td>
+						<? printCrewName( $sessionCrewid, "rank", "noLink" ); ?>
+						<input type="hidden" name="nominator" value="<?=$sessionCrewid;?>" />
+					</td>
+				</tr>
+				<tr>
+					<td class="tableCellLabel">Award</td>
+					<td>&nbsp;</td>
+					<td>
+						<select name="award">
+
+							<?php
+							
+							$pullAwards = "SELECT awardid, awardName FROM sms_awards ORDER BY awardOrder ASC";
+							$pullAwardsResult = mysql_query( $pullAwards );
+							
+							while( $awardArray = mysql_fetch_array( $pullAwardsResult ) ) {
+								extract( $awardArray, EXTR_OVERWRITE );
+
+							?>
+
+							<option value="<?=$awardid;?>"><? printText( $awardName ); ?></option>
+
+							<?php } ?>
+
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="3" height="15"></td>
+				</tr>
+				<tr>
+					<td class="tableCellLabel">
+						Please give a brief reason why you believe this crew member deserves this award.
+					</td>
+					<td>&nbsp;</td>
+					<td><textarea name="reason" class="desc" rows="6"></textarea></td>
+				</tr>
+				<tr>
+					<td colspan="3" height="15"></td>
+				</tr>
+				<tr>
+					<td colspan="2"></td>
+					<td><input type="image" src="<?=path_userskin;?>buttons/nominate.png" name="action" value="Nominate" class="button" /></td>
+				</tr>
+			</table>
+			</form>
+		</div>
 		
+		<div id="two" class="ui-tabs-container ui-tabs-hide">
+			<form method="post" action="<?=$webLocation;?>admin.php?page=user&sub=nominate&t2">
+			<table>
+				<tr>
+					<td class="tableCellLabel">Crew Member</td>
+					<td>&nbsp;</td>
+					<td>
+						<select name="crew">
+
+							<?php
+
+							$getCrew = "SELECT crew.crewid, crew.firstName, crew.lastName, rank.rankName ";
+							$getCrew.= "FROM sms_crew AS crew, sms_ranks AS rank WHERE ";
+							$getCrew.= "crew.crewType = 'npc' AND crew.rankid = rank.rankid AND ";
+							$getCrew.= "crew.crewid != '$sessionCrewid' ORDER BY crew.rankid ASC";
+							$getCrewResult = mysql_query( $getCrew );
+
+							$author = $firstName . " " . $lastName;
+
+							while( $crewArray = mysql_fetch_array( $getCrewResult ) ) {
+								extract( $crewArray, EXTR_OVERWRITE );
+
+							?>
+
+							<option value="<?=$crewid;?>"><? printText( $rankName . " " . $firstName . " " . $lastName ); ?></option>
+
+							<?php } ?>
+
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td class="tableCellLabel">Nominated By</td>
+					<td>&nbsp;</td>
+					<td>
+						<? printCrewName( $sessionCrewid, "rank", "noLink" ); ?>
+						<input type="hidden" name="nominator" value="<?=$sessionCrewid;?>" />
+					</td>
+				</tr>
+				<tr>
+					<td class="tableCellLabel">Award</td>
+					<td>&nbsp;</td>
+					<td>
+						<select name="award">
+
+							<?php
+							
+							$pullAwards = "SELECT awardid, awardName FROM sms_awards WHERE awardCat = 'ic' ORDER BY awardOrder ASC";
+							$pullAwardsResult = mysql_query( $pullAwards );
+							
+							while( $awardArray = mysql_fetch_array( $pullAwardsResult ) ) {
+								extract( $awardArray, EXTR_OVERWRITE );
+
+							?>
+
+							<option value="<?=$awardid;?>"><? printText( $awardName ); ?></option>
+
+							<?php } ?>
+
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="3" height="15"></td>
+				</tr>
+				<tr>
+					<td class="tableCellLabel">
+						Please give a brief reason why you believe this NPC deserves this award for their in character actions.
+					</td>
+					<td>&nbsp;</td>
+					<td><textarea name="reason" class="desc" rows="6"></textarea></td>
+				</tr>
+				<tr>
+					<td colspan="3" height="15"></td>
+				</tr>
+				<tr>
+					<td colspan="2"></td>
+					<td><input type="image" src="<?=path_userskin;?>buttons/nominate.png" name="action" value="Nominate" class="button" /></td>
+				</tr>
+			</table>
+			</form>
+		</div>
 	</div>
+</div>
 
 <? } else { errorMessage( "crew award nomination" ); } ?>
