f3a58d041f44164bdcd51646120c2c903bc97cd3
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-04-15 03:43:23 +0000

    giving users the ability to edit their own posts

--- trunk/pages/post.php
+++ trunk/pages/post.php
@@ -5,12 +5,12 @@ This is a necessary system file. Do not modify this page unless you are highly
 knowledgeable as to the structure of the system. Modification of this file may
 cause SMS to no longer function.
 
-Author: Nathan Wharry [ mail@herschwolf.net ]
+Authors: David VanScott [ davidv@anodyne-productions.com ]
 File: pages/post.php
 Purpose: To display the individual posts to a mission
 
 System Version: 2.6.0
-Last Modified: 2008-02-25 1645 EST
+Last Modified: 2008-04-14 2342 EST
 **/
 
 /* define the page class */
@@ -22,7 +22,7 @@ if(isset($_GET['id']) && is_numeric($_GET['id']))
 }
 else
 {
-	$mp_id = "";
+	$mp_id = NULL;
 }
 
 /* pull in the menu */
@@ -33,86 +33,90 @@ if( isset( $sessionCrewid ) ) {
 }
 
 /* get post id for individual message display */
-if( !empty($mp_id) ) {
+if(isset($mp_id))
+{
 
 	/* pull all the information relating to the post */
-	$getpost = "SELECT * FROM sms_posts ";
-	$getpost.= "WHERE postid = '$mp_id' LIMIT 1";
-	$getpostResult = mysql_query ( $getpost );
+	$getpost = "SELECT * FROM sms_posts WHERE postid = $mp_id LIMIT 1";
+	$getpostResult = mysql_query($getpost);
 	
 	/* pull all posts to create the next and prev post links */
-	$getposts = "SELECT postid FROM sms_posts ";
-	$getposts.= "WHERE postStatus = 'activated' ";
-	$getposts.= "ORDER BY postPosted ASC";
-	$getpostsResult = mysql_query ( $getposts );
+	$getposts = "SELECT postid FROM sms_posts WHERE postStatus = 'activated' ORDER BY postPosted ASC";
+	$getpostsResult = mysql_query($getposts);
 	
 	/* extract the post data into the MySQL field name variables */
-	$postinfo = mysql_fetch_array( $getpostResult );
-		extract( $postinfo, EXTR_OVERWRITE );
+	$postinfo = mysql_fetch_array($getpostResult);
+		extract($postinfo, EXTR_OVERWRITE);
+		$tempAuthors = explode(",", $postAuthor); /* temporary array for user post editing */
 	
 	/* pull the mission title */
-	$getmission = "SELECT missionid, missionTitle ";
-	$getmission.= "FROM sms_missions ";
-	$getmission.= "WHERE missionid = '$postMission'";
-	$getmissionResult = mysql_query ( $getmission );
+	$getmission = "SELECT missionid, missionTitle FROM sms_missions WHERE missionid = '$postMission'";
+	$getmissionResult = mysql_query($getmission);
 	
 	/* extract the mission title */
-	$mission = mysql_fetch_array( $getmissionResult );
-		extract( $mission, EXTR_OVERWRITE );
+	$mission = mysql_fetch_array($getmissionResult);
+		extract($mission, EXTR_OVERWRITE);
 	
 	/* if post is untitled give it a title */
-	if ( $postTitle == "" ) {
-		 $postTitle = "[ Untitled ]";
+	if($postTitle == "")
+	{
+		$postTitle = "[ Untitled ]";
 	}
 	
 ?>
 
 <div class="body">
 
-	<span class="fontTitle"><i><? printText( $missionTitle ); ?></i> - <? printText( $postTitle ); ?></span>
-	<br />
-	<span class="fontMedium">by <? displayAuthors ( $postid, "noLink" ); ?></span>
-	<br /><br />
+	<span class="fontTitle"><em><? printText( $missionTitle ); ?></em> &ndash; <? printText( $postTitle ); ?></span><br />
+	<span class="fontMedium">by <? displayAuthors ( $postid, "noLink" ); ?></span><br /><br />
 	
 	<span class="fontNormal postDetails">
 	<div align="center">
 
-	<?  
+	<?php
 		
-		/* point the previous and next post buttons to the correct posts */
-		$idNumbers = array();
-		
-		while( $myrow = mysql_fetch_array( $getpostsResult ) ) {
-			$idNumbers[] = $myrow['postid'];
-		}
-		
-		$arrayCount = count($idNumbers) -1;
-		
-		foreach( $idNumbers as $key => $value ) {
-			if( $mp_id == $value ) {
-				
-				$nextKey = $key+1;
-				$prevKey = $key-1;
-				
-				/* display the previous and next links in the post details box */
-				if( $prevKey >= 0 && $idNumbers[$prevKey] != '' ) {
-					echo "<a href='" . $webLocation . "/index.php?page=post&id=" . $idNumbers[$prevKey] . "'><img src='" . $webLocation . "/images/previous.png' alt='Previous Entry' border='0' class='image' /></a>";
-				} if( ( $prevKey >= 0 && $idNumbers[$prevKey] != '' ) && ( $nextKey <= $arrayCount && $idNumbers[$nextKey] != '' ) ) {
-					echo "&nbsp;";
-				} if( $nextKey <= $arrayCount && $idNumbers[$nextKey] != '' ) {
-					echo "<a href='$webLocation/index.php?page=post&id=$idNumbers[$nextKey]'><img src='$webLocation/images/next.png' alt='Next Entry' border='0' class='image' /></a>";
-				}
-			} /* close if(mp_id == value) */
-		} /* close foreach loop */
+	/* point the previous and next post buttons to the correct posts */
+	$idNumbers = array();
+	
+	while($myrow = mysql_fetch_array($getpostsResult))
+	{
+		$idNumbers[] = $myrow['postid'];
+	}
+	
+	$arrayCount = count($idNumbers) -1;
+	
+	foreach($idNumbers as $key => $value)
+	{
+		if($mp_id == $value)
+		{
+			$nextKey = $key+1;
+			$prevKey = $key-1;
+			
+			/* display the previous and next links in the post details box */
+			if( $prevKey >= 0 && $idNumbers[$prevKey] != '' ) {
+				echo "<a href='" . $webLocation . "/index.php?page=post&id=" . $idNumbers[$prevKey] . "'><img src='" . $webLocation . "/images/previous.png' alt='Previous Entry' border='0' class='image' /></a>";
+			} if( ( $prevKey >= 0 && $idNumbers[$prevKey] != '' ) && ( $nextKey <= $arrayCount && $idNumbers[$nextKey] != '' ) ) {
+				echo "&nbsp;";
+			} if( $nextKey <= $arrayCount && $idNumbers[$nextKey] != '' ) {
+				echo "<a href='$webLocation/index.php?page=post&id=$idNumbers[$nextKey]'><img src='$webLocation/images/next.png' alt='Next Entry' border='0' class='image' /></a>";
+			}
+		} /* close if(mp_id == value) */
+	} /* close foreach loop */
 		
 	?>
 	
-			<br />
-			<b>Post Details</b><br />
+			<br /><strong>Post Details</strong><br />
 			<?
 		
-			if( in_array( "m_posts", $sessionAccess ) ) {
+			if(
+				in_array("m_posts2", $sessionAccess) ||
+				(in_array("m_posts1", $sessionAccess) && in_array($sessionCrewid, $tempAuthors))
+			) {
 				echo "<a href='" . $webLocation . "admin.php?page=manage&sub=posts&id=" . $mp_id . "' class='edit'><b>Edit</b></a>";
+			}
+			
+			if(in_array("m_posts2", $sessionAccess))
+			{
 				echo "&nbsp; &middot; &nbsp;";
 
 			?>	
@@ -124,15 +128,13 @@ if( !empty($mp_id) ) {
 					<a href="<?=$webLocation;?>admin.php?page=manage&sub=posts&remove=<?=$mp_id;?>" class="delete">Delete</a>
 				</noscript>
 				
-			<?
+			<?php
 
-				if( $postinfo['postStatus'] == "pending" ) {
-				
+				if( $postinfo['postStatus'] == "pending" )
+				{
 					echo "&nbsp; &middot; &nbsp;";
 					echo "<a href='" . $webLocation . "admin.php?page=manage&sub=activate&type=post&id=" . $mp_id . "&action=activate'><b>Activate</b></a>";
-				
 				}
-				
 			}
 			
 			?><p></p>
@@ -143,6 +145,11 @@ if( !empty($mp_id) ) {
 				<td class="tableCellLabel">Title</td>
 				<td>&nbsp;</td>
 				<td><? printText( $postTitle ); ?></td>
+			</tr>
+			<tr>
+				<td class="tableCellLabel">Mission</td>
+				<td>&nbsp;</td>
+				<td><a href="<?=$webLocation;?>index.php?page=mission&mid=<?=$missionid;?>"><? printText( $missionTitle ); ?></a></td>
 			</tr>
 			<tr>
 				<td class="tableCellLabel">Author(s)</td>
