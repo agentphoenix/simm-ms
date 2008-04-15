18cff69477c6964497a5995f894ce60bcb166dcd
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-04-15 05:03:18 +0000

    removed unnecessary access checks

--- trunk/admin/reports.php
+++ trunk/admin/reports.php
@@ -10,21 +10,22 @@ File: admin/reports.php
 Purpose: Page to pull in the necessary report page
 
 System Version: 2.6.0
-Last Modified: 2008-04-05 1642 EST
+Last Modified: 2008-04-15 0102 EST
 **/
 
 /* define the page class and variables */
 $pageClass = "admin";
 $subMenuClass = "reports";
 
-if(isset($_GET['sub']))
-{
+if(isset($_GET['sub'])) {
 	$sub = $_GET['sub'];
+} else {
+	$sub = NULL;
 }
 
-/* if they have a session, continue processing */
-if( isset( $sessionCrewid ) && in_array( "reports", $sessionAccess ) ) {
-
+/* if they have a session, continue */
+if(isset($sessionCrewid))
+{
 	/* pull in the main navigation */
 	include_once( "skins/" . $sessionDisplaySkin . "/menu.php" );
 
