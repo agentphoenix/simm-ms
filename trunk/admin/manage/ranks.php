8712d31088e3e1dfbcd209a06fd35f8b83a8e7ff
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-04-17 22:04:40 +0000

    new rank management page

--- trunk/admin/manage/ranks.php
+++ trunk/admin/manage/ranks.php
@@ -10,7 +10,7 @@ File: admin/manage/ranks.php
 Purpose: Page that moderates the ranks
 
 System Version: 2.6.0
-Last Modified: 2007-10-10 1023 EST
+Last Modified: 2008-04-17 1803 EST
 **/
 
 /* access check */
@@ -19,149 +19,115 @@ if( in_array( "m_ranks", $sessionAccess ) ) {
 	/* set the page class and variables */
 	$pageClass = "admin";
 	$subMenuClass = "manage";
-	$rank = $_GET['rank'];
-	$set = $_GET['set'];
-	$actionCreate = $_POST['action_create_x'];
-	$actionUpdate = $_POST['action_update_x'];
-	$actionDelete = $_POST['action_delete_x'];
+	$result = FALSE;
+	$query = FALSE;
 	
-	/* if there is no rank specified, set it to 1 */
-	if( !isset( $rank ) ) {
-		$rank = "1";
+	/* create an array of allowed ranks */
+	$allowedRanksArray = explode( ",", $allowedRanks );
+	
+	/* set the rank variable */
+	if(isset($_GET['rank']) && is_numeric($_GET['rank'])) {
+		$rank = $_GET['rank'];
+	} else {
+		$rank = 1;
 	}
 	
-	/* if there is no rank set specified, set it to the system default */
-	if( !isset( $set ) ) {
+	/* set the rank set variable */
+	if(isset($_GET['set']) && in_array($_GET['set'], $allowedRanksArray)) {
+		$set = $_GET['set'];
+	} else {
 		$set = $rankSet;
 	}
 	
-	/* define the POST variables */
-	$rankid = $_POST['rankid'];
-	$rankName = addslashes( $_POST['rankName'] );
-	$rankClass = $_POST['rankClass'];
-	$rankOrder = $_POST['rankOrder'];
-	$rankImage = $_POST['rankImage'];
-	$rankDisplay = $_POST['rankDisplay'];
-	
 	/* if the POST action is update */
-	if( $actionUpdate ) {
-		
-		$rankArray = array();
-		
-		foreach( $_POST as $key => $value )
+	if(isset($_POST['action_update_x']))
+	{
+		if(isset($_POST['rankid']) && is_numeric($_POST['rankid']))
 		{
-			if( substr( $key, 0, 2 ) == "x_" )
-			{
-				/* strip the x_ from the key */
-				$key = substr_replace( $key, '', 0, 2 );
-				
-				/* get the id of the rank */
-				$offset = strpos( $key, "_" );
-				$id = substr( $key, 0, $offset );
-				$key = substr_replace( $key, '', 0, ($offset+1) );
-				
-				if( !array_key_exists( $id ) ) {
-					$rankArray[$id] = array();
-				}
-				
-				$rankArray[$id][$key] = $value;
-				
-				echo "<pre>";
-				print_r( $rankArray );
-				echo "</pre>";
-			}
+			$rankid = $_POST['rankid'];
+		}
+		else
+		{
+			$rankid = NULL;
 		}
 		
-		/* do the update query */
-		$query = "UPDATE sms_ranks SET ";
-		$query.= "rankName = '$rankName', rankClass = '$rankClass', rankOrder = '$rankOrder', ";
-		$query.= "rankImage = '$rankImage', rankDisplay = '$rankDisplay' ";
-		$query.= "WHERE rankid = '$rankid' LIMIT 1";
-		//$result = mysql_query( $query );
+		$update = "UPDATE sms_ranks SET rankOrder = %d, rankName = %s, rankImage = %s, rankDisplay = %s, rankClass = %d ";
+		$update.= "WHERE rankid = $rankid LIMIT 1";
+		
+		$query = sprintf(
+			$update,
+			escape_string($_POST['rankOrder']),
+			escape_string($_POST['rankName']),
+			escape_string($_POST['rankImage']),
+			escape_string($_POST['rankDisplay']),
+			escape_string($_POST['rankClass'])
+		);
+		
+		$result = mysql_query($query);
 		
 		/* optimize table */
 		optimizeSQLTable( "sms_ranks" );
 		
 		$action = "update";
-	
-	/* if the POST action is create */
-	} elseif( $actionCreate ) {
+	}
+	elseif(isset($_POST['action_type']) && $_POST['action_type'] == "create")
+	{	
+		$insert = "INSERT INTO sms_ranks (rankOrder, rankName, rankImage, rankDisplay, rankClass) VALUES(%d, %s, %s, %s, %d)";
 		
-		/* do the create query */
-		$query = "INSERT INTO sms_ranks ( rankid, rankOrder, rankName, rankImage, rankType, rankDisplay, rankClass ) ";
-		$query.= "VALUES ( '', '$rankOrder', '$rankName', '$rankImage', '1', '1', '$rankClass' )";
-		$result = mysql_query( $query );
+		$query = sprintf(
+			$insert,
+			escape_string($_POST['rankOrder']),
+			escape_string($_POST['rankName']),
+			escape_string($_POST['rankImage']),
+			escape_string($_POST['rankDisplay']),
+			escape_string($_POST['rankClass'])
+		);
+		
+		$result = mysql_query($query);
 		
 		/* optimize table */
-		optimizeSQLTable( "sms_ranks" );
+		optimizeSQLTable("sms_ranks");
 		
 		$action = "create";
-	
-	/* if the POST action is delete */
-	} elseif( $actionDelete ) {
+	}
+	elseif(isset($_POST['action_delete_x']))
+	{
+		if(isset($_POST['rankid']) && is_numeric($_POST['rankid']))
+		{
+			$rankid = $_POST['rankid'];
+		}
+		else
+		{
+			$rankid = NULL;
+		}
 		
 		/* do the delete query */
-		$query = "DELETE FROM sms_ranks WHERE rankid = '$rankid' LIMIT 1";
-		$result = mysql_query( $query );
+		$query = "DELETE FROM sms_ranks WHERE rankid = $rankid LIMIT 1";
+		$result = mysql_query($query);
 		
 		/* optimize table */
 		optimizeSQLTable( "sms_ranks" );
 		
 		$action = "delete";
-	
 	}
-	
-	/* strip the slashes */
-	$rankName = stripslashes( $rankName );
 
 ?>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$("a[rel*=facebox]").click(function() {
+			var action = $(this).attr("myAction");
+			
+			jQuery.facebox(function() {
+				jQuery.get('admin/ajax/rank_' + action + '.php', function(data) {
+					jQuery.facebox(data);
+				});
+			});
+			return false;
+		});
+	});
+</script>
 	
 	<div class="body">
-	
-		<div align="center">
-			<span class="fontSmall">Click on the rank image to view and edit that <b>rank set</b></span><br /><br />
-	
-			<?
-			
-			/* split the array by comma */
-			$allowedRanksArray = explode( ",", $allowedRanks );
-			
-			/* loop through to create the list of rank sets */
-			foreach( $allowedRanksArray as $key => $value ) {
-	
-			?>
-	
-			<a href="<?=$webLocation;?>admin.php?page=manage&sub=ranks&set=<?=trim( $value );?>">
-				<img src="<?=$webLocation;?>images/ranks/<?=trim( $value );?>/preview.png" border="0" alt="" class="image" />
-			</a>
-			
-			<? } ?>
-		</div><br />
-		
-		<div align="center">
-			<span class="fontSmall">Click on the rank image to view and edit that <b>color set</b></span><br /><br />
-			
-			<?
-			
-			/* get the rank classes from the database */
-			$getRankClasses = "SELECT rankClass, rankImage FROM sms_ranks ";
-			$getRankClasses.= "WHERE rankImage LIKE '%-blank.png' ";
-			$getRankClasses.= "GROUP BY rankClass ORDER BY rankClass ASC";
-			$getRankClassesResult = mysql_query( $getRankClasses );
-			
-			/* loop through the spit out the rank links */
-			while( $classFetch = mysql_fetch_array( $getRankClassesResult ) ) {
-				extract( $classFetch, EXTR_OVERWRITE );
-			
-			?>
-			
-			<a href="<?=$webLocation;?>admin.php?page=manage&sub=ranks&set=<?=trim( $set );?>&rank=<?=$rankClass;?>">
-				<img src="<?=$webLocation;?>images/ranks/<?=trim( $set );?>/<?=$rankImage;?>" border="0" alt="Rank Class <?=$rankClass;?>" class="image" />
-			</a>
-			
-			<? } ?>
-		</div><br />
-		
 		<?
 		
 		$check = new QueryCheck;
@@ -173,48 +139,41 @@ if( in_array( "m_ranks", $sessionAccess ) ) {
 		}
 		
 		?>
+		<span class="fontTitle">Rank Management</span><br /><br />
+		Use the fields below to manage the ranks used by SMS. <strong class="yellow">Please note</strong> that all the rank sets use the same data from the database, the only thing that changes are the images. You cannot edit a certain rank set to be different from another. If you want to create a new rank item, use the link below.<br /><br />
 		
-		<span class="fontTitle">Create New Rank</span>
-	
-		<form method="post" action="<?=$webLocation;?>admin.php?page=manage&sub=ranks&set=<?=trim( $set );?>&rank=<?=$rank;?>">
-		<table cellpadding="0" cellspacing="3">
-			<tr>
-				<td width="40">
-					<span class="fontNormal"><b>Class</b></span><br />
-					<input type="text" class="class" name="rankClass" maxlength="3" />
-				</td>
-				<td width="40">
-					<span class="fontNormal"><b>Order</b></span><br />
-					<input type="text" class="order" name="rankOrder" maxlength="3" />
-				</td>
-				<td width="375">
-					<span class="fontNormal"><b>Rank</b></span><br />
-					<input type="text" class="name" name="rankName" />
-				</td>
-				<td align="left" valign="bottom">
-					<input type="image" src="<?=path_userskin;?>buttons/create.png" class="button" name="action_create" value="Create" />
-				</td>
-		    </tr>
-			<tr>
-				<td></td>
-				<td></td>
-				<td>
-					<span class="fontNormal"><b>Image</b></span><br />
-					<span class="fontSmall">images/ranks/<?=trim( $set );?>/</span><input type="text" class="image" name="rankImage" />
-				</td>
-				<td></td>
-			</tr>
-		</table>
-		</form>
-		<br /><br />
+		<a href="#" rel="facebox" myAction="add" class="add fontMedium"><strong>Create New Rank &raquo;</strong></a><br /><br />
 		
-		<span class="fontTitle">Manage Existing Ranks</span>
+		<p><span class="fontSmall">Click on the rank image to view that rank set</span><br /><br />
+		<?php foreach($allowedRanksArray as $key => $value) { ?>
+			<a href="<?=$webLocation;?>admin.php?page=manage&sub=ranks&set=<?=trim( $value );?>">
+				<img src="<?=$webLocation;?>images/ranks/<?=trim( $value );?>/preview.png" border="0" alt="" class="image" />
+			</a>
+		<?php } ?></p>
+		
+		<p><span class="fontSmall">Click on the rank image to view and edit that <b>color set</b></span><br /><br />
+		<?php
 	
-		<table cellpadding="0" cellspacing="3">
-			<?
+		/* get the rank classes from the database */
+		$getRankClasses = "SELECT rankClass, rankImage FROM sms_ranks WHERE rankImage LIKE '%-blank.png' ";
+		$getRankClasses.= "GROUP BY rankClass ORDER BY rankClass ASC";
+		$getRankClassesResult = mysql_query( $getRankClasses );
+	
+		/* loop through the spit out the rank links */
+		while( $classFetch = mysql_fetch_array( $getRankClassesResult ) ) {
+			extract( $classFetch, EXTR_OVERWRITE );
+	
+		?>
+			<a href="<?=$webLocation;?>admin.php?page=manage&sub=ranks&set=<?=trim( $set );?>&rank=<?=$rankClass;?>">
+				<img src="<?=$webLocation;?>images/ranks/<?=trim( $set );?>/<?=$rankImage;?>" border="0" alt="Rank Class <?=$rankClass;?>" class="image" />
+			</a>
+		<?php } ?></p>
+		
+		<table cellpadding="0" cellspacing="3" border="0">
+			<?php
 			
 			/* pull the ranks from the database */
-			$getRanks = "SELECT * FROM sms_ranks WHERE rankClass = '$rank' ORDER BY rankOrder ASC";
+			$getRanks = "SELECT * FROM sms_ranks WHERE rankClass = $rank ORDER BY rankOrder ASC";
 			$getRanksResult = mysql_query( $getRanks );
 			
 			/* loop through the results and fill the form */
@@ -224,23 +183,42 @@ if( in_array( "m_ranks", $sessionAccess ) ) {
 			?>
 			<form method="post" action="<?=$webLocation;?>admin.php?page=manage&sub=ranks&set=<?=$set;?>&rank=<?=$rank;?>">
 			<tr>
-				<td width="40">
+				<td colspan="2" width="165">
 					<span class="fontNormal"><b>Class</b></span><br />
-					<input type="text" class="class" name="x_<?=$rankid;?>_rankClass"  maxlength="3" value="<?=$rankClass;?>" />
-				</td>
-				<td width="40">
-					<span class="fontNormal"><b>Order</b></span><br />
-					<input type="text" class="order" name="x_<?=$rankid;?>_rankOrder" maxlength="3" value="<?=$rankOrder;?>" />
+					<select name="rankClass">
+						<?php
+						
+						$get = "SELECT deptClass, deptName, deptColor FROM sms_departments ";
+						$get.= "WHERE deptDisplay = 'y' GROUP BY deptClass ORDER BY deptOrder ASC";
+						$getR = mysql_query($get);
+						
+						while($fetch = mysql_fetch_array($getR)) {
+							extract($fetch, EXTR_OVERWRITE);
+							
+							if($fetch[0] == $rankClass)
+							{
+								$selected = "selected";
+							}
+							else
+							{
+								$selected = FALSE;
+							}
+							
+							echo "<option value='" . $fetch[0] . "' style='color:#" . $fetch[2] . ";' " . $selected . ">" . $fetch[1] . "</option>";
+						}
+						
+						?>
+					</select>
 				</td>
 				<td>
 					<span class="fontNormal"><b>Rank</b></span><br />
-					<input type="text" class="name" name="x_<?=$rankid;?>_rankName" value="<?=stripslashes( $rankName );?>" />
+					<input type="text" class="name" name="rankName" value="<?=stripslashes( $rankName );?>" />
 				</td>
-				<td width="150" align="center" valign="bottom">
+				<td rowspan="2" width="150" align="center" valign="middle">
 					<img src="<?=$webLocation . 'images/ranks/' . trim( $set ) . '/' . $rankImage;?>" alt="<?=$rankName;?>" border="0" />
 				</td>
 				<td rowspan="2" align="center" valign="middle">
-					<input type="hidden" name="x_<?=$rankid;?>_rankid" value="<?=$rankid;?>" />
+					<input type="hidden" name="rankid" value="<?=$rankid;?>" />
 					<input type="image" src="<?=path_userskin;?>buttons/update.png" class="button" name="action_update" value="Update" /><br />
 					<script type="text/javascript">
 						document.write( "<input type=\"image\" src=\"<?=path_userskin;?>buttons/delete.png\" name=\"action_delete\" value=\"Delete\" class=\"button\" onClick=\"javascript:return confirm('This action is permanent and cannot be undone. Are you sure you want to delete this rank?')\" />" );
@@ -251,18 +229,21 @@ if( in_array( "m_ranks", $sessionAccess ) ) {
 				</td>
 			</tr>
 			<tr>
-				<td colspan="2">
+				<td>
+					<span class="fontNormal"><b>Order</b></span><br />
+					<input type="text" class="order" name="rankOrder" maxlength="3" value="<?=$rankOrder;?>" />
+				</td>
+				<td>
 					<span class="fontNormal"><b>Display?</b></span><br />
-					<select name="x_<?=$rankid;?>_rankDisplay">
+					<select name="rankDisplay">
 						<option value="y"<? if( $rankDisplay == "y" ) { echo " selected"; } ?>>Yes</option>
 						<option value="n"<? if( $rankDisplay == "n" ) { echo " selected"; } ?>>No</option>
 					</select>
 				</td>
 				<td>
 					<span class="fontNormal"><b>Image</b></span><br />
-					<span class="fontSmall">images/ranks/<?=trim( $set );?>/</span><input type="text" class="image" name="x_<?=$rankid;?>_rankImage" value="<?=$rankImage;?>" />
+					<span class="fontSmall">images/ranks/<?=trim( $set );?>/</span><input type="text" class="image" name="rankImage" value="<?=$rankImage;?>" />
 				</td>
-			    <td></td>
 			</tr>
 			<tr>
 				<td colspan="5" height="25"></td>
