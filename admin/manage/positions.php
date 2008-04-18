36677d8da0e0e6c2b5072984e23e0f8a543d4ae2
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-04-18 18:39:02 +0000

    updating the positions management page and adding the ajax page for creating a new position

--- admin/manage/positions.php
+++ admin/manage/positions.php
@@ -9,90 +9,95 @@ Author: David VanScott [ davidv@anodyne-productions.com ]
 File: admin/manage/positions.php
 Purpose: Page that moderates the positions
 
-System Version: 2.5.0
-Last Modified: 2007-07-10 1007 EST
+System Version: 2.6.0
+Last Modified: 2008-04-18 1437 EST
 **/
 
 /* access check */
-if( in_array( "m_positions", $sessionAccess ) ) {
-
+if(in_array("m_positions", $sessionAccess))
+{
 	/* set the page class and vars */
 	$pageClass = "admin";
 	$subMenuClass = "manage";
-	$actionCreate = $_POST['action_create_x'];
-	$actionUpdate = $_POST['action_update_x'];
-	$actionDelete = $_POST['action_delete_x'];
+	$query = FALSE;
+	$result = FALSE;
+	$positionid = FALSE;
 	
-	/* do some advanced checking to make sure someone's not trying to do a SQL injection */
-	if( !empty( $_GET['dept'] ) && preg_match( "/^\d+$/", $_GET['dept'], $matches ) == 0 ) {
-		errorMessageIllegal( "positions management page" );
-		exit();
-	} else {
-		/* set the GET variable */
+	if(isset($_GET['dept']) && is_numeric($_GET['dept']))
+	{
 		$dept = $_GET['dept'];
 	}
-	
-	/* if there is no department in the URL, set it to 1 */
-	if( !$dept ) {
-		$dept = "1";
+	else
+	{
+		$dept = 1;
 	}
-
-	/* define the POST variables */
-	$positionid = $_POST['positionid'];
-	$positionName = addslashes( $_POST['positionName'] );
-	$positionDept = $_POST['positionDept'];
-	$positionOrder = $_POST['positionOrder'];
-	$positionDesc = addslashes( $_POST['positionDesc'] );
-	$positionOpen = $_POST['positionOpen'];
-	$positionType = $_POST['positionType'];
-	$positionDisplay = $_POST['positionDisplay'];
 	
-	/* if the POST action is update */
-	if( $actionUpdate ) {
+	if(isset($_POST['action_update_x']))
+	{
+		if(isset($_POST['positionid']) && is_numeric($_POST['positionid']))
+		{
+			$positionid = $_POST['positionid'];
+		}
 		
-		/* do the update query */
-		$query = "UPDATE sms_positions SET ";
-		$query.= "positionName = '$positionName', positionDept = '$positionDept', ";
-		$query.= "positionOrder = '$positionOrder', positionDesc = '$positionDesc', ";
-		$query.= "positionOpen = '$positionOpen', positionType = '$positionType', ";
-		$query.= "positionDisplay = '$positionDisplay' WHERE positionid = '$positionid' LIMIT 1";
-		$result = mysql_query( $query );
+		$update = "UPDATE sms_positions SET positionName = %s, positionDept = %d, positionOrder = %d, positionDesc = %s, ";
+		$update.= "positionOpen = %d, positionType = %s, positionDisplay = %s WHERE positionid = $positionid LIMIT 1";
+		
+		$query = sprintf(
+			$update,
+			escape_string($_POST['positionName']),
+			escape_string($_POST['positionDept']),
+			escape_string($_POST['positionOrder']),
+			escape_string($_POST['positionDesc']),
+			escape_string($_POST['positionOpen']),
+			escape_string($_POST['positionType']),
+			escape_string($_POST['positionDisplay'])
+		);
+		
+		$result = mysql_query($query);
 		
 		/* optimize the table */
 		optimizeSQLTable( "sms_positions" );
 		
 		$action = "update";
-	
-	/* if the POST action is create */
-	} elseif( $actionCreate ) {
+	}
+	elseif(isset($_POST['action_type']) && $_POST['action_type'] == "create")
+	{
+		$insert = "INSERT INTO sms_positions (positionName, positionDept, positionOrder, positionDesc, positionOpen, ";
+		$insert.= "positionType, positionDisplay) VALUES (%s, %d, %d, %s, %d, %s, %s)";
 		
-		/* do the create query */
-		$query = "INSERT INTO sms_positions ( positionid, positionOrder, positionName, positionDesc, positionDept, positionType, positionOpen, positionDisplay ) ";
-		$query.= "VALUES ( '', '$positionOrder', '$positionName', '$positionDesc', '$positionDept', '$positionType', '$positionOpen', '$positionDisplay' )";
+		$query = sprintf(
+			$insert,
+			escape_string($_POST['positionName']),
+			escape_string($_POST['positionDept']),
+			escape_string($_POST['positionOrder']),
+			escape_string($_POST['positionDesc']),
+			escape_string($_POST['positionOpen']),
+			escape_string($_POST['positionType']),
+			escape_string($_POST['positionDisplay'])
+		);
+		
 		$result = mysql_query( $query );
 		
 		/* optimize the table */
 		optimizeSQLTable( "sms_positions" );
 		
 		$action = "create";
-	
-	/* if the POST action is delete */
-	} elseif( $actionDelete ) {
+	}
+	elseif(isset($_POST['action_delete_x']))
+	{
+		if(isset($_POST['positionid']) && is_numeric($_POST['positionid']))
+		{
+			$positionid = $_POST['positionid'];
+		}
 		
-		/* do the delete query */
-		$query = "DELETE FROM sms_positions WHERE positionid = '$positionid' LIMIT 1";
-		$result = mysql_query( $query );
+		$query = "DELETE FROM sms_positions WHERE positionid = $positionid LIMIT 1";
+		$result = mysql_query($query);
 		
 		/* optimize the table */
 		optimizeSQLTable( "sms_positions" );
 		
 		$action = "delete";
-	
 	}
-	
-	/* strip the slashes */
-	$positionName = stripslashes( $positionName );
-	$positionDesc = stripslashes( $positionDesc );
 	
 	/* grab the departments for the menu */
 	$getDepts = "SELECT deptid, deptName FROM sms_departments ORDER BY deptOrder ASC";
@@ -103,12 +108,45 @@ if( in_array( "m_positions", $sessionAccess ) ) {
 	$countDeptsFinal = $countDepts - 1;
 	
 ?>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$("a[rel*=facebox]").click(function() {
+			var action = $(this).attr("myAction");
+			
+			jQuery.facebox(function() {
+				jQuery.get('admin/ajax/position_' + action + '.php', function(data) {
+					jQuery.facebox(data);
+				});
+			});
+			return false;
+		});
+	});
+</script>
 
 	<div class="body">
+		<?php
 		
-		<div align="center">
-			<span class="fontSmall">Click on the department name to view and edit the positions</span><br />
-			<b>
+		$check = new QueryCheck;
+		$check->checkQuery( $result, $query );
+				
+		if( !empty( $check->query ) ) {
+			$check->message( "position", $action );
+			$check->display();
+		}
+		
+		?>
+		
+		<span class="fontTitle">Position Management</span><br /><br />
+		Use the fields below to manage the positions used by SMS. You can create new positions and tie them to a department by using the link below to add a new position.
+		
+		<? if( $dept == 1 ) { ?>
+		<strong class="yellow">Please Note:</strong> Several functions that control emails in SMS use position ids 1 and 2 for the Commanding Officer and Executive Officer respectively. Because of this, you cannot delete the first two positions. If you change either of these positions to have different id numbers, some of the email functions will not work properly.
+		<? } ?><br /><br />
+		
+		<a href="#" rel="facebox" myAction="add" class="add fontMedium"><strong>Add New Position &raquo;</strong></a><br /><br />
+		
+		<div class="update notify-normal fontNormal">
+			<strong class="orange">Click on the department name to view and edit the positions</strong><br /><br />
 			<?
 			
 			/* loop through the departments */
@@ -143,108 +181,18 @@ if( in_array( "m_positions", $sessionAccess ) ) {
 			
 			}
 			
-			?>
-			</b>
+			?><br /><br />
 		</div><br />
-		
-		<?
-		
-		$check = new QueryCheck;
-		$check->checkQuery( $result, $query );
-				
-		if( !empty( $check->query ) ) {
-			$check->message( "position", $action );
-			$check->display();
-		}
-		
-		?>
-		
-		<span class="fontTitle">Create New Position</span>
 	
-		<form method="post" action="<?=$webLocation;?>admin.php?page=manage&sub=positions&dept=<?=$dept;?>">
-		<table cellpadding="0" cellspacing="3">
-			<tr>
-				<td colspan="3">
-					<span class="fontNormal"><b>Position</b></span><br />
-					<input type="text" class="name" name="positionName" />
-				</td>
-				<td width="5" rowspan="3" align="center" valign="top">&nbsp;</td>
-				<td width="80%" rowspan="3" align="center" valign="top">
-					<span class="fontNormal"><b>Description</b></span><br />
-					<textarea name="positionDesc" rows="5" class="desc"></textarea>
-					<br />
-					<input type="hidden" name="position" value="<?=$positionid;?>" />
-                    <input type="image" src="<?=path_userskin;?>buttons/create.png" class="button" name="action_create" value="Create" />
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<span class="fontNormal"><b>Order</b></span><br />
-					<input type="text" class="order" name="positionOrder" maxlength="3" />
-				</td>
-				<td>
-					<span class="fontNormal"><b>Type</b></span><br />
-					<select name="positionType">
-						<option value="senior">Senior Staff</option>
-						<option value="crew">Crew</option>
-					</select>
-				</td>
-				<td>
-					<span class="fontNormal"><b>Display?</b></span><br />
-					<select name="positionDisplay">
-						<option value="y">Yes</option>
-						<option value="n">No</option>
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<span class="fontNormal"><b>Open</b></span><br />
-					<input type="text" class="order" maxlength="3" name="positionOpen" />
-				</td>
-				<td colspan="2">
-					<span class="fontNormal"><b>Department</b></span><br />
-					<select name="positionDept">
-					<?
-					
-					$getDepts = "SELECT deptid, deptName, deptColor FROM sms_departments ";
-					$getDepts.= "WHERE deptDisplay = 'y' ORDER BY deptOrder ASC";
-					$getDeptsResult = mysql_query( $getDepts );
+		<?php
 		
-					while( $deptFetch = mysql_fetch_array( $getDeptsResult ) ) {
-						extract( $deptFetch, EXTR_OVERWRITE );
-					
-					?>
-					
-						<option value="<?=$deptid;?>" style="color:#<?=$deptColor;?>;" <? if( $dept == $deptid ) { echo "selected"; } ?>><?=$deptName;?></option>
-						
-					<? } ?>
-					</select>
-				</td>
-			</tr>
-		</table>
-		</form>
-		<br /><br />
-		
-		<?
-		
-		$fetchName = "SELECT deptName FROM sms_departments WHERE deptid = '$dept' LIMIT 1";
+		$fetchName = "SELECT deptName FROM sms_departments WHERE deptid = $dept LIMIT 1";
 		$fetchResult = mysql_query( $fetchName );
 		$department = mysql_fetch_assoc( $fetchResult );
 		
 		?>
 		
-		<span class="fontTitle">Manage Existing <?=$department['deptName'];?> Positions</span>
-		
-		<? if( $dept == "1" ) { ?>
-		<br /><br />
-		<span class="yellow">
-		<b>Please Note:</b> Several functions that control emails in SMS use position ids 1 and 2 for the
-		Commanding Officer and Executive Officer respectively. Because of this, you cannot delete the first
-		two positions. If you change either of these positions to have different id numbers, some of the 
-		email functions will not work properly.<br /><br />
-		</span>
-		<? } ?>
+		<span class="fontTitle"><?=$department['deptName'];?> Positions</span><br /><br />
 	
 		<table cellpadding="0" cellspacing="3">
 			<?
