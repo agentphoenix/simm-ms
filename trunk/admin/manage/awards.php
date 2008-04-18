552ebd759fd415d95c5ef01747e647d6a8ac0b32
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-04-18 20:27:01 +0000

    updating the award management page and making award creation done through an ajax page

--- trunk/admin/manage/awards.php
+++ trunk/admin/manage/awards.php
@@ -10,47 +10,48 @@ File: admin/manage/awards.php
 Purpose: Page that moderates the awards
 
 System Version: 2.6.0
-Last Modified: 2008-01-15 1747 EST
+Last Modified: 2008-04-18 1625 EST
 **/
 
 /* access check */
-if( in_array( "m_awards", $sessionAccess ) ) {
-
+if(in_array("m_awards", $sessionAccess))
+{
 	/* set the page class and vars */
 	$pageClass = "admin";
 	$subMenuClass = "manage";
-	$update = $_POST['action_update_x'];
-	$create = $_POST['action_create_x'];
-	$delete = $_POST['action_delete_x'];
+	$query = FALSE;
+	$result = FALSE;
+	$awardid = FALSE;
 
 	/* if the POST action is update */
-	if( isset( $update ) ) {
+	if(isset($_POST['action_update_x']))
+	{	
+		if(isset($_POST['awardid']) && is_numeric($_POST['awardid']))
+		{
+			$awardid = $_POST['awardid'];
+		}
 		
-		/* define the POST variables */
-		$awardid = $_POST['awardid'];
-		$awardName = addslashes( $_POST['awardName'] );
-		$awardDesc = addslashes( $_POST['awardDesc'] );
-		$awardOrder = $_POST['awardOrder'];
-		$awardImage = $_POST['awardImage'];
-		$awardCat = $_POST['awardCat'];
+		$update = "UPDATE sms_awards SET awardName = %s, awardOrder = %d, awardImage = %s, awardDesc = %s, awardCat = %s ";
+		$update.= "WHERE awardid = $awardid LIMIT 1";
 		
-		/* do the update query */
-		$query = "UPDATE sms_awards SET ";
-		$query.= "awardName = '$awardName', awardOrder = '$awardOrder', ";
-		$query.= "awardImage = '$awardImage', awardDesc = '$awardDesc', ";
-		$query.= "awardOrder = '$awardOrder', awardCat = '$awardCat' ";
-		$query.= "WHERE awardid = '$awardid' LIMIT 1";
-		$result = mysql_query( $query );
+		$query = sprintf(
+			$update,
+			escape_string($_POST['awardName']),
+			escape_string($_POST['awardOrder']),
+			escape_string($_POST['awardImage']),
+			escape_string($_POST['awardDesc']),
+			escape_string($_POST['awardCat'])
+		);
+		
+		$result = mysql_query($query);
 		
 		/* optimize the table */
 		optimizeSQLTable( "sms_awards" );
 		
 		$action = "update";
-	
-	/* if the POST action is create */
-	} elseif( isset( $create ) ) {
-		
-		/* build the insert query */
+	}
+	elseif(isset($_POST['action_type']) && $_POST['action_type'] == "create")
+	{
 		$insert = "INSERT INTO sms_awards ( awardName, awardImage, awardDesc, awardOrder, awardCat ) ";
 		$insert.= "VALUES ( %s, %s, %s, %d, %s )";
 
@@ -64,39 +65,48 @@ if( in_array( "m_awards", $sessionAccess ) ) {
 			escape_string( $_POST['awardCat'] )
 		);
 
-		/* run the query */
 		$result = mysql_query( $query );
 		
 		/* optimize the table */
 		optimizeSQLTable( "sms_awards" );
 		
 		$action = "create";
-	
-	/* if the POST action is delete */
-	} elseif( $delete ) {
+	}
+	elseif(isset($_POST['action_delete_x']))
+	{
+		if(isset($_POST['awardid']) && is_numeric($_POST['awardid']))
+		{
+			$awardid = $_POST['awardid'];
+		}
 		
-		$awardid = $_POST['awardid'];
-		
-		/* do the delete query */
-		$query = "DELETE FROM sms_awards WHERE awardid = '$awardid' LIMIT 1";
+		$query = "DELETE FROM sms_awards WHERE awardid = $awardid LIMIT 1";
 		$result = mysql_query( $query );
 		
 		/* optimize the table */
 		optimizeSQLTable( "sms_awards" );
 		
 		$action = "delete";
-	
 	}
-	
-	/* strip the slashes */
-	$awardName = stripslashes( $awardName );
-	$awardDesc = stripslashes( $awardDesc );
 
 ?>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$("a[rel*=facebox]").click(function() {
+			var action = $(this).attr("myAction");
+			
+			jQuery.facebox(function() {
+				jQuery.get('admin/ajax/award_' + action + '.php', function(data) {
+					jQuery.facebox(data);
+				});
+			});
+			return false;
+		});
+	});
+</script>
 
 	<div class="body">
 		
-		<?
+		<?php
 		
 		$check = new QueryCheck;
 		$check->checkQuery( $result, $query );
@@ -108,58 +118,13 @@ if( in_array( "m_awards", $sessionAccess ) ) {
 		
 		?>
 	
-		<span class="fontTitle">Create New Award</span><br /><br />
-		You can create awards specific to your sim or use the awards from the fleet you are in. When you
-		create an award, you will need to have two images, one small image (that will be displayed in the
-		character bio) and a larger image (that will be displayed in the list of awards). When you create
-		the award, just type the name of the image (award.jpg) and not the full path.<br /><br />
+		<span class="fontTitle">Crew Award Management</span><br /><br />
+		Awards are a great way to reward members of your sim for their hard work, be that in character or out of character. While Anodyne doesn&rsquo;t provide awards by default in SMS, you can use the awards from the fleet you are in or even create awards specific to your sim. When creating an award, we recommend having two images, one small image (that will be displayed in the character bio) and a larger image (that will be displayed in the list of awards). When you create the award, just type the name of the image <em class="orange">(i.e. award.jpg)</em> and not the full path.<br /><br />
 	
-		<form method="post" action="<?=$webLocation;?>admin.php?page=manage&sub=awards">
-		<table cellpadding="0" cellspacing="3">
-			<tr>
-				<td colspan="2">
-					<span class="fontNormal"><b>Award</b></span><br />
-					<input type="text" class="name" name="awardName" maxlength="100" />
-				</td>
-				<td rowspan="3" valign="top" align="center" width="55%">
-					<span class="fontNormal"><b>Description</b></span><br />
-					<textarea name="awardDesc" class="desc" rows="6"></textarea>
-				</td>
-			</tr>
-			<tr>
-				<td width="50">
-					<span class="fontNormal"><b>Order</b></span><br />
-					<input type="text" class="order" name="awardOrder" maxlength="3" />
-				</td>
-				<td>
-					<span class="fontNormal"><b>Category</b></span><br />
-					<select name="awardCat">
-						<option value="ic">In character</option>
-						<option value="ooc">Out of character</option>
-						<option value="both">Both</option>
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td colspan="2">
-					<span class="fontNormal"><b>Image</b></span><br />
-					<span class="fontSmall">images/awards/</span><input type="text" class="image" name="awardImage" maxlength="50" />
-				</td>
-			</tr>
-			<tr>
-				<td colspan="2"></td>
-				<td align="center">
-					<input type="image" src="<?=path_userskin;?>buttons/create.png" class="button" name="action_create" value="Create" />
-				</td>
-			</tr>
-		</table>
-		</form>
-		<br /><br />
-		
-		<span class="fontTitle">Manage Existing Awards</span>
+		<a href="#" rel="facebox" myAction="add" class="add fontMedium"><strong>Add New Award &raquo;</strong></a><br /><br />
 	
 		<table cellpadding="0" cellspacing="3">
-			<?
+			<?php
 			
 			/* pull the ranks from the database */
 			$getAwards = "SELECT * FROM sms_awards ORDER BY awardOrder ASC";
