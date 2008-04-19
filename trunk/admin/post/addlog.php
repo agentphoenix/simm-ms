467a2c9e025fa636ed0ed2fc2a1c7af38128a4d6
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-04-19 17:43:44 +0000

    apache and security fixes for the add log page

--- trunk/admin/post/addlog.php
+++ trunk/admin/post/addlog.php
@@ -10,60 +10,75 @@ File: admin/post/addlog.php
 Purpose: Page to add a personal log
 
 System Version: 2.6.0
-Last Modified: 2007-08-21 0905 EST
+Last Modified: 2008-04-19 1319 EST
 **/
 
 /* access check */
-if( in_array( "p_addlog", $sessionAccess ) ) {
-
+if(in_array("p_addlog", $sessionAccess))
+{
 	/* set the page class */
 	$pageClass = "admin";
 	$subMenuClass = "post";
-	$add = $_POST['action_x'];
+	$query = FALSE;
+	$result = FALSE;
 	
-	/* do some advanced checking to make sure someone's not trying to do a SQL injection */
-	if( !empty( $_GET['id'] ) && preg_match( "/^\d+$/", $_GET['id'], $matches ) == 0 ) {
-		errorMessageIllegal( "add personal log page" );
-		exit();
-	} else {
-		/* set the GET variable */
-		$id = $_GET['id'];
-	}
-	
-	if( $add ) {
+	if(isset($_POST['action_x']))
+	{	
+		$today = getdate();
 		
-		/* add the necessary slashes */
-		$logTitle = addslashes( $_POST['logTitle'] );
-		$logContent = addslashes( $_POST['logContent'] );
-		$logAuthor = $_POST['logAuthor'];
+		$insert = "INSERT INTO sms_personallogs (logAuthor, logTitle, logContent, logPosted, logStatus) VALUES (%d, %s, %s, %d, %s)";
+		
+		$query = sprintf(
+			$insert,
+			escape_string($_POST['logAuthor']),
+			escape_string($_POST['logTitle']),
+			escape_string($_POST['logContent']),
+			escape_string($today[0]),
+			escape_string('activated')
+		);
 	
-		$postLog = "INSERT INTO sms_personallogs ( logid, logAuthor, logTitle, logContent, logPosted, logStatus ) ";
-		$postLog.= "VALUES ( '', '$logAuthor', '$logTitle', '$logContent', UNIX_TIMESTAMP(), 'activated' )";
-		$result = mysql_query( $postLog );
+		$result = mysql_query($query);
+		
+		/* update the last post for the author */
+		if(isset($_POST['logAuthor']) && is_numeric($_POST['logAuthor']))
+		{
+			$logAuthor = $_POST['logAuthor'];
+		}
+		else
+		{
+			$logAuthor = NULL;
+		}
+		
+		$update = "UPDATE sms_crew SET lastPost = %d WHERE crewid = $logAuthor LIMIT 1";
+		
+		$query2 = sprintf(
+			$update,
+			escape_string($today[0])
+		);
+		
+		$result2 = mysql_query($query2);
 		
 		/* optimize the table */
 		optimizeSQLTable( "sms_personallogs" );
-		
-		/* strip the slashes added for the query */
-		$logTitle = stripslashes( $logTitle );
-		$logContent = stripslashes( $logContent );
-		
-		/* update the player's last post timestamp */
-		$updateTimestamp = "UPDATE sms_crew SET lastPost = UNIX_TIMESTAMP() WHERE crewid = '$logAuthor' LIMIT 1";
-		$updateTimestampResult = mysql_query( $updateTimestamp );
-		
-		/*optimize the table */
 		optimizeSQLTable( "sms_crew" );
 		
 		/* if the user wants the email sent, do it */
-		if( $_POST['sendEmail'] == "y" ) {
-		
-			/** EMAIL THE LOG **/
+		if(isset($_POST['sendEmail']))
+		{
+			foreach($_POST as $key => $value)
+			{
+				$$key = $value;
+			}
+			
+			if(!is_numeric($logAuthor))
+			{
+				$logAuthor = NULL;
+			}
 			
 			/* set the email author */
 			$userFetch = "SELECT crew.crewid, crew.firstName, crew.lastName, crew.email, rank.rankName ";
 			$userFetch.= "FROM sms_crew AS crew, sms_ranks AS rank ";
-			$userFetch.= "WHERE crew.crewid = '$logAuthor' AND crew.rankid = rank.rankid LIMIT 1";
+			$userFetch.= "WHERE crew.crewid = $logAuthor AND crew.rankid = rank.rankid LIMIT 1";
 			$userFetchResult = mysql_query( $userFetch );
 			
 			while( $userFetchArray = mysql_fetch_array( $userFetchResult ) ) {
@@ -74,32 +89,29 @@ if( in_array( "p_addlog", $sessionAccess ) ) {
 				
 				$from = $rankName . " " . $firstName . " " . $lastName . " < " . $email . " >";
 				$name = $userFetchArray['rankName'] . " " . $userFetchArray['firstName'] . " " . $userFetchArray['lastName'];
-		
 			}
 			
 			/* define the variables */
-			$to = getCrewEmails( "emailLogs" );
+			$to = getCrewEmails("emailLogs");
 			$subject = $emailSubject . " " . $name . "'s Personal Log - " . stripslashes( $logTitle );
 			$message = stripslashes( $logContent );
 			
 			/* send the email */
 			mail( $to, $subject, $message, "From: " . $from . "\nX-Mailer: PHP/" . phpversion() );
-		
 		}
-		
 	}
 	
 	?>
 	
 	<div class="body">
-		
-		<?
+		<?php
 		
 		$check = new QueryCheck;
-		$check->checkQuery( $result, $postLog );
+		$check->checkQuery($result, $query);
 				
-		if( !empty( $check->query ) ) {
-			$check->message( "personal log", "add" );
+		if(!empty($check->query))
+		{
+			$check->message("personal log", "add");
 			$check->display();
 		}
 		
@@ -107,12 +119,7 @@ if( in_array( "p_addlog", $sessionAccess ) ) {
 		
 		<span class="fontTitle">Add Personal Log</span><br /><br />
 	
-		This page should be used in the event that a member of the crew has accidentally
-		posted incorrectly.  For instance, if a player has replied to one of the emails
-		sent out to the system instead of logging in and posting, you can copy and paste
-		the contents of their email into this form and put the entry into the system. For
-		all other personal logs, please use the <a href="<?=$webLocation;?>admin.php?page=post&sub=log">
-		Write Personal Log</a> page.<br /><br />
+		This page should be used in the event that a member of the crew has accidentally posted incorrectly.  For instance, if a player has replied to one of the emails sent out to the system instead of logging in and posting, you can copy and paste the contents of their email into this form and put the entry into the system. For all other personal logs, please use the <a href="<?=$webLocation;?>admin.php?page=post&sub=log"> Write Personal Log</a> page.<br /><br />
 	
 		<form method="post" action="<?=$webLocation;?>admin.php?page=post&sub=addlog">
 		<table>
@@ -121,7 +128,7 @@ if( in_array( "p_addlog", $sessionAccess ) ) {
 				<td>&nbsp;</td>
 				<td>
 					<select name="logAuthor">
-					<?
+					<?php
 					
 					/* query the users database */
 					$sql = "SELECT crew.crewid, crew.firstName, crew.lastName, rank.rankName ";
