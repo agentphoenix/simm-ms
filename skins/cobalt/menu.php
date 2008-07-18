b079cfe0e03f8cde10f1a380c7f96a39fc3b9529
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-07-18 21:29:50 +0000

    committing the new cobalt

--- skins/cobalt/menu.php
+++ skins/cobalt/menu.php
@@ -1,17 +1,27 @@
 <?php
 
 /**
-This is a necessary system file. Do not modify this page unless you are highly
-knowledgeable as to the structure of the system. Modification of this file may
-cause SMS to no longer function.
+Edits to this skin are permissible if the original credits stay intact.
 
 Author: David VanScott [ davidv@anodyne-productions.com ]
 File: skins/cobalt/menu.php
 Purpose: Page that creates the navigation menu for SMS 2
 
-Skin Version: 1.0
-Last Modified: 2008-03-28 1634 EST
+Skin Version: 2.0
+Last Modified: 2008-06-22 1715 EST
 **/
+
+$ship = array($shipPrefix, $shipName, $shipRegistry);
+
+foreach ($ship as $key => $value)
+{
+	if (empty($value))
+	{
+		unset($ship[$key]);
+	}
+}
+
+$shipString = implode(' ', $ship);
 
 ?>
 
@@ -26,30 +36,36 @@ Last Modified: 2008-03-28 1634 EST
 
 /* create a new instance of the menu class */
 $menu = new Menu;
-$menu->skin = $sessionDisplaySkin;
+
+if(isset($sessionCrewid))
+{
+	$menu->skin = $sessionDisplaySkin;
+}
 
 ?>
 
-<div id="mainNav">
-	<?php
-	
-	$menu->main();
-	
-	if( isset( $sessionCrewid ) ) {
-		$menu->user( $sessionCrewid );
-	}
-
-	?>
-</div>
-
 <div id="header">
-	<img src="skins/cobalt/images/header-left.jpg" alt="" style="float:left;" />
-	<img src="skins/cobalt/images/sms.jpg" alt="" style="float:left;" />
-	<img src="skins/cobalt/images/header-right.jpg" alt="" style="float:right;" />
-	<img src="skins/cobalt/images/anodyne.jpg" alt="" style="float:right;" />
+	<img src="<?=SKIN_PATH;?>images/header_left.png" border="0" alt="" style="float:left;" />
+	<img src="<?=SKIN_PATH;?>images/header_right.png" border="0" alt="" style="float:right;" />
+	<div class="head">
+		<img src="<?=SKIN_PATH;?>images/sms.png" border="0" alt="SMS2" style="float:right;" />
+		<?=$shipString;?>
+	</div>
+	<div class="mainNav">
+		<?php
+
+		$menu->main();
+
+		if(isset($sessionCrewid))
+		{
+			$menu->user($sessionCrewid);
+		}
+
+		?>
+	</div><br />
 </div>
 
-<div class="content">
+<div id="content" class="wrapper">
 	<div class="nav">
 		<div class="login">
 		<? if( isset( $sessionCrewid ) ) { ?>
@@ -58,17 +74,19 @@ $menu->skin = $sessionDisplaySkin;
 		<? } else { ?>
 			<form method="post" action="<?=$webLocation;?>login.php?action=checkLogin" class="login">
 				<b>Username</b><br />
-				<input type="text" name="username" size="12" class="loginSmallText" /><br /><br />
+				<input type="text" name="username" size="12" class="text" /><br /><br />
 				
 				<b>Password</b><br />
-				<input type="password" name="password" size="12" class="loginSmallText" /><br /><br />
+				<input type="password" name="password" size="12" class="text" /><br /><br />
 				
-				<input type="image" src="skins/cobalt/buttons/login-small.png" name="submit" class="buttonLogin" value="Login" />
+				<input type="image" src="<?=SKIN_PATH;?>buttons/login-small.png" name="submit" class="buttonSmall" value="Login" />
 			</form>
 			<br />
 			<a href="<?=$webLocation;?>login.php?action=reset">&laquo; Reset Password</a>
 		<? } ?>
-		</div>
+		<br /><br />
+		<? include_once( 'framework/stardate.php' ); ?>
+		</div> <!-- close the .login layer -->
 		<br />
 
 		<?
@@ -107,4 +125,4 @@ $menu->skin = $sessionDisplaySkin;
 		}
 		
 		?>
-	</div>
+	</div> <!-- close the .nav layer -->
