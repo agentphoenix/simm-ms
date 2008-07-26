bca8fac189d6dcf818f57e35bb1ee5f15a43bf00
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-07-26 22:28:51 +0000

    tagging the 2.6 release

---
+++ tags/sms-2.6.0/admin/post/mission.php
@@ -0,0 +1,447 @@
+<?php
+
+/**
+This is a necessary system file. Do not modify this page unless you are highly
+knowledgeable as to the structure of the system. Modification of this file may
+cause SMS to no longer function.
+
+Author: David VanScott [ davidv@anodyne-productions.com ]
+File: admin/post/mission.php
+Purpose: Page to post a mission entry
+
+System Version: 2.6.0
+Last Modified: 2008-06-30 1931 EST
+**/
+
+/* access check */
+if(in_array("p_mission", $sessionAccess))
+{	
+	/* set the page class */
+	$pageClass = "admin";
+	$subMenuClass = "post";
+	$query = FALSE;
+	$result = FALSE;
+	$today = getdate();
+	
+	/* check to make sure the id is legit */
+	if(isset($_GET['id']) && is_numeric($_GET['id']))
+	{
+		$id = $_GET['id'];
+	}
+	else
+	{
+		$id = NULL;
+	}
+	
+	if(isset($_POST['action_post_x']))
+	{
+		$getModerated = "SELECT crewid FROM sms_crew WHERE moderatePosts = 'y'";
+		$getModeratedResult = mysql_query( $getModerated );
+		$modArray = array();
+	
+		while( $moderated = mysql_fetch_array( $getModeratedResult ) ) {
+			extract( $moderated, EXTR_OVERWRITE );
+	
+			$modArray[] = $moderated['0'];
+		}
+		/** end moderation check **/
+		
+		if(count($modArray) > 0 && in_array($sessionCrewid, $modArray)) {
+			$postStatus = "pending";
+		} elseif($sessionCrewid == "" || $sessionCrewid == 0) {
+			$postStatus = "pending";
+		} elseif($sessionCrewid > 0) {
+			$postStatus = "activated";
+		} if($_POST['postMission'] == "") {
+			$postStatus = "pending";
+		}
+	
+		if(!isset($id))
+		{
+			$insert = "INSERT INTO sms_posts (postAuthor, postTitle, postLocation, postTimeline, postContent, postPosted, postMission, ";
+			$insert.= "postStatus, postTag) VALUES (%d, %s, %s, %s, %s, %d, %d, %s, %s)";
+			
+			$query = sprintf(
+				$insert,
+				escape_string($sessionCrewid),
+				escape_string($_POST['postTitle']),
+				escape_string($_POST['postLocation']),
+				escape_string($_POST['postTimeline']),
+				escape_string($_POST['postContent']),
+				escape_string($today[0]),
+				escape_string($_POST['postMission']),
+				escape_string($postStatus),
+				escape_string($_POST['postTag'])
+			);
+		}
+		else
+		{
+			$update = "UPDATE sms_posts SET postTitle = %s, postLocation = %s, postTimeline = %s, postContent = %s, postPosted = %d, ";
+			$update.= "postStatus = %s, postTag = %s WHERE postid = $id LIMIT 1";
+			
+			$query = sprintf(
+				$update,
+				escape_string($_POST['postTitle']),
+				escape_string($_POST['postLocation']),
+				escape_string($_POST['postTimeline']),
+				escape_string($_POST['postContent']),
+				escape_string($today[0]),
+				escape_string($postStatus),
+				escape_string($_POST['postTag'])
+			);
+		}
+	
+		$result = mysql_query($query);
+		
+		$action = "post";
+		
+		/* update the player's last post time stamp */
+		$updateTimestamp = "UPDATE sms_crew SET lastPost = UNIX_TIMESTAMP() WHERE crewid = $sessionCrewid LIMIT 1";
+		$updateTimestampResult = mysql_query($updateTimestamp);
+		
+		/* optimize the table */
+		optimizeSQLTable( "sms_crew" );
+		optimizeSQLTable( "sms_posts" );
+		
+		/** EMAIL THE POST **/
+		
+		/* set the email author */
+		$userFetch = "SELECT crew.crewid, crew.firstName, crew.lastName, crew.email, rank.rankShortName ";
+		$userFetch.= "FROM sms_crew AS crew, sms_ranks AS rank ";
+		$userFetch.= "WHERE crew.crewid = '$sessionCrewid' AND crew.rankid = rank.rankid LIMIT 1";
+		$userFetchResult = mysql_query( $userFetch );
+		
+		while( $userFetchArray = mysql_fetch_array( $userFetchResult ) ) {
+			extract( $userFetchArray, EXTR_OVERWRITE );
+		}
+		
+		$firstName = str_replace( "'", "", $firstName );
+		$lastName = str_replace( "'", "", $lastName );
+		
+		$from = $rankShortName . " " . $firstName . " " . $lastName . " < " . $email . " >";
+		
+		foreach($_POST as $k => $v)
+		{
+			$$k = $v;
+		}
+		
+		switch($postStatus)
+		{
+			case 'activated':
+				$to = getCrewEmails( "emailPosts" );
+				$subject = $emailSubject . " " . printMissionTitle($postMission) . " - " . $postTitle;
+				$message = "A Post By " . displayEmailAuthors($postAuthors, 'noLink') . "\r\n";
+				$message.= "Location: " . stripslashes($postLocation) . "\r\n";
+				$message.= "Timeline: " . stripslashes($postTimeline) . "\r\n";
+				$message.= "Tag: " . stripslashes($postTag) . "\r\n\r\n";
+				$message.= stripslashes($postContent);
+				break;
+				
+			case 'pending':
+				$to = printCOEmail();
+				$subject = $emailSubject . " " . printMissionTitle($postMission) . " - " . $postTitle . " (Awaiting Approval)";
+				$message = "A Post By " . displayEmailAuthors($postAuthors, 'noLink') . "\r\n";
+				$message.= "Location: " . stripslashes($postLocation) . "\r\n";
+				$message.= "Timeline: " . stripslashes($postTimeline) . "\r\n";
+				$message.= "Tag: " . stripslashes($postTag) . "\r\n\r\n";
+				$message.= stripslashes($postContent) . "\r\n\r\n";
+				$message.= "Please log in to approve this post.  " . $webLocation . "login.php?action=login";
+				break;
+		}
+		
+		/* send the email */
+		mail( $to, $subject, $message, "From: " . $from . "\nX-Mailer: PHP/" . phpversion() );
+	}
+	elseif(isset($_POST['action_save_x']))
+	{
+		if(!isset($id))
+		{
+			$insert = "INSERT INTO sms_posts (postAuthor, postTitle, postLocation, postTimeline, postContent, postPosted, postMission, ";
+			$insert.= "postStatus, postTag) VALUES (%d, %s, %s, %s, %s, %d, %d, %s, %s)";
+			
+			$query = sprintf(
+				$insert,
+				escape_string($sessionCrewid),
+				escape_string($_POST['postTitle']),
+				escape_string($_POST['postLocation']),
+				escape_string($_POST['postTimeline']),
+				escape_string($_POST['postContent']),
+				escape_string($today[0]),
+				escape_string($_POST['postMission']),
+				escape_string('saved'),
+				escape_string($_POST['postTag'])
+			);
+		}
+		else
+		{
+			$update = "UPDATE sms_posts SET postTitle = %s, postLocation = %s, postTimeline = %s, postContent = %s, postPosted = %d, ";
+			$update.= "postStatus = %s, postTag = %s WHERE postid = $id LIMIT 1";
+			
+			$query = sprintf(
+				$update,
+				escape_string($_POST['postTitle']),
+				escape_string($_POST['postLocation']),
+				escape_string($_POST['postTimeline']),
+				escape_string($_POST['postContent']),
+				escape_string($today[0]),
+				escape_string('saved'),
+				escape_string($_POST['postTag'])
+			);
+		}
+	
+		$result = mysql_query($query);
+		
+		/* optimize the table */
+		optimizeSQLTable( "sms_posts" );
+		
+		$action = "save";
+	}
+	elseif(isset($_POST['action_delete_x']))
+	{
+		$query = "DELETE FROM sms_posts WHERE postid = $id LIMIT 1";
+		$result = mysql_query($query);
+	
+		/* optimize the table */
+		optimizeSQLTable( "sms_posts" );
+		
+		$action = "delete";
+	}
+	
+?>
+	
+	<div class="body">
+		<?php
+		
+		$check = new QueryCheck;
+		$check->checkQuery( $result, $query );
+				
+		if( !empty( $check->query ) ) {
+			$check->message( "mission entry", $action );
+			$check->display();
+		}
+		
+		if( $useMissionNotes == "y" && !isset($_POST['action_delete_x']))
+		{
+		
+		?>
+			
+		<script type="text/javascript">
+			$(document).ready(function() {
+				$('a#toggle').click(function() {
+					$('#notes').slideToggle('slow');
+					return false;
+				});
+			});
+		</script>
+
+		<div class="update notify-normal">
+			<a href="#" id="toggle" class="fontNormal" style="float:right;margin-right:.5em;">Show/Hide</a>
+			
+			<span class="fontTitle">Mission Notes</span>
+			<div id="notes" style="display:none;">
+				<br />
+				<?
+
+				$getNotes = "SELECT missionNotes FROM sms_missions WHERE missionStatus = 'current' LIMIT 1";
+				$getNotesResult = mysql_query( $getNotes );
+				$notes = mysql_fetch_array( $getNotesResult );
+
+				printText( $notes['0'] );
+
+				?>
+			</div>
+		</div><br />
+		<? } ?>
+	
+		<span class="fontTitle">Post Mission Entry</span><br /><br />
+	
+		<? if(!isset($id)) { ?>
+		
+		<form method="post" action="<?=$webLocation;?>admin.php?page=post&sub=mission">
+		<table>
+			<tr>
+				<td class="narrowLabel tableCellLabel">Author</td>
+				<td>&nbsp;</td>
+				<td><? printCrewName( $sessionCrewid, "rank", "noLink" ); ?></td>
+			</tr>
+			<tr>
+				<td class="narrowLabel tableCellLabel">Mission</td>
+				<td>&nbsp;</td>
+				<td class="fontNormal">
+					<?
+					
+					$missionTitle = "SELECT missionid, missionTitle FROM sms_missions WHERE missionStatus = 'current' LIMIT 1";
+					$missionTitleResult = mysql_query( $missionTitle );
+					$missionCount = mysql_num_rows( $missionTitleResult );
+					
+					while( $titleArray = mysql_fetch_array( $missionTitleResult ) ) {
+						extract( $titleArray, EXTR_OVERWRITE );
+					}
+					
+					if( $missionCount == 0 ) {
+						echo "<b>You must <a href='" . $webLocation . "admin.php?page=manage&sub=missions'>create a mission</a> before posting!</b>";
+					} else {
+					
+					?>
+					
+					<a href="<?=$webLocation;?>index.php?page=mission&id=<?=$missionid;?>"><? printText( $missionTitle ); ?></a>
+					<input type="hidden" name="postMission" value="<?=$missionid;?>" />
+					<? } ?>
+				</td>
+			</tr>
+			<tr>
+				<td class="narrowLabel tableCellLabel">Title</td>
+				<td>&nbsp;</td>
+				<td><input type="text" class="name" name="postTitle" style="font-weight:bold;" length="100" /></td>
+			</tr>
+			<tr>
+				<td class="narrowLabel tableCellLabel">Location</td>
+				<td>&nbsp;</td>
+				<td><input type="text" class="name" name="postLocation" style="font-weight:bold;" length="100" /></td>
+			</tr>
+			<tr>
+				<td class="narrowLabel tableCellLabel">Timeline</td>
+				<td>&nbsp;</td>
+				<td><input type="text" class="name" name="postTimeline" style="font-weight:bold;" length="100" /></td>
+			</tr>
+			<tr>
+				<td class="narrowLabel tableCellLabel">Tag</td>
+				<td>&nbsp;</td>
+				<td><input type="text" class="name" name="postTag" style="font-weight:bold;" length="100" /></td>
+			</tr>
+			<tr>
+				<td class="narrowLabel tableCellLabel">Content</td>
+				<td>&nbsp;</td>
+				<td><textarea name="postContent" class="desc" rows="15"></textarea></td>
+			</tr>
+			<tr>
+				<td colspan="3" height="20"></td>
+			</tr>
+			
+			<? if( $missionCount > 0 ) { ?>
+			<tr>
+				<td colspan="2">&nbsp;</td>
+				<td>
+					<input type="image" src="<?=path_userskin;?>buttons/save.png" name="action_save" value="Save" class="button" />
+					&nbsp;&nbsp;
+					<script type="text/javascript">
+						document.write( "<input type=\"image\" src=\"<?=path_userskin;?>buttons/post.png\" name=\"action_post\" value=\"Post\" class=\"button\" onClick=\"javascript:return confirm('Are you sure you want to post this mission entry?')\" />" );
+					</script>
+					<noscript>
+						<input type="image" src="<?=path_userskin;?>buttons/post.png" name="action_post" class="button" value="Post" />
+					</noscript>
+				</td>
+			</tr>
+			<? } ?>
+		</table>
+		</form>
+		
+		<?
+	
+		}
+		elseif(isset($id) && !isset($_POST['action_delete_x']))
+		{
+			$getPost = "SELECT * FROM sms_posts WHERE postid = $id LIMIT 1";
+			$getPostResults = mysql_query( $getPost );
+			
+			while( $fetchPost = mysql_fetch_array( $getPostResults ) ) {
+				extract( $fetchPost, EXTR_OVERWRITE );
+			}
+	
+		?>
+	
+		<form method="post" action="<?=$webLocation;?>admin.php?page=post&sub=mission&id=<?=$id;?>">
+		<table>
+			<tr>
+				<td class="narrowLabel tableCellLabel">Author</td>
+				<td>&nbsp;</td>
+				<td><? displayAuthors( $postid, "noLink" ); ?></td>
+			</tr>
+			<tr>
+				<td class="narrowLabel tableCellLabel">Mission</td>
+				<td>&nbsp;</td>
+				<td class="fontNormal">
+					<?
+					
+					$missionTitle = "SELECT missionid, missionTitle FROM sms_missions WHERE missionStatus = 'current' LIMIT 1";
+					$missionTitleResult = mysql_query( $missionTitle );
+					$missionCount = mysql_num_rows( $missionTitleResult );
+					
+					while( $titleArray = mysql_fetch_array( $missionTitleResult ) ) {
+						extract( $titleArray, EXTR_OVERWRITE );
+					}
+					
+					if( $missionCount == 0 ) {
+						echo "<b>You must <a href='" . $webLocation . "admin.php?page=manage&sub=missions'>create a mission</a> before posting!</b>";
+					} else {
+					
+					?>
+					
+					<a href="<?=$webLocation;?>index.php?page=mission&id=<?=$missionid;?>"><?=$missionTitle;?></a>
+					<input type="hidden" name="postMission" value="<?=$missionid;?>" />
+					<? } ?>
+				</td>
+			</tr>
+			<tr>
+				<td class="narrowLabel tableCellLabel">Title</td>
+				<td>&nbsp;</td>
+				<td><input type="text" class="name" name="postTitle" style="font-weight:bold;" length="100" value="<?=stripslashes( $postTitle );?>" /></td>
+			</tr>
+			<tr>
+				<td class="narrowLabel tableCellLabel">Location</td>
+				<td>&nbsp;</td>
+				<td><input type="text" class="name" name="postLocation" style="font-weight:bold;" length="100" value="<?=stripslashes( $postLocation );?>" /></td>
+			</tr>
+			<tr>
+				<td class="narrowLabel tableCellLabel">Timeline</td>
+				<td>&nbsp;</td>
+				<td><input type="text" class="name" name="postTimeline" style="font-weight:bold;" length="100" value="<?=stripslashes( $postTimeline );?>" /></td>
+			</tr>
+			<tr>
+				<td class="narrowLabel tableCellLabel">Tag</td>
+				<td>&nbsp;</td>
+				<td><input type="text" class="name" name="postTag" style="font-weight:bold;" length="100" value="<?=stripslashes( $postTag );?>" /></td>
+			</tr>
+			<tr>
+				<td class="narrowLabel tableCellLabel">Content</td>
+				<td>&nbsp;</td>
+				<td><textarea name="postContent" class="desc" rows="15"><?=stripslashes( $postContent );?></textarea></td>
+			</tr>
+			<tr>
+				<td colspan="3" height="20"></td>
+			</tr>
+			
+			<? if( $missionCount > 0 && $sessionCrewid == $postAuthor && $postStatus == "saved" ) { ?>
+			<tr>
+				<td colspan="2">&nbsp;</td>
+				<td>
+					<script type="text/javascript">
+						document.write( "<input type=\"image\" src=\"<?=path_userskin;?>buttons/delete.png\" name=\"action_delete\" value=\"Delete\" class=\"button\" onClick=\"javascript:return confirm('This action is permanent and cannot be undone. Are you sure you want to delete this saved post?')\" />" );
+					</script>
+					<noscript>
+						<input type="image" src="<?=path_userskin;?>buttons/delete.png" name="action_delete" value="Delete" class="button" />
+					</noscript>
+					&nbsp;&nbsp;
+					<input type="image" src="<?=path_userskin;?>buttons/save.png" name="action_save" value="Save" class="button" />
+					&nbsp;&nbsp;
+					<script type="text/javascript">
+						document.write( "<input type=\"image\" src=\"<?=path_userskin;?>buttons/post.png\" name=\"action_post\" value=\"Post\" class=\"button\" onClick=\"javascript:return confirm('Are you sure you want to post this saved mission entry?')\" />" );
+					</script>
+					<noscript>
+						<input type="image" src="<?=path_userskin;?>buttons/post.png" name="action_post" class="button" value="Post" />
+					</noscript>
+				</td>
+			</tr>
+			<? } ?>
+		</table>
+		</form>
+		
+		<? } elseif(isset($id) && isset($_POST['action_delete_x'])) { ?>
+	
+		Please return to the Control Panel to continue.
+	
+		<? } ?>
+		
+	</div>
+
+<? } else { errorMessage( "mission posting" ); } ?>
---
+++ tags/sms-2.6.0/update.php
@@ -0,0 +1,197 @@
+<?php
+
+/**
+This is a necessary system file. Do not modify this page unless you are highly
+knowledgeable as to the structure of the system. Modification of this file may
+cause the system to no longer function.
+
+Author: David VanScott [ davidv@anodyne-productions.com ]
+File: update.php
+Purpose: New update system that will dynamically pull the right update file based
+	on what version of the system is in use
+
+System Version: 2.6.0
+Last Modified: 2008-06-15 1432 EST
+**/
+
+/* define the step var */
+if (isset($_GET['step']) && is_numeric($_GET['step']))
+{
+	$step = $_GET['step'];
+}
+else
+{
+	$step = 1;
+}
+
+if (isset($_GET['version']) && is_numeric($_GET['version']))
+{
+	$urlVersion = $_GET['version'];
+}
+else
+{
+	$urlVersion = FALSE;
+}
+
+/* array for controlling trailing zeroes */
+$versionArray = array(
+	0 => 20,
+	1 => 21,
+	2 => 22,
+	3 => 23,
+	4 => 24,
+	5 => 25,
+	6 => 26
+);
+
+/* array with all the possible versions */
+$versionsArray = array(
+	230,
+	231,
+	240,
+	241,
+	242,
+	243,
+	244,
+	250,
+	251,
+	2511,
+	252,
+	253,
+	254,
+	255,
+	256
+);
+
+/* count the number of items in the versions array */
+$versionsCount = count($versionsArray);
+
+/* make sure the version is formatted right */
+if (in_array($urlVersion, $versionArray))
+{
+	$urlVersion = $urlVersion . 0;
+}
+
+/* destroy the session if it exists */
+session_unset();
+
+/* pull in the globals files */
+require_once('framework/functionsGlobal.php');
+include_once('framework/functionsUtility.php');
+
+switch($step)
+{
+	case 2:
+	
+		/* if there's not version specified in the URL, try to find out what version it is */
+		if( !isset( $urlVersion ) ) {
+			
+			$getUpdateVersion = "SELECT sysVersion FROM sms_system WHERE sysid = 1 LIMIT 1";
+			$getUpdateVersionResult = mysql_query( $getUpdateVersion );
+			$updateVersion = mysql_fetch_array( $getUpdateVersionResult );
+			
+			/* make sure the periods have been removed */
+			$urlVersion = str_replace(".", "", $updateVersion[0]);
+	
+		}
+		
+		if ($urlVersion == 2441)
+		{
+			$urlVersion = 244;
+		}
+		
+		/** PULL IN THE UPDATE FILE **/
+		foreach ($versionsArray as $key1 => $value1)
+		{
+			/* if we're at the right point in the array, start the update code */
+			if ($urlVersion == $value1)
+			{
+				/* duplicate the versions array */
+				$versionsArrayNew = $versionsArray;
+				
+				/* slice the array so it only includes the files that need to be used */
+				$versionsArrayNew = array_slice($versionsArray, $key1);
+				
+				/* loop through the new array and pull in the update files */
+				foreach ($versionsArrayNew as $key2 => $value2)
+				{
+					/* pull in the update files sequentially */
+					/* echo "update/" . $value2 . ".php<br />"; */
+					include_once( "update/" . $value2 . ".php" );
+				}
+			}
+		}
+	
+		/** UPDATE THE VERSION IN THE DATABASE **/
+		$updateVersion = "UPDATE sms_system SET sysVersion = '2.6.0', sysBaseVersion = '2.6', ";
+		$updateVersion.= "sysIncrementVersion = '.0', sysLaunchStatus = 'n' WHERE sysid = 1 LIMIT 1";
+		$updateVersionResult = mysql_query( $updateVersion );
+		
+		break;
+	case 3:
+
+		/** PULL IN THE UPDATE FILE **/
+		require_once( "update/starbase.php" );
+		
+		break;
+
+}
+
+?>
+
+<html>
+<head>
+	<title>SMS 2.6 :: Update</title>
+	<link rel="stylesheet" href="update/update.css" type="text/css" />
+</head>
+<body>
+	<div id="install">	
+		<div class="header">
+			<img src="update/update.jpg" alt="" border="0" />
+		</div> <!-- close .header -->
+		<div class="content">
+			
+			<? if( $step == "1" ) { ?>
+			
+			SMS 2.6 is a major update to SMS which not only fixes a handful of existing bugs, but it also enhances existing features as well as offering brand new functionality. A complete list change log is available after the system has been updated in the Reports > Version History page. Some of the major changes in this version include:
+			
+			<ul>
+				<li>Departmental Databases</li>
+				<li>Ability for users to edit their own posts and logs</li>
+				<li>Ability for admins to set the group access defaults (CO, XO, Department Head, Standard Player)</li>
+				<li>Private news items</li>
+				<li>Built-in stardate script</li>
+				<li>Personalized menu</li>
+				<li>New awards system that sends nominated awards to a queue for CO review</li>
+				<li>Better Apache compatibility</li>
+				<li>Brand new activation page</li>
+				<li>Slick new manifest page</li>
+				<li>Cleaned up Private Messages</li>
+				<li>Using the jQuery Javascript library for better tabs, lightbox functionality, and modal windows</li>
+				<li>Two dozen existing bugs fixed</li>
+				<li>Better security</li>
+				<li>And much more!</li>
+			</ul>
+			
+			<h1><a href="update.php?step=2&version=<?=$urlVersion;?>">Next Step &raquo;</a></h1>
+			
+			<? } elseif( $step == "2" ) { ?>
+			
+			The changes have been made to your system.  Please make sure the necessary files are uploaded to your server.  If you still experience problems with any of the issues that have been fixed, please report them on the Anodyne Support Forum.<br /><br />
+			
+			One of the changes to SMS 2.6 required that the ranks table be blown away and re-built. The script makes every effort to update every crew member from the old rank data to the new rank data, but you may find that a handful of crew members have the wrong rank (and therefore the wrong rank image) and may require manual editing. We apologize for this inconvenience.<br /><br />
+			
+			<b>Note:</b> If you were logged in to your site, you may receive an error why trying to go to the Control Panel. To correct this, please log back in to your site.
+
+			<h1>
+				<a href="<?=$webLocation;?>index.php?page=main">Return to your site &raquo;</a>
+			</h1>
+			
+			<? } ?>
+		</div>
+		<div class="footer">
+			Copyright &copy; 2005-<?php echo date('Y'); ?> by <a href="http://www.anodyne-productions.com/" target="_blank">Anodyne Productions</a>
+		</div> <!-- close .footer -->
+	</div> <!-- close #install -->
+</body>
+</html>
