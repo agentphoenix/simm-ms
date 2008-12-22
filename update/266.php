d93684351afc9b6a4bd857088c520b96343fb449
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-12-22 16:41:34 +0000

    fixed typo in a position description for fresh installs

--- update/266.php
+++ update/266.php
@@ -3,7 +3,7 @@
 Author: David VanScott [ davidv@anodyne-productions.com ]
 File: update/266.php
 Purpose: Update to 2.6.7
-Last Modified: 2008-12-17 0824 EST
+Last Modified: 2008-12-22 1140 EST
 **/
 
 /*
@@ -28,6 +28,6 @@ mysql_query("UPDATE sms_menu_items SET menuTitle = 'Current Mission(s)' WHERE me
 | for this release.
 |
 */
-mysql_query("INSERT INTO sms_system_versions ( `version`, `versionDate`, `versionShortDesc`, `versionDesc` ) VALUES ('2.6.7', '', 'This release...', 'Fixed bug where rank management would only build department class menus for departments that were being displayed, causing issues for unused ranks that were updated;Fixed bug with next/previous links where they didn\'t respect when a log/post/news item was posted;Added the ability to run multiple missions simultaneously;Added more specific information to the award nomination emails (nominee, nominated by, award, and reason) so it isn\'t just a nondescript notice;Fixed bug where quotation marks couldn\'t be used in some bio fields;Fixed bug where SMS would still try to run the update check class even if an admin had set their update notification level to none')");
+mysql_query("INSERT INTO sms_system_versions ( `version`, `versionDate`, `versionShortDesc`, `versionDesc` ) VALUES ('2.6.7', '', 'This release...', 'Fixed bug where rank management would only build department class menus for departments that were being displayed, causing issues for unused ranks that were updated;Fixed bug with next/previous links where they didn\'t respect when a log/post/news item was posted;Added the ability to run multiple missions simultaneously;Added more specific information to the award nomination emails (nominee, nominated by, award, and reason) so it isn\'t just a nondescript notice;Fixed bug where quotation marks couldn\'t be used in some bio fields;Fixed bug where SMS would still try to run the update check class even if an admin had set their update notification level to none;Fixed typo in a position description for fresh installs')");
 
 ?>
