06e4246459810b044631d745c6ee57666fa6b4dc
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-12-27 19:09:21 +0000

    tagging sms 2.6.7

---
+++ tags/sms-2.6.7/update.php
@@ -0,0 +1,191 @@
+<?php
+
+/**
+This is a necessary system file. Do not modify this page unless you are highly
+knowledgeable as to the structure of the system. Modification of this file may
+cause the system to no longer function.
+
+Author: David VanScott [ davidv@anodyne-productions.com ]
+File: update.php
+Purpose: New update system that will dynamically pull the right update file based
+	on what version of the system is in use
+
+System Version: 2.6.7
+Last Modified: 2008-12-22 1140 EST
+**/
+
+/* define the step var */
+if (isset($_GET['step']) && is_numeric($_GET['step']))
+{
+	$step = $_GET['step'];
+}
+else
+{
+	$step = 1;
+}
+
+if (isset($_GET['version']) && is_numeric($_GET['version']))
+{
+	$urlVersion = $_GET['version'];
+}
+else
+{
+	$urlVersion = FALSE;
+}
+
+/* array for controlling trailing zeroes */
+$versionArray = array(
+	0 => 20,
+	1 => 21,
+	2 => 22,
+	3 => 23,
+	4 => 24,
+	5 => 25,
+	6 => 26
+);
+
+/* array with all the possible versions */
+$versionsArray = array(
+	230,
+	231,
+	240,
+	241,
+	242,
+	243,
+	244,
+	250,
+	251,
+	2511,
+	252,
+	253,
+	254,
+	255,
+	256,
+	260,
+	261,
+	262,
+	263,
+	264,
+	265,
+	266
+);
+
+/* count the number of items in the versions array */
+$versionsCount = count($versionsArray);
+
+/* make sure the version is formatted right */
+if (in_array($urlVersion, $versionArray))
+{
+	$urlVersion = $urlVersion . 0;
+}
+
+/* destroy the session if it exists */
+session_unset();
+
+/* pull in the globals files */
+require_once('framework/functionsGlobal.php');
+include_once('framework/functionsUtility.php');
+
+switch($step)
+{
+	case 2:
+	
+		/* if there's not version specified in the URL, try to find out what version it is */
+		if( $urlVersion === FALSE ) {
+			
+			$getUpdateVersion = "SELECT sysVersion FROM sms_system WHERE sysid = 1 LIMIT 1";
+			$getUpdateVersionResult = mysql_query( $getUpdateVersion );
+			$updateVersion = mysql_fetch_array( $getUpdateVersionResult );
+			
+			/* make sure the periods have been removed */
+			$urlVersion = str_replace(".", "", $updateVersion[0]);
+	
+		}
+		
+		if ($urlVersion == 2441)
+		{
+			$urlVersion = 244;
+		}
+		
+		/** PULL IN THE UPDATE FILE **/
+		foreach ($versionsArray as $key1 => $value1)
+		{
+			/* if we're at the right point in the array, start the update code */
+			if ($urlVersion == $value1)
+			{
+				/* duplicate the versions array */
+				$versionsArrayNew = $versionsArray;
+				
+				/* slice the array so it only includes the files that need to be used */
+				$versionsArrayNew = array_slice($versionsArray, $key1);
+				
+				/* loop through the new array and pull in the update files */
+				foreach ($versionsArrayNew as $key2 => $value2)
+				{
+					/* pull in the update files sequentially */
+					include_once( "update/" . $value2 . ".php" );
+				}
+			}
+		}
+	
+		/** UPDATE THE VERSION IN THE DATABASE **/
+		$updateVersion = "UPDATE sms_system SET sysVersion = '2.6.7', sysBaseVersion = '2.6', ";
+		$updateVersion.= "sysIncrementVersion = '.7', sysLaunchStatus = 'n' WHERE sysid = 1 LIMIT 1";
+		$updateVersionResult = mysql_query( $updateVersion );
+		
+		break;
+	case 3:
+
+		/** PULL IN THE UPDATE FILE **/
+		require_once("update/starbase.php");
+		
+		break;
+}
+
+?>
+
+<html>
+<head>
+	<title>SMS 2.6 :: Update</title>
+	<link rel="stylesheet" href="update/update.css" type="text/css" />
+</head>
+<body>
+	<div id="install">	
+		<div class="header">
+			<img src="update/update.jpg" alt="" border="0" />
+		</div> <!-- close .header -->
+		<div class="content">
+			
+			<? if( $step == "1" ) { ?>
+			
+			SMS 2.6.7 is an update to latest release of SMS which updates the system with the following changes:
+			
+			<ul>
+				<li>Fixed bug in rank management where department class menu would only be built with departments that are displayed. This can cause issues if you update a rank that is part of a department no longer being displayed, thus causing it to appear in the first department class and throwing off the ranks.</li>
+				<li>Fixed bug where next/previous links were ignoring the date posted and just using the ID, resulting in incorrect ordering when moving through posts with the links</li>
+				<li>Added the ability to run multiple missions simultaneously</li>
+				<li>Added more specific information to the award nomination emails (nominee, nominated by, award, and reason) so it isn&rsquo;t just a nondescript notice</li>
+				<li>Fixed bug where quotation marks couldn&rsquo;t be used in some bio fields</li>
+				<li>Fixed bug where SMS would try to start the version check class even if an admin had set their notification level to none</li>
+				<li>Fixed typo in a position description for fresh installs</li>
+				<li>Fixed the join agree page to say terms of use instead of disclaimer as the former is more accurate</li>
+			</ul>
+			
+			<h1><a href="update.php?step=2&version=<?=$urlVersion;?>">Next Step &raquo;</a></h1>
+			
+			<? } elseif( $step == "2" ) { ?>
+			
+			The changes have been made to your system.  If you still experience problems with any of the issues that have been fixed, please report them on the Anodyne Support Forum.
+
+			<h1>
+				<a href="<?=$webLocation;?>index.php?page=main">Return to your site &raquo;</a>
+			</h1>
+			
+			<? } ?>
+		</div>
+		<div class="footer">
+			Copyright &copy; 2005-<?php echo date('Y'); ?> by <a href="http://www.anodyne-productions.com/" target="_blank">Anodyne Productions</a>
+		</div> <!-- close .footer -->
+	</div> <!-- close #install -->
+</body>
+</html>
