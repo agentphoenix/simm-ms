24c2d9827f49b161abdafe8e89487bb451427ec5
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2008-12-29 20:28:44 +0000

    updating changed files doc and the 2.6.7 update file

--- trunk/update/267.php
+++ trunk/update/267.php
@@ -3,7 +3,7 @@
 Author: David VanScott [ davidv@anodyne-productions.com ]
 File: update/267.php
 Purpose: Update to 2.6.8
-Last Modified: 2008-12-28 2129 EST
+Last Modified: 2008-12-29 1525 EST
 **/
 
 /*
@@ -18,6 +18,6 @@ Last Modified: 2008-12-28 2129 EST
 | for this release.
 |
 */
-mysql_query("INSERT INTO sms_system_versions ( `version`, `versionDate`, `versionShortDesc`, `versionDesc` ) VALUES ('2.6.8', '', 'This release...', 'Fixed bug where saved mission posts and joint posts showed the missions in the dropdown twice')");
+mysql_query("INSERT INTO sms_system_versions ( `version`, `versionDate`, `versionShortDesc`, `versionDesc` ) VALUES ('2.6.8', '', 'This release...', 'Fixed bug where saved mission posts and joint posts showed the missions in the dropdown twice;Fixed bug where departments and positions on the Departments and Positions page were not ordered by their respective orders, but by ID instead')");
 
 ?>
