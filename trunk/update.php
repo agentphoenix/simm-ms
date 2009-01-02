cce23034cd1fe7833a04b08c0bf38ca1edddb0f6
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2009-01-02 20:52:57 +0000

    fixed bug where commas at the end of the character images string caused an additional image to be counted, and when displayed in the gallery, would appear as the full bio page

--- trunk/update.php
+++ trunk/update.php
@@ -11,7 +11,7 @@ Purpose: New update system that will dynamically pull the right update file base
 	on what version of the system is in use
 
 System Version: 2.6.8
-Last Modified: 2008-12-28 2128 EST
+Last Modified: 2009-01-02 1551 EST
 **/
 
 /* define the step var */
@@ -163,6 +163,8 @@ switch($step)
 			
 			<ul>
 				<li>Fixed bug where saved mission posts and joint posts showed the missions in the dropdown twice</li>
+				<li>Fixed bug where commas at the end of the character images string caused an additional image to be counted, and when displayed in the gallery, would appear as the full bio page</li>
+				<li>Fixed bug where positions and departments on the Positions &amp; Departments page didn&rsquo;t respect the order fields but where ordered by ID instead</li>
 			</ul>
 			
 			<h1><a href="update.php?step=2&version=<?=$urlVersion;?>">Next Step &raquo;</a></h1>
