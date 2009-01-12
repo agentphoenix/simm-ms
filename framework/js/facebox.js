51a20adfaaa29fc73635ec89ca2c24bbabc04e5e
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2009-01-12 02:27:18 +0000

    transitioning the HUD over to all CSS

--- framework/js/facebox.js
+++ framework/js/facebox.js
@@ -1,6 +1,6 @@
 /*
  * Facebox (for jQuery)
- * version: 1.0 (12/19/2007)
+ * version: 1.2 (05/05/2008)
  * @requires jQuery v1.2 or later
  *
  * Examples at http://famspam.com/facebox/
@@ -8,12 +8,12 @@
  * Licensed under the MIT:
  *   http://www.opensource.org/licenses/mit-license.php
  *
- * Copyright 2007 Chris Wanstrath [ chris@ozmm.org ]
+ * Copyright 2007, 2008 Chris Wanstrath [ chris@ozmm.org ]
  *
  * Usage:
  *  
  *  jQuery(document).ready(function() {
- *    jQuery('a[@rel*=facebox]').facebox() 
+ *    jQuery('a[rel*=facebox]').facebox() 
  *  })
  *
  *  <a href="#terms" rel="facebox">Terms</a>
@@ -30,143 +30,153 @@
  * 
  *    jQuery.facebox('some html')
  *
- *  This will open a facebox with "some html" as the content.
+ *  The above will open a facebox with "some html" as the content.
  *    
- *    jQuery.facebox(function() { ajaxes })
+ *    jQuery.facebox(function($) { 
+ *      $.get('blah.html', function(data) { $.facebox(data) })
+ *    })
  *
- *  This will show a loading screen before the passed function is called,
- *  allowing for a better ajax experience.
+ *  The above will show a loading screen before the passed function is called,
+ *  allowing for a better ajaxy experience.
+ *
+ *  The facebox function can also display an ajax page or image:
+ *  
+ *    jQuery.facebox({ ajax: 'remote.html' })
+ *    jQuery.facebox({ image: 'dude.jpg' })
+ *
+ *  Want to close the facebox?  Trigger the 'close.facebox' document event:
+ *
+ *    jQuery(document).trigger('close.facebox')
+ *
+ *  Facebox also has a bunch of other hooks:
+ *
+ *    loading.facebox
+ *    beforeReveal.facebox
+ *    reveal.facebox (aliased as 'afterReveal.facebox')
+ *    init.facebox
+ *
+ *  Simply bind a function to any of these hooks:
+ *
+ *   $(document).bind('reveal.facebox', function() { ...stuff to do after the facebox and contents are revealed... })
  *
  */
 (function($) {
-  $.facebox = function(data) {
-    $.facebox.init()
+  $.facebox = function(data, klass) {
     $.facebox.loading()
-    $.isFunction(data) ? data.call() : $.facebox.reveal(data)
+
+    if (data.ajax) fillFaceboxFromAjax(data.ajax)
+    else if (data.image) fillFaceboxFromImage(data.image)
+    else if (data.div) fillFaceboxFromHref(data.div)
+    else if ($.isFunction(data)) data.call($)
+    else $.facebox.reveal(data, klass)
   }
 
-  $.facebox.settings = {
-    loading_image : 'images/hud_loading.gif',
-    close_image   : 'images/hud_close.png',
-    image_types   : [ 'png', 'jpg', 'jpeg', 'gif' ],
-    facebox_html  : '\
+  /*
+   * Public, $.facebox methods
+   */
+
+  $.extend($.facebox, {
+    settings: {
+      opacity      : 0.85,
+      overlay      : true,
+      loadingImage : 'images/hud_loading.gif',
+      closeImage   : 'images/hud_close.png',
+      imageTypes   : [ 'png', 'jpg', 'jpeg', 'gif' ],
+      facebox_html  : '\
   <div id="facebox" style="display:none;"> \
     <div class="popup"> \
-      <table cellpadding="0" cellspacing="0"> \
-        <tbody> \
-          <tr class="title_bar"> \
-            <td class="top_left">&nbsp;</td> \
-			<td class="top_title"> \
-				<a href="#" class="close"> \
-				  <img src="" title="close" class="close_image" style="float:left; padding:3px 0 0 0;" /> \
-				</a> \
-				SIMM Management System \
-			</td> \
-			<td class="top_right">&nbsp;</td> \
-          </tr> \
-          <tr> \
-            <td class="side"></td> \
-            <td class="body_hud"> \
-              <div class="content_hud"> \
-              </div> \
-            </td> \
-            <td class="side"></td> \
-          </tr> \
-          <tr class="footer"> \
-            <td class="bottom_left"></td> \
-			<td class="bottom"></td> \
-			<td class="bottom_right"></td> \
-          </tr> \
-        </tbody> \
-      </table> \
+      <div class="hud_body"> \
+        <div class="hud_title"> \
+          	<a href="#" class="close"> \
+			  <img src="" title="close" class="close_image" /> \
+			</a> \
+			SIMM Management System</div> \
+        <div class="content_hud" style="padding:10px;"></div> \
+      </div> \
     </div> \
   </div>'
-  }
+  },
 
-  $.facebox.loading = function() {
-    if ($('#facebox .loading').length == 1) return true
+    loading: function() {
+      init()
+      if ($('#facebox .loading').length == 1) return true
+      showOverlay()
 
-    $('#facebox .content_hud').empty()
-    $('#facebox .body_hud').children().hide().end().
-      append('<div class="loading"><img src="'+$.facebox.settings.loading_image+'"/></div>')
+      $('#facebox .content_hud').empty()
+      $('#facebox .hud_body').children().hide().end().
+        append('<div class="loading"><img src="'+$.facebox.settings.loadingImage+'"/></div>')
 
-    var pageScroll = $.facebox.getPageScroll()
-    $('#facebox').css({
-      top:	pageScroll[1] + ($.facebox.getPageHeight() / 10),
-      left:	pageScroll[0]
-    }).show()
+      $('#facebox').css({
+        // top:	getPageScroll()[1] + (getPageHeight() / 10),
+        // left:	385.5
+      }).show()
 
-    $(document).bind('keydown.facebox', function(e) {
-      if (e.keyCode == 27) $.facebox.close()
-    })
-  }
+      $(document).bind('keydown.facebox', function(e) {
+        if (e.keyCode == 27) $.facebox.close()
+        return true
+      })
+      $(document).trigger('loading.facebox')
+    },
 
-  $.facebox.reveal = function(data, klass) {
-    if (klass) $('#facebox .content_hud').addClass(klass)
-    $('#facebox .content_hud').append(data)
-    $('#facebox .loading').remove()
-    $('#facebox .body_hud').children().fadeIn('normal')
-  }
+    reveal: function(data, klass) {
+      $(document).trigger('beforeReveal.facebox')
+      if (klass) $('#facebox .content').addClass(klass)
+      $('#facebox .content_hud').append(data)
+      $('#facebox .loading').remove()
+      $('#facebox .hud_body').children().fadeIn('normal')
+      // $('#facebox').css('left', $(window).width() / 2 - ($('#facebox table').width() / 2))
+      $(document).trigger('reveal.facebox').trigger('afterReveal.facebox')
+    },
 
-  $.facebox.close = function() {
-    $(document).unbind('keydown.facebox')
-    $('#facebox').fadeOut(function() {
-      $('#facebox .content_hud').removeClass().addClass('content_hud')
-    })
-    return false
-  }
+    close: function() {
+      $(document).trigger('close.facebox')
+      return false
+    }
+  })
 
-  $.fn.facebox = function() {
-    $.facebox.init()
+  /*
+   * Public, $.fn methods
+   */
 
-    var image_types = $.facebox.settings.image_types.join('|')
-    image_types = new RegExp('\.' + image_types + '$', 'i')
+  $.fn.facebox = function(settings) {
+    init(settings)
 
-    function click_handler() {
+    function clickHandler() {
       $.facebox.loading(true)
 
-      // support for rel="facebox[.inline_popup]" syntax, to add a class
-      var klass = this.rel.match(/facebox\[\.(\w+)\]/)
+      // support for rel="facebox.inline_popup" syntax, to add a class
+      // also supports deprecated "facebox[.inline_popup]" syntax
+      var klass = this.rel.match(/facebox\[?\.(\w+)\]?/)
       if (klass) klass = klass[1]
 
-      // div
-      if (this.href.match(/#/)) {
-        var url    = window.location.href.split('#')[0]
-        var target = this.href.replace(url,'')
-        $.facebox.reveal($(target).clone().show(), klass)
-
-      // image
-      } else if (this.href.match(image_types)) {
-        var image = new Image()
-        image.onload = function() {
-          $.facebox.reveal('<div class="image"><img src="' + image.src + '" /></div>', klass)
-        }
-        image.src = this.href
-
-      // ajax
-      } else {
-        $.get(this.href, function(data) { $.facebox.reveal(data, klass) })
-      }
-
+      fillFaceboxFromHref(this.href, klass)
       return false
     }
 
-    this.click(click_handler)
-    return this
+    return this.click(clickHandler)
   }
 
-  $.facebox.init = function() {
-    if ($.facebox.settings.inited) {
-      return true
-    } else {
-      $.facebox.settings.inited = true
-    }
+  /*
+   * Private methods
+   */
 
-    $('body').append($.facebox.settings.facebox_html)
+  // called one time to setup facebox on this page
+  function init(settings) {
+    if ($.facebox.settings.inited) return true
+    else $.facebox.settings.inited = true
+
+    $(document).trigger('init.facebox')
+    makeCompatible()
+
+    var imageTypes = $.facebox.settings.imageTypes.join('|')
+    $.facebox.settings.imageTypesRegexp = new RegExp('\.' + imageTypes + '$', 'i')
+
+    if (settings) $.extend($.facebox.settings, settings)
+    $('body').append($.facebox.settings.faceboxHtml)
 
     var preload = [ new Image(), new Image() ]
-    preload[0].src = $.facebox.settings.close_image
-    preload[1].src = $.facebox.settings.loading_image
+    preload[0].src = $.facebox.settings.closeImage
+    preload[1].src = $.facebox.settings.loadingImage
 
     $('#facebox').find('.b:first, .bl, .br, .tl, .tr').each(function() {
       preload.push(new Image())
@@ -174,11 +184,11 @@
     })
 
     $('#facebox .close').click($.facebox.close)
-    $('#facebox .close_image').attr('src', $.facebox.settings.close_image)
+    $('#facebox .close_image').attr('src', $.facebox.settings.closeImage)
   }
-
+  
   // getPageScroll() by quirksmode.com
-  $.facebox.getPageScroll = function() {
+  function getPageScroll() {
     var xScroll, yScroll;
     if (self.pageYOffset) {
       yScroll = self.pageYOffset;
@@ -193,8 +203,8 @@
     return new Array(xScroll,yScroll) 
   }
 
-  // adapter from getPageSize() by quirksmode.com
-  $.facebox.getPageHeight = function() {
+  // Adapted from getPageSize() by quirksmode.com
+  function getPageHeight() {
     var windowHeight
     if (self.innerHeight) {	// all except Explorer
       windowHeight = self.innerHeight;
@@ -205,4 +215,90 @@
     }	
     return windowHeight
   }
+
+  // Backwards compatibility
+  function makeCompatible() {
+    var $s = $.facebox.settings
+
+    $s.loadingImage = $s.loading_image || $s.loadingImage
+    $s.closeImage = $s.close_image || $s.closeImage
+    $s.imageTypes = $s.image_types || $s.imageTypes
+    $s.faceboxHtml = $s.facebox_html || $s.faceboxHtml
+  }
+
+  // Figures out what you want to display and displays it
+  // formats are:
+  //     div: #id
+  //   image: blah.extension
+  //    ajax: anything else
+  function fillFaceboxFromHref(href, klass) {
+    // div
+    if (href.match(/#/)) {
+      var url    = window.location.href.split('#')[0]
+      var target = href.replace(url,'')
+      $.facebox.reveal($(target).clone().show(), klass)
+
+    // image
+    } else if (href.match($.facebox.settings.imageTypesRegexp)) {
+      fillFaceboxFromImage(href, klass)
+    // ajax
+    } else {
+      fillFaceboxFromAjax(href, klass)
+    }
+  }
+
+  function fillFaceboxFromImage(href, klass) {
+    var image = new Image()
+    image.onload = function() {
+      $.facebox.reveal('<div class="image"><img src="' + image.src + '" /></div>', klass)
+    }
+    image.src = href
+  }
+
+  function fillFaceboxFromAjax(href, klass) {
+    $.get(href, function(data) { $.facebox.reveal(data, klass) })
+  }
+
+  function skipOverlay() {
+    return $.facebox.settings.overlay == false || $.facebox.settings.opacity === null 
+  }
+
+  function showOverlay() {
+    if (skipOverlay()) return
+
+    if ($('facebox_overlay').length == 0) 
+      $("body").append('<div id="facebox_overlay" class="facebox_hide"></div>')
+
+    $('#facebox_overlay').hide().addClass("facebox_overlayBG")
+      .css('opacity', $.facebox.settings.opacity)
+      .click(function() { $(document).trigger('close.facebox') })
+      .fadeIn(200)
+    return false
+  }
+
+  function hideOverlay() {
+    if (skipOverlay()) return
+
+    $('#facebox_overlay').fadeOut(200, function(){
+      $("#facebox_overlay").removeClass("facebox_overlayBG")
+      $("#facebox_overlay").addClass("facebox_hide") 
+      $("#facebox_overlay").remove()
+    })
+    
+    return false
+  }
+
+  /*
+   * Bindings
+   */
+
+  $(document).bind('close.facebox', function() {
+    $(document).unbind('keydown.facebox')
+    $('#facebox').fadeOut(function() {
+      $('#facebox .content_hud').removeClass().addClass('content')
+      hideOverlay()
+      $('#facebox .loading').remove()
+    })
+  })
+
 })(jQuery);
