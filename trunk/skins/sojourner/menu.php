b841556981744aa05c585447f2ed943f74cbf3f8
david.vanscott <david.vanscott@9c8e6ba3-2940-0410-ac8a-0737ef5a70a7>
2009-07-12 20:37:08 +0000

    lots of changes to the sojourner skin

--- trunk/skins/sojourner/menu.php
+++ trunk/skins/sojourner/menu.php
@@ -16,6 +16,8 @@ $page = (isset($_GET['page'])) ? $_GET['page'] : 'main';
 
 <script type="text/javascript">
 	$(document).ready(function(){
+		var name = '<?php echo $name;?>';
+		
 		$('#nav-main').clickMenu();
 		$('ul.hidemenu').show();
 		
@@ -26,6 +28,25 @@ $page = (isset($_GET['page'])) ? $_GET['page'] : 'main';
 		    next:   '#next', 
 		    prev:   '#prev' 
 		});
+		
+		$('a#userpanel').toggle(function(){
+			$('div.panel-body').slideDown('normal', function(){
+				$('.panel-trigger div.ui-icon').removeClass('ui-icon-triangle-1-s');
+				$('.panel-trigger div.ui-icon').addClass('ui-icon-triangle-1-n');
+			});
+			return false;
+		}, function(){
+			$('div.panel-body').slideUp('normal', function(){
+				$('.panel-trigger div.ui-icon').removeClass('ui-icon-triangle-1-n');
+				$('.panel-trigger div.ui-icon').addClass('ui-icon-triangle-1-s');
+			});
+			return false;
+		});
+		
+		if (name == 'admin.php')
+		{
+			$('#container .content .body').css('marginRight', '20%');
+		}
 	});
 </script>
 
@@ -42,16 +63,99 @@ if(isset($sessionCrewid))
 	$menu->skin = $sessionDisplaySkin;
 }
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+$unread = unreadMessages($sessionCrewid);
+
+$unreadIcon = ($unread == 0) ? 'icon-gray' : 'icon-green';
+$unreadImage = '<img src="skins/sojourner/images/'. $unreadIcon .'.png" border="0" alt="" /> &nbsp;';
 
 ?>
+
+<div id="panel">
+	<div class="panel-body">
+		<div class="wrapper">
+			<?php if (!isset($sessionCrewid)): ?>
+				<form method="post" action="<?php echo $webLocation;?>login.php?action=checkLogin">
+					<table class="panel-login">
+						<tbody>
+							<tr>
+								<td align="right"><strong>Username</strong></td>
+								<td>&nbsp;</td>
+								<td><input type="text" name="username" class="text" /></td>
+								<td rowspan="2">&nbsp;</td>
+								<td rowspan="2" valign="middle">
+									<input type="image" src="<?=SKIN_PATH;?>buttons/login-small.png" name="submit" class="buttonSmall" value="Login" />
+								</td>
+							</tr>
+							<tr>
+								<td align="right"><strong>Password</strong></td>
+								<td>&nbsp;</td>
+								<td><input type="password" name="password" class="text" /></td>
+							</tr>
+							<tr>
+								<td colspan="2"></td>
+								<td><br /><a href="<?=$webLocation;?>login.php?action=reset">&laquo; Reset Password</a></td>
+								<td colspan="2"></td>
+							</tr>
+						</tbody>
+					</table>
+				</form>
+			<?php else: ?>
+				<table class="table100">
+					<tbody>
+						<tr>
+							<td class="panel_1 align-top"><h4><? printCrewName( $sessionCrewid, "rank", "noLink" ); ?></h4>
+								<ul class="none">
+									<li><a href="<?php echo $webLocation;?>admin.php?page=user&sub=account&crew=<?php echo $sessionCrewid;?>">Edit Account</a></li>
+									<li><a href="http://localhost/nova/trunk/index.php/user/preferences">Edit Preferences</a></li>
+									<li><a href="http://localhost/nova/trunk/index.php/user/status">Request LOA</a></li>
+									<li><a href="http://localhost/nova/trunk/index.php/messages/index">Private Messages</a></li>
+									
+									<li><a href="<?php echo $webLocation;?>login.php?action=logout"><span>Logout</span></a></li>
+								</ul>
+							</td>
+							<td class="panel_spacer"></td>
+							<td class="panel_2 align-top">
+								<h4>Characters</h4>
+								<ul class="none">
+									<li>
+										<a href="http://localhost/nova/trunk/index.php/personnel/character/7">Major Alex Diaz</a> 
+										&nbsp;&nbsp;
+										<a href="http://localhost/nova/trunk/index.php/#" class="edit">[ Edit ]</a>
+									</li>
+								</ul>
+							</td>
+							<td class="panel_spacer"></td>
+							<td class="panel_3 align-top">
+								<h4>My Links</h4>
+								<?php $menu->user($sessionCrewid);?>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			<?php endif; ?>
+		</div>
+	</div>
+	<div class="panel-handle">
+		<div class="wrapper">
+			<?php if (isset($sessionCrewid)): ?>
+				<a href="#" id="userpanel" class="panel-trigger"><span>Dashboard</span></a>
+
+				<ul id="panel-handle-left">
+					<li><a href="<?php echo $webLocation;?>admin.php?page=user&sub=inbox"><span><?php echo $unreadImage;?>Inbox (<?php echo $unread;?>)</span></a></li>
+					<li><a href="<?php echo $webLocation;?>login.php?action=logout"><span>Logout</span></a></li>
+				</ul>
+			<?php else: ?>
+				<a href="#" id="userpanel" class="panel-trigger"><span>Login</span></a>
+			<?php endif;?>
+		</div>
+	</div>
+</div>
 
 <div id="header">
 	<div class="mainNav">
 		<div class="wrapper">
 			<div class="float-right"><img src="<?php echo SKIN_PATH;?>images/sojourner.png" alt="" /></div>
-			<?php $menu->main();?>
+			<?php echo $menu->main();?>
 		</div>
 	</div>
 </div>
@@ -67,8 +171,21 @@ ini_set('display_errors', 1);
 				<div class="cycle-container cycle-1">
 					<div class="cycle-1-content"><?php echo missionInfo();?></div>
 				</div>
-				<div class="cycle-container">Content 2</div>
-				<div class="cycle-container">Content 3</div>
+				<div class="cycle-container cycle-2">
+					<div class="cycle-1-content">
+						<h2>Backstory</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<h3><a href="#">Read the backstory &raquo;</a></h3>
+					</div>
+				</div>
+				<div class="cycle-container cycle-3">
+					<div class="cycle-1-content">
+						<h2>Join Today</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<h3><a href="#">Join now &raquo;</a></h3>
+					</div>
+				</div>
+				<div class="cycle-container">Mission Archives</div>
 			</div>
 		</div>
 	<?php else: ?>
@@ -82,24 +199,7 @@ ini_set('display_errors', 1);
 		<?php if (CUR_PAGE == 'admin.php'): ?>
 			<div class="nav">
 				<div class="login">
-				<? if(isset($sessionCrewid)) { ?>
-					<i>Hello, <? printCrewName( $sessionCrewid, "noRank", "noLink" ); ?></i><br />
-					{ <a href="<?=$webLocation;?>login.php?action=logout">Log Out</a> }
-				<? } else { ?>
-					<form method="post" action="<?=$webLocation;?>login.php?action=checkLogin" class="login">
-						<b>Username</b><br />
-						<input type="text" name="username" size="12" class="text" /><br /><br />
-					
-						<b>Password</b><br />
-						<input type="password" name="password" size="12" class="text" /><br /><br />
-					
-						<input type="image" src="<?=SKIN_PATH;?>buttons/login-small.png" name="submit" class="buttonSmall" value="Login" />
-					</form>
-					<br />
-					<a href="<?=$webLocation;?>login.php?action=reset">&laquo; Reset Password</a>
-				<? } ?>
-				<br /><br />
-				<?php include_once('framework/stardate.php');?>
+					<?php include_once('framework/stardate.php');?>
 				</div> <!-- close the .login layer -->
 				<br />
 	
